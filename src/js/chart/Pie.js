0ad95434b6e8acd9d98191ab534b6ec511e3e0c7
xile611 <327197681@qq.com>
2015-08-25 11:42:00 +0800

    feat(init): init add chart,component,shape

---
+++ src/js/chart/Pie.js
@@ -0,0 +1,106 @@
+'use strict';
+
+import React from 'react/addons';
+import Sector from '../shape/Sector';
+import PolarChartMixin from './PolarChartMixin';
+
+const Pie = React.createClass({
+  mixins: [PolarChartMixin],
+
+  propTypes: {
+    data: React.PropTypes.array,
+    keys: React.PropTypes.object,
+    minAngle: React.PropTypes.number
+  },
+
+  getDefaultProps () {
+    return {
+      // 数据
+      data: [],
+      keys: {
+        name: 'name',
+        value: 'value'
+      },
+      minAngle: 0
+    };
+  },
+
+  getSectors () {
+    let {cx, cy, innerRadius, outerRadius, startAngle, data, keys, minAngle, clockWise} = this.props;
+
+    let valueKey = keys.value,
+        len = data.length,
+        sectors = [],
+        sum = 0,
+        percent,
+        _startAngle,
+        _endAngle;
+
+    data.forEach((entry) => {
+      sum += entry[valueKey];
+    });
+
+    if (sum > 0) {
+
+      data.forEach((entry, i) => {
+        percent = entry[valueKey] / sum;
+
+        if (i) {
+          _startAngle = clockWise ? sectors[i - 1].endAngle : sectors[i - 1].startAngle;
+        } else {
+          _startAngle = startAngle;
+        }
+
+        _endAngle = _startAngle + (clockWise ? 1 : -1) * (minAngle + percent * (360 - len * minAngle));
+
+        sectors.push({
+          fill: entry.fill,
+          cx: cx,
+          cy: cy,
+          innerRadius: innerRadius,
+          outerRadius: outerRadius,
+          startAngle: clockWise ? _startAngle : _endAngle,
+          endAngle: clockWise ? _endAngle : _startAngle
+        });
+      });
+    }
+
+    return sectors;
+  },
+
+  renderSectors () {
+    let {cx, cy, innerRadius, outerRadius, data, keys, minAngle, clockWise, ...others} = this.props;
+    let sectors = this.getSectors();
+
+    return sectors.map((entry, i) => {
+      return (
+        <Sector
+          {...others}
+          cx={entry.cx}
+          cy={entry.cy}
+          fill={entry.fill}
+          innerRadius={entry.innerRadius}
+          outerRadius={entry.outerRadius}
+          startAngle={entry.startAngle}
+          endAngle={entry.endAngle}
+          key={'sector-' + i}/>
+      );
+    });
+  },
+
+  render () {
+    let data = this.props.data;
+
+    if (!data || !data.length) {
+      return;
+    }
+
+    return (
+      <g className='layer-pie'>
+        {this.renderSectors()}
+      </g>
+    );
+  }
+});
+
+export default Pie;
---
+++ src/js/shape/Curve.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import React from 'react/addons';
+import Interpolation from '../util/interpolation';
+import ShapeMixin from './ShapeMixin';
+
+const Curve = React.createClass({
+
+  mixins: [ShapeMixin],
+
+  propTypes: {
+    type: React.PropTypes.string,
+    points: React.PropTypes.arrayOf(React.PropTypes.object)
+  },
+
+  getDefaultProps () {
+    return {
+      // 点坐标
+      points: [],
+      // 曲线类型，linear - 折线
+      // stepBefore - 节点靠前的阶梯曲线, stepMiddle - 节点居中的阶梯曲线, stepAfter - 节点靠后的阶梯曲线
+      // smooth - 光滑曲线
+      type: 'linear'
+    };
+  },
+  /**
+   * 获取曲线路径
+   * @param {String} type 曲线的类型
+   * @param {Array} points 曲线的节点
+   * @return {String} 路径
+   */
+  getPath (type, points) {
+    if (Interpolation[type]) {
+      return Interpolation[type](points);
+    }
+
+    return Interpolation.linear(points);
+  },
+
+  render () {
+    let {points, type, ...others} = this.props;
+
+    if (!points || !points.length) {
+      return null;
+    }
+
+    return <path {...others} d={this.getPath(type, points)}/>;
+  }
+});
+
+export default Curve;
