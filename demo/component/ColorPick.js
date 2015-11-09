558f18bd1d40f85f59f0ad54dab1618444e9b69b
evilucifero <x@evilucifero.com>
2015-11-10 02:30:08 +0800

    add ColorPick Demo

--- demo/component/ColorPick.js
+++ demo/component/ColorPick.js
@@ -1,69 +1,81 @@
 'use strict';
 
-import React from 'react';
+import React from 'react/addons';
 import R from 'ramda';
+import {ColorUtils} from 'recharts'
 
 export default React.createClass({
   getInitialState() {
     return {
-      L: 50,
-      R: 50
+      l: 50,
+      r: 50,
+      h: Math.ceil(360 * Math.random()),
     };
   },
 
   handleR(e) {
     this.setState({
-      R: parseInt(e.target.value)
+      r: parseInt(e.target.value)
     });
   },
 
   handleL(e) {
     this.setState({
-      L: parseInt(e.target.value)
+      l: parseInt(e.target.value)
+    });
+  },
+
+  handleH(e) {
+    this.setState({
+      h: Math.ceil(360 * Math.random())
     });
   },
 
   render() {
-    const {R, L} = this.state;
-    const colorCircle = this.getColorCircle(R/100, L/100);
+    const {r, l, h} = this.state;
+    const colorCircle = ColorUtils.getLabColorCircle(r/100, l/100);
+
     return (
       <div>
-        <span>欢乐or抑郁(0-100)：&emsp;</span>
-        <input value={R} type='number' onChange={this.handleR} />
+        <span>欢乐or抑郁(L值, 0-100)：&nbsp;</span>
+        <input value={r} type='number' onChange={this.handleR} />
         <br/><br/>
-        <span>光明or黑暗(0-100)：&emsp;</span>
-        <input value={L} type='number' onChange={this.handleL} />
+        <span>光明or黑暗(半径, 0-100)：&nbsp;</span>
+        <input value={l} type='number' onChange={this.handleL} />
         <br/><br/>
         <div style={{padding: '2px'}}>
-          <span>color circle: </span>
           {
             R.repeat({}, 360).map((v, i) => (
               <div key={i} style={{
                 background: colorCircle(i),
                 display: 'inline-block',
                 width: '3px',
-                height: '300px'
+                height: '60px'
               }}></div>
             ))
           }
+          <span>&nbsp;color circle</span>
         </div>
-        <br/><hr/><br/>
+        <br/><hr/>
+        <div><button onClick={this.handleH}>Reset</button></div>
+        <br/>
         <div>
           {
-            ['complementary','triadic','split-complementary'].map((schema, index) => (
+            ['complementary','triadic','split-complementary']
+            .map((schema, index) => (
               <div key={index} style={{marginBottom: '40px'}}>
-                <span>{schema+': '}</span>
                 {
-                  this.getColorGroups(schema).map((v, i) => (
+                  ColorUtils.getColorGroups(h, schema).map((v, i) => (
                     <div key={i} style={{
                       background: colorCircle(v),
                       display: 'inline-block',
-                      width: '80px',
-                      height: '80px',
+                      width: '60px',
+                      height: '60px',
                       marginRight: '10px'
                     }}></div>
                   ))
                 }
+                <span>&nbsp;{schema}</span>
               </div>
             ))
           }
