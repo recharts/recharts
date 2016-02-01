d7fbc27bbb133e50542c86e6fd51565d0f2daefa
twobin.lbb <twobin.lbb@alibaba-inc.com>
2016-02-01 14:38:09 +0800

    feat:utilTest

---
+++ test/specs/util/LodashUtils.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { expect } from 'chai';
+import { ScatterChart, Scatter, CartesianGrid, Tooltip, XAxis, YAxis, ZAxis, CartesianAxis } from 'recharts';
+import LodashUtils from '../../../src/util/LodashUtils';
+import { mount, render } from 'enzyme';
+
+describe('ScatterChart of three dimension data', () => {
+  const data01 = [
+    { x: 100, y: 200, z: 200 },
+    { x: 120, y: 100, z: 260 },
+    { x: 170, y: 300, z: 400 },
+    { x: 140, y: 250, z: 280 },
+    { x: 150, y: 400, z: 500 },
+    { x: 110, y: 280, z: 200 },
+  ];
+
+  const data02 = [
+    { x: 200, y: 260, z: 240 },
+    { x: 240, y: 290, z: 220 },
+    { x: 190, y: 290, z: 250 },
+    { x: 198, y: 250, z: 210 },
+    { x: 180, y: 280, z: 260 },
+    { x: 210, y: 220, z: 230 },
+  ];
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <ScatterChart width={400} height={400} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+        <XAxis dataKey={"x"} name="stature" unit="cm"/>
+        <YAxis dataKey={"y"} name="weight" unit="kg"/>
+        <ZAxis dataKey={"z"} range={[4, 20]} name="score" unit="km"/>
+        <CartesianGrid />
+        <Scatter name="A school" data={data01} fillOpactity={0.3} fill="#ff7300"/>
+        <Scatter name="B school" data={data02} fill="#347300"/>
+        <Tooltip/>
+      </ScatterChart>
+    );
+  });
+
+  it('LodashUtils', () => {
+    expect(LodashUtils.isString('wrapper')).to.equal(true);
+  });
+
+  it('LodashUtils', () => {
+    expect(LodashUtils.isArray([])).to.equal(true);
+  });
+
+  it('LodashUtils', () => {
+    expect(LodashUtils.isNumber(0)).to.equal(true);
+  });
+
+  it('LodashUtils', () => {
+    expect(LodashUtils.isFunction(() => {})).to.equal(true);
+  });
+
+  it('LodashUtils', () => {
+    expect(LodashUtils.range(1, 5)).to.eql([1, 2, 3, 4]);
+  });
+
+  it('LodashUtils', () => {
+    expect(LodashUtils.getPercentValue('25%', 1)).to.eql(0.25);
+  });
+
+  it('LodashUtils', () => {
+    assert.isObject(LodashUtils.getElementOfObject(wrapper), 'getElementOfObject is an object');
+  });
+
+  it('LodashUtils', () => {    expect(LodashUtils.toPercentage(0.25)).to.eql('25%');
+  });
+
+  it('LodashUtils', () => {
+    const num = '101.00';
+    expect(LodashUtils.toFixed(101, 2).toString()).to.eql(num);
+  });
+});
