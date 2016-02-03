e75d63b583cc3948ad3c421601190cdcb628f984
xile611 <327197681@qq.com>
2016-02-03 14:44:00 +0800

    fix(LineChart, CartesianChart): 1.fix the bug of margin when only part of the attributes are specified 2.fix the bug of number axis when domain is specified 3.fix the bug of category number when no dataKey is specified 4.format the code in README.md

--- README.md
+++ README.md
@@ -24,17 +24,17 @@ The biggest feature or difference is helping you write charts in React applicati
 ## Examples
 
 ```jsx
-<LineChart 
-  width={400} 
-  height={400} 
+<LineChart
+  width={400}
+  height={400}
   data={data}
   margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
 >
-  <XAxis dataKey='name' />
+  <XAxis dataKey="name" />
   <Tooltip />
-  <CartesianGrid stroke='#f5f5f5' />
-  <Line type='monotone' dataKey='uv' stroke='#ff7300' yAxisId={0} />
-  <Line type='monotone' dataKey='pv' stroke='#387908' yAxisId={1} />
+  <CartesianGrid stroke="#f5f5f5" />
+  <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+  <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
 </LineChart>
 ```
 
--- demo/component/LineChart.js
+++ demo/component/LineChart.js
@@ -26,7 +26,18 @@ export default React.createClass({
   render () {
     return (
       <div className='line-charts'>
-        <p>A simple LineChart</p>
+        <p>A simple LineChart with fixed domain y-axis</p>
+        <div className='line-chart-wrapper'>
+          <LineChart width={400} height={400} data={data} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+            <CartesianGrid stroke='#f5f5f5'/>
+            <Legend/>
+            <XAxis />
+            <YAxis domain={[0, 500]}/>
+            <Line type='monotone' dataKey='uv' stroke='#ff7300'/>
+          </LineChart>
+        </div>
+
+        <p>A simple LineChart with customized line dot</p>
         <div className='line-chart-wrapper'>
           <LineChart width={400} height={400} data={data} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
             <CartesianGrid stroke='#f5f5f5'/>
@@ -38,7 +49,7 @@ export default React.createClass({
         <p>LineChart with two y-axes</p>
         <div className='line-chart-wrapper' style={{padding: 40}}>
           <LineChart width={400} height={400} data={data}
-            margin={{top: 5, right: 30, left: 30, bottom: 5}}>
+            margin={{top: 10, bottom: 10, left: 30, right: 30}}>
             <XAxis dataKey='name'/>
             <Tooltip/>
             <CartesianGrid stroke='#f5f5f5'/>
