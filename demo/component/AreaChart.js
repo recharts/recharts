5e6800ceb8abbc425ee9a560a82416c54b51b7e3
xile611 <327197681@qq.com>
2016-02-22 17:56:47 +0800

    docs: update demo of AreaChart

--- demo/component/AreaChart.js
+++ demo/component/AreaChart.js
@@ -36,30 +36,30 @@ const CustomTooltip = React.createClass({
   }
 });
 
-        // <p>Stacked AreaChart</p>
-        // <div className="area-chart-wrapper">
-        //   <AreaChart width={800} height={400} data={data}
-        //     margin={{ top: 20, right: 80, left: 20, bottom: 5 }}>
-        //     <XAxis dataKey="name" label="province"/>
-        //     <YAxis/>
-        //     <Tooltip />
-        //     <Area stackId="0" type="monotone" dataKey="uv" stroke="#ff7300" fill="#ff7300" />
-        //     <Area stackId="0" type="monotone" dataKey="pv" stroke="#387908" fill="#387908" />
-        //   </AreaChart>
-        // </div>
-
-        // <p>Tiny AreaChart</p>
-        // <div className="area-chart-wrapper">
-        //   <AreaChart width={100} height={50} data={data} margin={{ top: 5, right: 0, left: 0, bottom: 5 }}>
-        //     <Area type="monotone" dataKey="uv" stroke="#ff7300" fill="#ff7300" />
-        //   </AreaChart>
-        // </div>
 export default React.createClass({
   displayName: 'AreaChartDemo',
 
   render() {
     return (
       <div className="area-charts">
+        <p>Stacked AreaChart</p>
+        <div className="area-chart-wrapper">
+          <AreaChart width={800} height={400} data={data}
+            margin={{ top: 20, right: 80, left: 20, bottom: 5 }}>
+            <XAxis dataKey="name" label="province"/>
+            <YAxis/>
+            <Tooltip />
+            <Area stackId="0" type="monotone" dataKey="uv" stroke="#ff7300" fill="#ff7300" />
+            <Area stackId="0" type="monotone" dataKey="pv" stroke="#387908" fill="#387908" />
+          </AreaChart>
+        </div>
+
+        <p>Tiny AreaChart</p>
+        <div className="area-chart-wrapper">
+          <AreaChart width={100} height={50} data={data} margin={{ top: 5, right: 0, left: 0, bottom: 5 }}>
+            <Area type="monotone" dataKey="uv" stroke="#ff7300" fill="#ff7300" />
+          </AreaChart>
+        </div>
 
         <p>AreaChart with three y-axes</p>
         <div className="area-chart-wrapper">
@@ -74,30 +74,30 @@ export default React.createClass({
           </AreaChart>
         </div>
 
+        <p>AreaChart of vertical layout </p>
+        <div className="area-chart-wrapper" style={{ margin: 40 }}>
+          <AreaChart width={400} height={400} data={data} layout='vertical'
+            margin={{top: 5, right: 30, bottom: 5, left: 5}}>
+            <YAxis type="category" dataKey="name" />
+            <XAxis type="number" xAxisId={0} orientation="top" />
+            <XAxis type="number" xAxisId={1} orientation="bottom" />
+            <Area dataKey="uv" type="monotone" stroke="#ff7300" fill="#ff7300" strokeWidth={2} xAxisId={0} />
+            <Area dataKey="pv" type="monotone" stroke="#387908" fill="#387908" strokeWidth={2} xAxisId={1} />
+            <Tooltip/>
+          </AreaChart>
+        </div>
+
+        <p>AreaChart with custom tooltip</p>
+        <div className="area-chart-wrapper">
+          <AreaChart width={900} height={250} data={data} margin={{ top: 10, right: 30, bottom: 10, left: 10 }}>
+            <XAxis dataKey="name" hasTick />
+            <YAxis tickCount={7} hasTick />
+            <Tooltip content={<CustomTooltip external={data} />} />
+            <CartesianGrid stroke="#f5f5f5" />
+            <Area type="monotone" dataKey="pv" stroke="#ff7300" fill="#ff7300" fillOpacity={0.25} />
+          </AreaChart>
+        </div>
       </div>
     );
   }
 });
-        // <p>AreaChart of vertical layout </p>
-        // <div className="area-chart-wrapper" style={{ margin: 40 }}>
-        //   <AreaChart width={400} height={400} data={data} layout='vertical'
-        //     margin={{top: 5, right: 30, bottom: 5, left: 5}}>
-        //     <YAxis type="category" dataKey="name" />
-        //     <XAxis type="number" xAxisId={0} orientation="top" />
-        //     <XAxis type="number" xAxisId={1} orientation="bottom" />
-        //     <Area dataKey="uv" type="monotone" stroke="#ff7300" fill="#ff7300" strokeWidth={2} xAxisId={0} />
-        //     <Area dataKey="pv" type="monotone" stroke="#387908" fill="#387908" strokeWidth={2} xAxisId={1} />
-        //     <Tooltip/>
-        //   </AreaChart>
-        // </div>
-
-        // <p>AreaChart with custom tooltip</p>
-        // <div className="area-chart-wrapper">
-        //   <AreaChart width={900} height={250} data={data} margin={{ top: 10, right: 30, bottom: 10, left: 10 }}>
-        //     <XAxis dataKey="name" hasTick />
-        //     <YAxis tickCount={7} hasTick />
-        //     <Tooltip content={<CustomTooltip external={data} />} />
-        //     <CartesianGrid stroke="#f5f5f5" />
-        //     <Area type="monotone" dataKey="pv" stroke="#ff7300" fill="#ff7300" fillOpacity={0.25} />
-        //   </AreaChart>
-        // </div>
