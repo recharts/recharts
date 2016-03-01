53fa95c62f18fc04d4abae41f2d1973f342b7a74
xile611 <327197681@qq.com>
2016-03-01 16:41:05 +0800

    fix: fix the "Illegal invocation" of LogUtils

--- src/util/LogUtils.js
+++ src/util/LogUtils.js
@@ -1,42 +1,24 @@
 const __DEV__ = process.env.NODE_ENV !== 'production';
 
-const empty = () => {};
-
-const getLogFunction = (type) => {
-  return (console && console[type]) || empty;
-};
-
-const generateLogFunction = (type) => {
-  const logFunction = getLogFunction(type);
-
-  return (condition, format, a, b, c, d, e, f) => {
-    if (__DEV__) {
-      if (format === undefined) {
-        logFunction('LogUtils requires an error message argument');
-      }
-
-      if (!condition) {
-        if (format === undefined) {
-          logFunction(
-            'Minified exception occurred; use the non-minified dev environment ' +
-            'for the full error message and additional helpful warnings.'
-          );
-        } else {
-          const args = [a, b, c, d, e, f];
-          let argIndex = 0;
-
-          logFunction(
-            format.replace(/%s/g, function() { return args[argIndex++]; })
-          );
-        }
-      }
+export const warn = (condition, format, a, b, c, d, e, f) => {
+  if (__DEV__ && typeof console !== 'undefined' && console.warn) {
+    if (format === undefined) {
+      console.warn('LogUtils requires an error message argument');
     }
 
-  };
-};
+    if (!condition) {
+      if (format === undefined) {
+        console.warn(
+          'Minified exception occurred; use the non-minified dev environment ' +
+          'for the full error message and additional helpful warnings.'
+        );
+      } else {
+        const args = [a, b, c, d, e, f];
+        let argIndex = 0;
 
-export const log = generateLogFunction('log');
-export const info = generateLogFunction('info');
-export const warn = generateLogFunction('warn');
-export const error = generateLogFunction('error');
+        console.warn(format.replace(/%s/g, () => args[argIndex++]));
+      }
+    }
+  }
+};
 
