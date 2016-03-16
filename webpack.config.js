cf4f49c8fb3a2856d74208f6fd953e1266ad493f
arcthur <arthurtemptation@gmail.com>
2016-03-16 15:36:52 +0800

    deps: update webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var fs = require('fs');
 var webpack = require('webpack');
+var env = process.env.NODE_ENV
 
-module.exports = {
+var config = {
   entry: './src/index.js',
 
   output: {
@@ -12,56 +13,65 @@ module.exports = {
 
   module: {
     loaders: [{
-      test: /\.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
+      test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       loader: 'babel',
     }],
-    resolve: {
-      alias: {
-        'react': path.resolve(__dirname, './node_modules/react/'),
-        'react-dom': path.resolve(__dirname, './node_modules/react-dom/'),
-        'react-addons-transition-group': path.resolve(__dirname, './node_modules/react-addons-transition-group')
-      }
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [
+      path.resolve('./src'),
+      'node_modules'
+    ],
+    mainFields: ['jsnext:main', 'main'],
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      'react-addons-transition-group': path.resolve(__dirname, './node_modules/react-addons-transition-group'),
     }
   },
 
-  externals: [{
-    'react': {
+  externals: {
+    "react": {
       root: 'React',
       commonjs2: 'react',
       commonjs: 'react',
       amd: 'react'
-    }
-  }, {
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'ReactDOM',
-      commonjs: 'ReactDOM',
-      amd: 'ReactDOM'
-    }
-  }, {
-    'react-dom/server': {
-      root: 'ReactDOMServer',
-      commonjs2: 'ReactDOMServer',
-      commonjs: 'ReactDOMServer',
-      amd: 'ReactDOMServer'
-    }
-  }, {
-    'react-addons-transition-group': {
-      root: 'ReactTransitionGroup',
-      commonjs2: 'ReactTransitionGroup',
-      commonjs: 'ReactTransitionGroup',
-      amd: 'ReactTransitionGroup'
-    }
-  }],
+    },
+    'react-dom': 'ReactDOM',
+    'react-dom/server': 'ReactDOMServer',
+    'react-addons-transition-group': 'ReactTransitionGroup',
+  },
 
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    })
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }),
   ],
 };
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  );
+}
+
+module.exports = config;
