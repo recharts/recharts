21d230bd7db4c1d5e0c474027b5a4412450a51ee
jasonHzq <hzongquan@126.com>
2016-04-05 20:27:41 +0800

    fix(Line): fix Line animation logic

--- demo/component/LineChart.js
+++ demo/component/LineChart.js
@@ -24,6 +24,15 @@ const data01 = [
   { day: '05-09', wether: 'sunny' },
 ];
 
+const data02 = [
+  { name: 'Page A', uv: 300, pv: 2600, amt: 3400 },
+  { name: 'Page B', uv: 400, pv: 4367, amt: 6400 },
+  { name: 'Page C', uv: 300, pv: 1398, amt: 2400 },
+  { name: 'Page D', uv: 200, pv: 9800, amt: 2400 },
+  { name: 'Page E', uv: 278, pv: 3908, amt: 2400 },
+  { name: 'Page F', uv: 189, pv: 4800, amt: 2400 },
+];
+
 const renderSpecialDot = (props) => {
   const { cx, cy, stroke, key } = props;
 
@@ -38,6 +47,10 @@ const renderLabel = (props) => {
 
 export default React.createClass({
   displayName: 'LineChartDemo',
+
+  getInitialState() {
+    return { data };
+  },
 
   render() {
     return (
@@ -89,6 +102,30 @@ export default React.createClass({
             <Line dataKey='uv' stroke='#ff7300' strokeWidth={2} yAxisId={0}/>
             <Line dataKey='pv' stroke='#387908' strokeWidth={2} yAxisId={1}/>
             <Line dataKey='amt' stroke='#38abc8' strokeWidth={2} yAxisId={2}/>
+          </LineChart>
+        </div>
+
+        <p>LineChart when data change</p>
+        <a
+          href="javascript:void(0)"
+          className="btn"
+
+          onClick={() => { this.setState({ data: this.state.data === data ? data02 : data }); }}
+        >
+          change data
+        </a>
+        <div className="line-chart-wrapper">
+          <LineChart
+            width={400}
+            height={400}
+            data={this.state.data}
+            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+          >
+            <CartesianGrid stroke="#f5f5f5" />
+            <Legend />
+            <XAxis />
+            <YAxis domain={[0, 500]} />
+            <Line type="monotone" dataKey="uv" dot={<CustomLineDot/>} stroke="#ff7300" />
           </LineChart>
         </div>
 
