aec73d638ae9fa14c5e52e543934eb8f1b599d21
Nick Palmer <nick@nick.codes>
2016-08-19 01:18:47 -0700

    Fix #136: New RepsonsiveContainer API
    * The ResponsiveContainer can make use of optional
      minWidth, minHeight and aspect options to give more
      control over the sizing of the chart in various layouts.

--- src/component/ResponsiveContainer.js
+++ src/component/ResponsiveContainer.js
@@ -7,56 +7,70 @@ import pureRender from '../util/PureRender';
 import { getPercentValue, isPercent } from '../util/DataUtils';
 import { warn } from '../util/LogUtils';
 
-const render = ({ width, height, container, children }) => {
+const render = ({ aspect, width, height, minWidth, minHeight, container, children }) => {
+
   warn(isPercent(width) || isPercent(height),
-    `The width(%s) and height(%s) are both fixed number,
-     maybe you don't need to use ResponsiveContainer.`,
+    `The width(%s) and height(%s) are both fixed numbers,
+     maybe you don't need to use a ResponsiveContainer.`,
     width, height
   );
 
-  const calculatedWidth = getPercentValue(width, container.width);
-  const calculatedHeight = getPercentValue(height, container.height);
+  warn(!aspect || aspect > 0,
+       'The aspect(%s) must be greater than zero.',
+       aspect);
+
+  let calculatedWidth = getPercentValue(width, container.width);
+  let calculatedHeight = getPercentValue(height, container.height);
+
+  if (aspect && aspect > 0) {
+    // Preserve the desired aspect ratio
+    if (calculatedWidth / calculatedHeight > aspect) {
+      calculatedWidth = calculatedHeight * aspect;
+    } else if (calculatedWidth / calculatedHeight < aspect) {
+      calculatedHeight = calculatedWidth / aspect;
+    }
+  }
 
   warn(calculatedWidth > 0 && calculatedHeight > 0,
     `The width(%s) and height(%s) of chart should be greater than 0,
-    please check the style of container, or the props width(%s) and height(%s).`,
-     calculatedWidth, calculatedHeight, width, height
+     please check the style of container, or the props width(%s) and height(%s),
+     or add a minWidth(%s) or minHeight(%s) or use aspect(%s) to control the
+     height and width.`,
+    calculatedWidth, calculatedHeight, width, height, minWidth, minHeight, aspect
   );
 
-  if (calculatedWidth > 0 && calculatedHeight > 0) {
-    return React.cloneElement(children, {
-      width: calculatedWidth,
-      height: calculatedHeight,
-    });
-  }
-
-  return null;
+  return React.cloneElement(children, {
+    width: calculatedWidth,
+    height: calculatedHeight,
+  });
 };
 
-const style = {
-  width: '100%',
-  height: '100%',
-};
-
-const ResponsiveContainer = props => (
-  <div className="recharts-responsive-container" style={style}>
-    <ContainerDimensions>
-      {
-        container =>
-          render({
-            container,
-            ...props,
-          })
+const ResponsiveContainer = props => {
+  const { minWidth, minHeight, width, height } = props;
+  const style = { width, height, minWidth, minHeight };
+  return (
+    <div className="recharts-responsive-container" style={style}>
+      <ContainerDimensions>
+        {
+          container =>
+            render({
+              container,
+              ...props,
+            })
       }
-    </ContainerDimensions>
-  </div>
-);
+      </ContainerDimensions>
+    </div>
+  );
+};
 
 ResponsiveContainer.displayName = 'ResponsiveContainer';
 ResponsiveContainer.propTypes = {
+  aspect: PropTypes.number,
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  children: PropTypes.node,
+  minHeight: PropTypes.number,
+  minWidth: PropTypes.number,
+  children: PropTypes.node.isRequired,
 };
 
 ResponsiveContainer.defaultProps = {
