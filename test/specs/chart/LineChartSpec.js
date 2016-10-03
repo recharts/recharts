afbaeb0a4ec10a1b169e71be817f0561455501d8
Bill Neff <bneff@synacor.com>
2016-10-02 23:14:57 -0400

    added tests for the tooltip-cursor on mouse enter/move/exit; added tests for chart when brush changes to make sure points update appropriately

--- test/specs/chart/LineChartSpec.js
+++ test/specs/chart/LineChartSpec.js
@@ -4,6 +4,8 @@ import { LineChart, Line, Curve, XAxis, YAxis, Tooltip, Brush, Legend } from 're
 import { mount, render } from 'enzyme';
 import sinon from 'sinon';
 
+/* eslint-disable max-len */
+
 describe('<LineChart />', () => {
   const data = [
     { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
@@ -17,7 +19,7 @@ describe('<LineChart />', () => {
   it('Render 1 line in simple LineChart', () => {
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.recharts-line .recharts-line-curve').length).to.equal(1);
@@ -26,22 +28,22 @@ describe('<LineChart />', () => {
   it('Render 1 line in simple LineChart', () => {
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <XAxis/>
-        <YAxis type="category"/>
-        <Line type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <XAxis />
+        <YAxis type="category" />
+        <Line type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.recharts-line .recharts-line-curve').length).to.equal(1);
   });
 
   it('Renders customized active dot when activeDot is set to be a ReactElement', () => {
-    const ActiveDot = ({cx, cy}) => {
-      return <circle cx={cx} cy={cy} r={10} className="customized-active-dot"/>;
+    const ActiveDot = ({ cx, cy }) => {
+      return <circle cx={cx} cy={cy} r={10} className="customized-active-dot" />;
     };
     const wrapper = mount(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line activeDot={<ActiveDot/>} type="monotone" dataKey="uv" stroke="#ff7300"/>
-        <Tooltip/>
+        <Line activeDot={<ActiveDot />} type="monotone" dataKey="uv" stroke="#ff7300" />
+        <Tooltip />
       </LineChart>
     );
 
@@ -58,13 +60,13 @@ describe('<LineChart />', () => {
   });
 
   it('Renders customized active dot when activeDot is set to be a function', () => {
-    const renderActiveDot = ({cx, cy}) => {
-      return <circle cx={cx} cy={cy} r={10} className="customized-active-dot"/>;
+    const renderActiveDot = ({ cx, cy }) => {
+      return <circle cx={cx} cy={cy} r={10} className="customized-active-dot" />;
     };
     const wrapper = mount(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line activeDot={renderActiveDot} type="monotone" dataKey="uv" stroke="#ff7300"/>
-        <Tooltip/>
+        <Line activeDot={renderActiveDot} type="monotone" dataKey="uv" stroke="#ff7300" />
+        <Tooltip />
       </LineChart>
     );
 
@@ -84,7 +86,7 @@ describe('<LineChart />', () => {
   it('Render 1 line in simple LineChart', () => {
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line strokeDasharray="5 5" type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line strokeDasharray="5 5" type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.recharts-line .recharts-line-curve').length).to.equal(1);
@@ -93,7 +95,7 @@ describe('<LineChart />', () => {
   it('Renders 1 dot no line when the length of data is 1', () => {
     const wrapper = render(
       <LineChart width={400} height={400} data={data.slice(0, 1)} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} label type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line isAnimationActive={false} label type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.recharts-line .recharts-line-curve').length).to.equal(0);
@@ -103,7 +105,7 @@ describe('<LineChart />', () => {
   it('Renders 6 labels when label is setted to be true', () => {
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} label type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line isAnimationActive={false} label type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.recharts-line-label').length).to.equal(6);
@@ -112,11 +114,11 @@ describe('<LineChart />', () => {
   it('Renders 6 labels when label is setted to be a function', () => {
     const renderLabel = (props) => {
       const { x, y, key } = props;
-      return <text className="customized-label" x={x} y={y} key={key}>test</text>
+      return <text className="customized-label" x={x} y={y} key={key}>test</text>;
     };
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} label={renderLabel} dataKey="uv" stroke="#ff7300"/>
+        <Line isAnimationActive={false} label={renderLabel} dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     const labels = wrapper.find('.customized-label');
@@ -127,11 +129,11 @@ describe('<LineChart />', () => {
   it('Renders 6 labels when label is setted to be a react element', () => {
     const CustomizedLabel = (props) => {
       const { x, y, key } = props;
-      return <text className="customized-label" x={x} y={y} key={key}>test</text>
+      return <text className="customized-label" x={x} y={y} key={key}>test</text>;
     };
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} label={<CustomizedLabel />} dataKey="uv" stroke="#ff7300"/>
+        <Line isAnimationActive={false} label={<CustomizedLabel />} dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     const labels = wrapper.find('.customized-label');
@@ -142,7 +144,7 @@ describe('<LineChart />', () => {
   it('Renders 6 dots when dot is setted to be true', () => {
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} dot type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line isAnimationActive={false} dot type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.recharts-line-dot').length).to.equal(6);
@@ -152,11 +154,11 @@ describe('<LineChart />', () => {
     const renderDot = (props) => {
       const { cx, cy, key } = props;
 
-      return <circle className="customized-dot" key={key} cx={cx} cy={cy} r={10}/>
+      return <circle className="customized-dot" key={key} cx={cx} cy={cy} r={10} />;
     };
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} dot={renderDot} type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line isAnimationActive={false} dot={renderDot} type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.customized-dot').length).to.equal(6);
@@ -166,11 +168,11 @@ describe('<LineChart />', () => {
     const Dot = (props) => {
       const { cx, cy, key } = props;
 
-      return <circle className="customized-dot" key={key} cx={cx} cy={cy} r={10}/>
+      return <circle className="customized-dot" key={key} cx={cx} cy={cy} r={10} />;
     };
     const wrapper = render(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} dot={<Dot />} type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line isAnimationActive={false} dot={<Dot />} type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     expect(wrapper.find('.customized-dot').length).to.equal(6);
@@ -179,7 +181,7 @@ describe('<LineChart />', () => {
   it('click on Curve should invoke onClick callback', () => {
     const wrapper = mount(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     wrapper.setProps({ data: [] });
@@ -189,7 +191,7 @@ describe('<LineChart />', () => {
     const onClick = sinon.spy();
     const wrapper = mount(
       <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line onClick={onClick} type="monotone" dataKey="uv" stroke="#ff7300"/>
+        <Line onClick={onClick} type="monotone" dataKey="uv" stroke="#ff7300" />
       </LineChart>
     );
     const curve = wrapper.find(Curve);
@@ -197,32 +199,89 @@ describe('<LineChart />', () => {
     expect(onClick.calledOnce).to.equal(true);
   });
 
-  it('MouseEnter lineChart should show tooltip, active dot, and cursor', () => {
+  it('should show tooltip cursor on MouseEnter and MouseMove and hide on MouseLeave', () => {
+    const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+    const height = 400;
+    const width = 400;
     const wrapper = mount(
-      <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <Line isAnimationActive={false} type="monotone" dataKey="uv" stroke="#ff7300"/>
+      <LineChart width={width} height={height} data={data} margin={margin}>
+        <Line isAnimationActive={false} type="monotone" dataKey="uv" stroke="#ff7300" />
         <Tooltip />
         <Brush />
         <Legend layout="vertical" />
       </LineChart>
     );
 
-    wrapper.simulate('mouseEnter');
-    wrapper.simulate('mouseMove');
+		// simulate entering just past Page A to test snapping of the cursor line
+    expect(wrapper.find('.recharts-tooltip-cursor').length).to.equal(0);
+    wrapper.simulate('mouseEnter', { pageX: margin.left + 10, pageY: height / 2 });
+
+    let tooltipCursors = wrapper.find('.recharts-tooltip-cursor');
+    expect(tooltipCursors.length).to.equal(1);
+
+		// make sure tooltip is in the right spot.
+    const chartBottom = height - margin.top - 2 * margin.bottom;
+    let expectedX = margin.left;
+    expect(tooltipCursors.at(0).props().d).to.equal(`M${expectedX},${margin.top}L${expectedX},${chartBottom}`);
+
+		// simulate moving 10 pixels past the PageC Dot
+    const chartWidth = width - margin.left - margin.right;
+    const dotSpacing = chartWidth / (data.length - 1);
+    expectedX = margin.left + dotSpacing * 2;
+    wrapper.simulate('mouseMove', { pageX: expectedX + 10, pageY: height / 2 });
+
+    tooltipCursors = wrapper.find('.recharts-tooltip-cursor');
+    expect(tooltipCursors.length).to.equal(1);
+
+
+    expect(tooltipCursors.at(0).props().d).to.equal(`M${expectedX},${margin.top}L${expectedX},${chartBottom}`);
+
+		// simulate leaving the area
     wrapper.simulate('mouseLeave');
+    expect(wrapper.find('.recharts-tooltip-cursor').length).to.equal(0);
 
-    wrapper.setState({
-      isTooltipActive: true,
-      activeTooltipIndex: 1,
-      activeTooltipLabel: 'test',
-      activeTooltipCoord: {
-        x: 100,
-        y: 100,
-      },
-    });
+  });
 
-    // expect(wrapper.find('.recharts-tooltip-cursor').length).to.equal(1);
-    // expect(wrapper.find('.recharts-line-active-dot').length).to.equal(1);
+  it('Should update the line chart when the brush changes', () => {
+    const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+    const height = 400;
+    const width = 400;
+    const wrapper = mount(
+      <LineChart width={width} height={height} data={data} margin={margin}>
+        <Line isAnimationActive={false} type="monotone" dataKey="uv" stroke="#ff7300" />
+        <Tooltip />
+        <Brush />
+        <Legend layout="vertical" />
+      </LineChart>
+    );
+
+    const lineDots = wrapper.find('.recharts-line-dots');
+    expect(lineDots.length).to.equal(1);
+    expect(lineDots.children().length).to.equal(6);
+
+		// verify one of the dots that we expect to move when the brush happens
+    expect(lineDots.childAt(2).props().payload.value).to.equal(data[2].uv);
+    expect(lineDots.childAt(2).props().cx).to.equal(164);
+    expect(lineDots.childAt(2).props().cy).to.equal(100);
+
+		// simulate a brush to only include the data elements at indices 2-4
+    wrapper.instance().handleBrushChange({ startIndex: 2, endIndex: 4 });
+
+		// we should only have three dots now
+    const newLineDots = wrapper.find('.recharts-line-dots');
+    expect(newLineDots.length).to.equal(1);
+    expect(newLineDots.children().length).to.equal(3);
+
+		// make sure the new first dot is the same as the old 2 dot, just in a new place
+    expect(newLineDots.childAt(0).props().payload.value).to.equal(data[2].uv);
+    expect(newLineDots.childAt(0).props().cx).to.equal(margin.left);
+    expect(newLineDots.childAt(0).props().cy).to.equal(20);
+
+		// make sure the new last dot is the same as the old 4 dot, just in the last spot
+    expect(newLineDots.childAt(2).props().payload.value).to.equal(data[4].uv);
+    expect(newLineDots.childAt(2).props().cx).to.equal(width - margin.right);
+    expect(newLineDots.childAt(2).props().cy).to.equal(43.4666666666667);
+
   });
 
 });
