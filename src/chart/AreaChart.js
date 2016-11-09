ca44fc4d8a301ef39e06db7dd024e88d366f78fd
evilucifero <x@evilucifero.com>
2016-11-09 16:30:45 +0800

    fix: fix lint

--- src/chart/AreaChart.js
+++ src/chart/AreaChart.js
@@ -145,7 +145,7 @@ export class AreaChart extends Component {
 
     return React.isValidElement(tooltipItem.props.cursor) ?
       React.cloneElement(tooltipItem.props.cursor, cursorProps) :
-        <Curve {...cursorProps} type="linear" className="recharts-tooltip-cursor" />;
+      <Curve {...cursorProps} type="linear" className="recharts-tooltip-cursor" />;
   }
 
   renderActiveDot(option, props) {
--- src/chart/LineChart.js
+++ src/chart/LineChart.js
@@ -107,7 +107,7 @@ export class LineChart extends Component {
 
     return React.isValidElement(tooltipItem.props.cursor) ?
       React.cloneElement(tooltipItem.props.cursor, cursorProps) :
-        <Curve {...cursorProps} type="linear" className="recharts-tooltip-cursor" />;
+      <Curve {...cursorProps} type="linear" className="recharts-tooltip-cursor" />;
   }
 
   renderActiveDot(option, props) {
