79a8e07ea47e0ce30925f88157b2de2e67855044
xile611 <327197681@qq.com>
2017-02-10 17:12:29 +0800

    fix: filter presentation attributes of LabelList entry

--- demo/component/RadialBarChart.js
+++ demo/component/RadialBarChart.js
@@ -54,13 +54,13 @@ export default class Demo extends Component {
         <p>RadialBarChart</p>
         <div className="radial-bar-chart-wrapper">
           <RadialBarChart width={500} height={300} cx={150} cy={150} innerRadius={20} outerRadius={140} barSize={10} data={data}>
-            <RadialBar minAngle={15} label={label} background dataKey="uv" >
+            <RadialBar minAngle={15} background dataKey="uv" >
               {
                 data.map((entry, index) => (
                   <Cell key={`cell-${index}`} fill={colors[index]}/>
                 ))
               }
-              <LabelList position="end" />
+              <LabelList position="insideEnd" fill="#fff" fontSize={10} />
             </RadialBar>
             <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
             <Tooltip/>
@@ -70,7 +70,7 @@ export default class Demo extends Component {
         <p>RadialBarChart with positive and negative value</p>
         <div className="radial-bar-chart-wrapper">
           <RadialBarChart width={500} height={300} cx={150} cy={150} innerRadius={20} outerRadius={140} data={data}>
-            <RadialBar startAngle={90} endAngle={-270} label={label} background dataKey="pv">
+            <RadialBar startAngle={90} endAngle={-270} background dataKey="pv">
               <LabelList position="end" />
             </RadialBar>
             <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
