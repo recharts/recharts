db601b8371c3daa146d7d9b061491a37b3e9fa3c
xile611 <327197681@qq.com>
2017-02-21 11:49:03 +0800

    feat: support customized horizontal line and vertical line in CartesianGrid, fix #512

--- src/cartesian/CartesianGrid.js
+++ src/cartesian/CartesianGrid.js
@@ -17,8 +17,12 @@ class CartesianGrid extends Component {
     y: PropTypes.number,
     width: PropTypes.number,
     height: PropTypes.number,
-    horizontal: PropTypes.bool,
-    vertical: PropTypes.bool,
+    horizontal: PropTypes.oneOfType([
+      PropTypes.object, PropTypes.element, PropTypes.func, PropTypes.bool,
+    ]),
+    vertical: PropTypes.oneOfType([
+      PropTypes.object, PropTypes.element, PropTypes.func, PropTypes.bool,
+    ]),
     horizontalPoints: PropTypes.arrayOf(PropTypes.number),
     verticalPoints: PropTypes.arrayOf(PropTypes.number),
     horizontalCoordinatesGenerator: PropTypes.func,
@@ -46,20 +50,55 @@ class CartesianGrid extends Component {
     fill: 'none',
   };
 
+  renderLineItem(option, props) {
+    let lineItem;
+
+    if (React.isValidElement(option)) {
+      lineItem = React.cloneElement(option, props);
+    } else if (_.isFunction(option)) {
+      lineItem = option(props);
+    } else {
+      const { x1, y1, x2, y2, key, fill, ...others } = props;
+
+      lineItem = (
+        <line
+          {...getPresentationAttributes(others)}
+          x1={x1}
+          y1={y1}
+          x2={x2}
+          y2={y2}
+          fill="none"
+          key={key}
+        />
+      );
+    }
+
+    return lineItem;
+  }
+
   /**
    * Draw the horizontal grid lines
    * @param {Array} horizontalPoints either passed in as props or generated from function
    * @return {Group} Horizontal lines
    */
   renderHorizontal(horizontalPoints) {
-    const { x, width } = this.props;
+    const { x, width, horizontal } = this.props;
 
     if (!horizontalPoints || !horizontalPoints.length) { return null; }
 
-    const props = getPresentationAttributes(this.props);
-    const items = horizontalPoints.map((entry, i) => (
-      <line {...props} fill="none" key={`line-${i}`} x1={x} y1={entry} x2={x + width} y2={entry} />
-    ));
+    const items = horizontalPoints.map((entry, i) => {
+      const props = {
+        ...this.props,
+        x1: x,
+        y1: entry,
+        x2: x + width,
+        y2: entry,
+        key: `line-${i}`,
+        index: i,
+      };
+
+      return this.renderLineItem(horizontal, props);
+    });
 
     return <g className="recharts-cartesian-grid-horizontal">{items}</g>;
   }
@@ -70,15 +109,23 @@ class CartesianGrid extends Component {
    * @return {Group} Vertical lines
    */
   renderVertical(verticalPoints) {
-    const { y, height } = this.props;
+    const { y, height, vertical } = this.props;
 
     if (!verticalPoints || !verticalPoints.length) { return null; }
 
-    const props = getPresentationAttributes(this.props);
+    const items = verticalPoints.map((entry, i) => {
+      const props = {
+        ...this.props,
+        x1: entry,
+        y1: y,
+        x2: entry,
+        y2: y + height,
+        key: `line-${i}`,
+        index: i,
+      };
 
-    const items = verticalPoints.map((entry, i) => (
-      <line {...props} fill="none" key={`line-${i}`} x1={entry} y1={y} x2={entry} y2={y + height} />
-    ));
+      return this.renderLineItem(vertical, props);
+    });
 
     return <g className="recharts-cartesian-grid-vertical">{items}</g>;
   }
