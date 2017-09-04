0c19e8a2d78da2daf582d962ad9ef31e3826faf4
Constantine Golub <constantinegolub@gmail.com>
2017-09-04 22:38:54 +0300

    add vertical/horizontal grid that alternate fill colors with opacity support

--- demo/component/CartesianGrid.js
+++ demo/component/CartesianGrid.js
@@ -8,16 +8,21 @@ export default class Demo extends Component {
   render () {
     let horizontalPoints = [10, 20, 30, 100, 400];
     let verticalPoints = [100, 200, 300, 400];
+    let verticalFill=['#5ab8ff', '#eee'];
 
     return (
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          width={500}
-          height={500}
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>
+      <div>
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            width={500}
+            height={500}
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+            verticalFill={verticalFill}
+            fillOpacityVertical={0.2}
+          />
+        </Surface>
+      </div>
     );
   }
 }
