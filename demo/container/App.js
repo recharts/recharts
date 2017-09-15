87dffdac3dcd047140b3e3898bd212ac5967ce63
Chaitanya Kumar Kamatham <kamthamc@users.noreply.github.com>
2017-09-15 15:48:19 -0700

    react router

--- demo/container/App.js
+++ demo/container/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import components from '../component/index';
 
--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import App from './container/App';
 
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-  </Router>
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App} />
+    </div>
+  </BrowserRouter>
 ), document.getElementById('root'));
