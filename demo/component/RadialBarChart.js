1d76ff32cf59f870f16cb74d1ab692f3e0176bf4
xile611 <327197681@qq.com>
2017-11-17 15:44:44 +0800

    demo: add demo of RadialBarChart

--- demo/component/RadialBarChart.js
+++ demo/component/RadialBarChart.js
@@ -7,13 +7,13 @@ import { scaleOrdinal, schemeCategory10 } from 'd3-scale';
 const colors = scaleOrdinal(schemeCategory10).range();
 
 const data = [
-  { name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8' },
-  { name: '25-29', uv: 26.69, pv: 4500, fill: '#83a6ed' },
-  { name: '30-34', uv: 15.69, pv: -1398, fill: '#8dd1e1' },
-  { name: '35-39', uv: 8.22, pv: 2800, fill: '#82ca9d' },
-  { name: '40-49', uv: 8.63, pv: 1908, fill: '#a4de6c' },
-  { name: '50+', uv: 2.63, pv: -2800, fill: '#d0ed57' },
-  { name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658' },
+  { name: '18-24', uv: 60, amt: 31.47, pv: 2400,  fill: '#8884d8' },
+  { name: '25-29', uv: 50, amt: 26.69, pv: 4500, fill: '#83a6ed' },
+  { name: '30-34', uv: 30, amt: 15.69, pv: -1398, fill: '#8dd1e1' },
+  { name: '35-39', uv: 59, amt: 8.22, pv: 2800, fill: '#82ca9d' },
+  { name: '40-49', uv: 48, amt: 8.63, pv: 1908, fill: '#a4de6c' },
+  { name: '50+', uv: 62, amt: 2.63, pv: -2800, fill: '#d0ed57' },
+  { name: 'unknow', uv: 38, amt: 6.67, pv: 4800, fill: '#ffc658' },
 ];
 
 const initialState = { data };
@@ -95,6 +95,24 @@ export default class Demo extends Component {
           </RadialBarChart>
         </div>
 
+        <p>RadialBarChart which has two RadialBar</p>
+        <div className="radial-bar-chart-wrapper">
+          <ResponsiveContainer>
+            <RadialBarChart
+              data={data}
+              cx="50%"
+              cy="50%"
+              innerRadius="20%"
+              outerRadius="90%"
+              startAngle={180}
+              endAngle={0}
+            >
+              <RadialBar minPointSize={15} background dataKey="uv" />
+              <RadialBar minPointSize={15} background dataKey="amt" />
+            </RadialBarChart>
+          </ResponsiveContainer>
+        </div>
+
         <p>RadialBarChart wrapped by ResponsiveContainer</p>
         <div className="radial-bar-chart-wrapper">
           <ResponsiveContainer>
@@ -106,7 +124,7 @@ export default class Demo extends Component {
               outerRadius="90%"
             >
               <RadialBar minPointSize={15} label={label} background dataKey="uv" />
-              <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
+
             </RadialBarChart>
           </ResponsiveContainer>
         </div>
@@ -120,8 +138,10 @@ export default class Demo extends Component {
               cy="50%"
               innerRadius="20%"
               outerRadius="90%"
+              startAngle={0}
+              endAngle={180}
             >
-              <PolarAngleAxis type="number" range={[0, 300]} />
+              <PolarAngleAxis type="number" domain={[0, 100]} />
               <RadialBar stackId="stack" minPointSize={15} background dataKey="uv">
                 {
                   data.map((entry, index) => (
