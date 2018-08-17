1d737f26b9285be186159544fcd870ab5e499065
Ian VanSchooten <ian.vanschooten@gmail.com>
2018-08-17 19:31:06 -0400

    Restore pointer events for tooltip cursor
    We need this in order to change to a magnifying glass cursor when 
    hovering over a section of a chart that can be `zoomed-in` on.

--- src/chart/generateCategoricalChart.js
+++ src/chart/generateCategoricalChart.js
@@ -649,6 +649,7 @@ const generateCategoricalChart = ({
       return {
         stroke: 'none',
         fill: '#ccc',
+        pointerEvents: 'all',
         x: layout === 'horizontal' ? activeCoordinate.x - halfSize : offset.left + 0.5,
         y: layout === 'horizontal' ? offset.top + 0.5 : activeCoordinate.y - halfSize,
         width: layout === 'horizontal' ? tooltipAxisBandSize : offset.width - 1,
