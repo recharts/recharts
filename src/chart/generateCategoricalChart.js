560d7aa78f39d06c40c001b7026e0d683192c908
Shy <puemos@gmail.com>
2018-10-16 19:25:40 +0200

    change: #1522 use lodash _.values instead of Object.values

--- src/chart/generateCategoricalChart.js
+++ src/chart/generateCategoricalChart.js
@@ -1255,7 +1255,7 @@ const generateCategoricalChart = ({
       const { xAxisMap, yAxisMap, offset } = this.state;
       const { width, height } = this.props;
       const xAxis = getAnyElementOfObject(xAxisMap);
-      const yAxisWithNiceTicks = Object.values(yAxisMap).find(axis => !_.isEmpty(axis.niceTicks));
+      const yAxisWithNiceTicks = _.values(yAxisMap).find(axis => !_.isEmpty(axis.niceTicks));
       const yAxis = yAxisWithNiceTicks || getAnyElementOfObject(yAxisMap);
       const props = element.props || {};
 
