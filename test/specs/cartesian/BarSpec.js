925cd088b240afc621393dc1a057343ff03e2a36
Jake Dluhy <jake.dluhy@producepay.com>
2018-11-02 13:03:49 -0700

    Fix bug preventing use of functions or custom components for the Bar background prop

--- test/specs/cartesian/BarSpec.js
+++ test/specs/cartesian/BarSpec.js
@@ -52,4 +52,50 @@ describe('<Bar />', () => {
 
     expect(wrapper.find('.recharts-bar-rectangle').length).to.equal(0);
   });
+
+  describe('With background', () => {
+    const composedDataWithBackground = [{
+      x: 10,
+      y: 50,
+      width: 20,
+      height: 20,
+      value: 40,
+      label: 'test',
+      background: { x: 10, y: 50, width: 20, height: 50 },
+    }, {
+      x: 50,
+      y: 50,
+      width: 20,
+      height: 50,
+      value: 100,
+      label: 'test',
+      background: { x: 50, y: 50, width: 20, height: 50 },
+    }];
+
+    it('Will create a background Rectangle with the passed in props', () => {
+      const wrapper = render(
+        <Surface width={500} height={500}>
+          <Bar data={composedDataWithBackground} background={{ fill: '#000' }} />
+        </Surface>
+      );
+
+      expect(wrapper.find('.recharts-bar-background-rectangle').length).to.equal(2);
+    });
+
+    it('Will accept a function for the background prop', () => {
+      const wrapper = render(
+        <Surface width={500} height={500}>
+          <Bar
+            data={composedDataWithBackground}
+            background={({ index }) => {
+              return (index === 0) ? <div className='test-custom-background' /> : null;
+            }}
+          />
+        </Surface>
+      );
+
+      expect(wrapper.find('.recharts-bar-background-rectangle').length).to.equal(0);
+      expect(wrapper.find('.test-custom-background').length).to.equal(1);
+    });
+  });
 });
