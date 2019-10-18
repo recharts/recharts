5c65744bcfd9302bd70f41750a03c67f35259af3
sainthkh <sainthkh@naver.com>
2019-10-18 17:45:15 +0900

    Applied new type system and removed presentation type.

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,1 +1,4 @@
+import { AriaAttributes, DOMAttributes, SVGProps } from 'react';
+
 export type LayoutType = 'horizontal' | 'vertical';
+export type PresentationAttributes<T> = AriaAttributes & DOMAttributes<T> & SVGProps<T>;
