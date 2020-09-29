bfcb09d1de8d5a64cc78cf38696e3c5c8e9d77e1
Emily Marigold Klassen <forivall@users.noreply.github.com>
2020-09-28 19:37:24 -0700

    fix: defer when syncing to other charts

--- src/chart/generateCategoricalChart.tsx
+++ src/chart/generateCategoricalChart.tsx
@@ -1271,50 +1271,12 @@ export const generateCategoricalChart = ({
     };
 
     handleReceiveSyncEvent = (cId: number | string, chartId: string, data: CategoricalChartState) => {
-      const { syncId, layout } = this.props;
-      const { updateId } = this.state;
+      const { syncId } = this.props;
 
       if (syncId === cId && chartId !== this.uniqueChartId) {
-        const { dataStartIndex, dataEndIndex } = data;
-
-        if (!_.isNil(data.dataStartIndex) || !_.isNil(data.dataEndIndex)) {
-          this.setState({
-            dataStartIndex,
-            dataEndIndex,
-            ...updateStateOfAxisMapsOffsetAndStackGroups(
-              {
-                props: this.props,
-                dataStartIndex,
-                dataEndIndex,
-                updateId,
-              },
-              this.state,
-            ),
-          });
-        } else if (!_.isNil(data.activeTooltipIndex)) {
-          const { chartX, chartY, activeTooltipIndex } = data;
-          const { offset, tooltipTicks } = this.state;
-          if (!offset) {
-            return;
-          }
-          const viewBox: CartesianViewBox = { ...offset, x: offset.left, y: offset.top };
-          // When a categotical chart is combined with another chart, the value of chartX
-          // and chartY may beyond the boundaries.
-          const validateChartX = Math.min(chartX, viewBox.x + viewBox.width);
-          const validateChartY = Math.min(chartY, viewBox.y + viewBox.height);
-          const activeLabel = tooltipTicks[activeTooltipIndex] && tooltipTicks[activeTooltipIndex].value;
-          const activePayload: any = getTooltipContent(this.state, this.props.data, activeTooltipIndex);
-          const activeCoordinate = tooltipTicks[activeTooltipIndex]
-            ? {
-                x: layout === 'horizontal' ? tooltipTicks[activeTooltipIndex].coordinate : validateChartX,
-                y: layout === 'horizontal' ? validateChartY : tooltipTicks[activeTooltipIndex].coordinate,
-              }
-            : originCoordinate;
-
-          this.setState({ ...data, activeLabel, activeCoordinate, activePayload });
-        } else {
-          this.setState(data);
-        }
+        (window.requestAnimationFrame || window.setImmediate || window.setTimeout)(
+          this.applySyncEvent.bind(this, data),
+        );
       }
     };
 
@@ -1508,6 +1470,51 @@ export const generateCategoricalChart = ({
 
       if (!_.isNil(syncId)) {
         eventCenter.emit(SYNC_EVENT, syncId, this.uniqueChartId, data);
+      }
+    }
+
+    applySyncEvent(data: CategoricalChartState) {
+      const { layout } = this.props;
+      const { updateId } = this.state;
+      const { dataStartIndex, dataEndIndex } = data;
+
+      if (!_.isNil(data.dataStartIndex) || !_.isNil(data.dataEndIndex)) {
+        this.setState({
+          dataStartIndex,
+          dataEndIndex,
+          ...updateStateOfAxisMapsOffsetAndStackGroups(
+            {
+              props: this.props,
+              dataStartIndex,
+              dataEndIndex,
+              updateId,
+            },
+            this.state,
+          ),
+        });
+      } else if (!_.isNil(data.activeTooltipIndex)) {
+        const { chartX, chartY, activeTooltipIndex } = data;
+        const { offset, tooltipTicks } = this.state;
+        if (!offset) {
+          return;
+        }
+        const viewBox: CartesianViewBox = { ...offset, x: offset.left, y: offset.top };
+        // When a categotical chart is combined with another chart, the value of chartX
+        // and chartY may beyond the boundaries.
+        const validateChartX = Math.min(chartX, viewBox.x + viewBox.width);
+        const validateChartY = Math.min(chartY, viewBox.y + viewBox.height);
+        const activeLabel = tooltipTicks[activeTooltipIndex] && tooltipTicks[activeTooltipIndex].value;
+        const activePayload: any = getTooltipContent(this.state, this.props.data, activeTooltipIndex);
+        const activeCoordinate = tooltipTicks[activeTooltipIndex]
+          ? {
+              x: layout === 'horizontal' ? tooltipTicks[activeTooltipIndex].coordinate : validateChartX,
+              y: layout === 'horizontal' ? validateChartY : tooltipTicks[activeTooltipIndex].coordinate,
+            }
+          : originCoordinate;
+
+        this.setState({ ...data, activeLabel, activeCoordinate, activePayload });
+      } else {
+        this.setState(data);
       }
     }
 
