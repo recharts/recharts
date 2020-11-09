30a2caf67f6c74fceac062ed080d5948303134be
Avindra Goolcharan <aavindraa@gmail.com>
2020-11-09 17:11:07 -0500

    Replace core-js polyfill
     * Math.sign was already ported to mathSign
     * Use MDN polyfill for Number.isFinite
    
    Also
    
     * Remove babel-polyfill, not in use

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,2 +1,6 @@
-import 'core-js/es/math';
-import 'core-js/es/number';
+// Polyfill isFinite because IE does
+// not support it.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isFinite#Polyfill
+if (Number.isFinite === undefined) Number.isFinite = function(value) {
+  return typeof value === 'number' && isFinite(value);
+}
