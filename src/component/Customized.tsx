66e9b7191c0151a951c58dbccbe327e21b59effd
Lucas Barboza <noisae@gmail.com>
2021-02-05 09:22:43 +0100

    Fix type on Customized Component
    In order to receive a ReactElement as the implementation expects, we need to add the type that enables that.

--- src/component/Customized.tsx
+++ src/component/Customized.tsx
@@ -6,7 +6,7 @@ import _ from 'lodash';
 import { Layer } from '../container/Layer';
 import { warn } from '../util/LogUtils';
 
-type Comp<P> = FunctionComponent<P> | Component<P>;
+type Comp<P> = FunctionComponent<P> | Component<P> | ReactElement<P>;
 export type Props<P, C extends Comp<P>> = P & {
   component: C;
 };
