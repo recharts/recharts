306089f344386d828dbcae4cf12f68c83717b586
Nikolas Rieble <nikolas.rieble@tulip.co>
2022-12-18 16:58:12 +0100

    AreaChart: Fix linter warnings

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /* eslint-env node */
 module.exports = {
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx'],
   roots: ['<rootDir>/test-jest'],
   maxWorkers: '50%',
   setupFilesAfterEnv: ['<rootDir>/test-jest/setupTests.ts'],
