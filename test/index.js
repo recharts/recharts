161ebacc7fcb826b1137b54ae82dc696b738faec
JM <proke03@naver.com>
2023-01-20 15:02:53 +0900

    Delete CartesianGridSpec.js

--- test/index.js
+++ test/index.js
@@ -15,7 +15,6 @@ require('./specs/cartesian/LineSpec');
 require('./specs/cartesian/ScatterSpec');
 require('./specs/cartesian/BrushSpec');
 require('./specs/cartesian/CartesianAxisSpec');
-require('./specs/cartesian/CartesianGridSpec');
 require('./specs/cartesian/ReferenceLineSpec');
 require('./specs/cartesian/ReferenceDotSpec');
 require('./specs/cartesian/ReferenceAreaSpec');
