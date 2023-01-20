7dacd1a00333000a8ec9dcc1613b3d8c0810e280
Jang Min <jangmin.dev@gmail.com>
2023-01-21 01:10:37 +0900

    test(JestMigration): convert CartesianGridSpec (#3199)

--- test-jest/cartesian/CartesianGrid.spec.tsx
+++ test-jest/cartesian/CartesianGrid.spec.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { expect } from 'chai';
-import { Surface, CartesianGrid } from 'recharts';
-import { render } from 'enzyme';
+import { render } from '@testing-library/react';
+import { Surface, CartesianGrid } from '../../src';
 
 describe('<CartesianGrid />', () => {
   const horizontalPoints = [10, 20, 30, 100, 400];
   const verticalPoints = [100, 200, 300, 400];
 
-  it('Render 5 horizontal lines and 4 vertical lines in simple CartesianGrid', () => {
-    const wrapper = render(
+  test('Render 5 horizontal lines and 4 vertical lines in simple CartesianGrid', () => {
+    const { container } = render(
       <Surface width={500} height={500}>
         <CartesianGrid
           x={0}
@@ -20,24 +19,24 @@ describe('<CartesianGrid />', () => {
         />
       </Surface>,
     );
-    expect(wrapper.find('line').length).to.equal(9);
+    expect(container.querySelectorAll('line')).toHaveLength(9);
   });
 
-  it("Don't render any lines when verticalPoints and horizontalPoints are empty", () => {
-    const wrapper = render(
+  test("Don't render any lines when verticalPoints and horizontalPoints are empty", () => {
+    const { container } = render(
       <Surface width={500} height={500}>
         <CartesianGrid width={500} height={500} />
       </Surface>,
     );
-    expect(wrapper.find('line').length).to.equal(0);
+    expect(container.querySelectorAll('line')).toHaveLength(0);
   });
 
-  it("Don't render any lines when width or height is smaller than 0", () => {
-    const wrapper = render(
+  test("Don't render any lines when width or height is smaller than 0", () => {
+    const { container } = render(
       <Surface width={500} height={500}>
         <CartesianGrid width={0} height={500} />
       </Surface>,
     );
-    expect(wrapper.find('line').length).to.equal(0);
+    expect(container.querySelectorAll('line')).toHaveLength(0);
   });
 });
