ff1163ef45c4f6c82f7ae6c76eb794d6c6d79338
JM <proke03@naver.com>
2023-01-21 13:32:36 +0900

    Convert RadarChartSpec.js -> RadarChart.spec.tsx

---
+++ test-jest/component/Customized.spec.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+import { Customized } from '../../src';
+
+describe('<Customized />', () => {
+  test('Render customized component by React.element', () => {
+    // eslint-disable-next-line react/prefer-stateless-function
+    class SvgElement extends Component {
+      render() {
+        return <rect className="customized-svg-element" />;
+      }
+    }
+    const { container } = render(<Customized component={<SvgElement />} />);
+    expect(container.querySelectorAll('.customized-svg-element')).toHaveLength(1);
+  });
+
+  test('Render customized component by Function', () => {
+    function rect() {
+      return <rect className="customized-svg-element" />;
+    }
+    // eslint-disable-next-line react/jsx-no-bind
+    const { container } = render(<Customized component={rect} />);
+    expect(container.querySelectorAll('.customized-svg-element')).toHaveLength(1);
+  });
+});
