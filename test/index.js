c7d0b7813e14b510b461ac5d4969ee1531c95370
JM <proke03@naver.com>
2023-01-21 10:58:11 +0900

    Convert ReferenceAreaSpec.js -> ReferenceArea.spec.tsx

--- test/index.js
+++ test/index.js
@@ -16,7 +16,6 @@ require('./specs/cartesian/BrushSpec');
 require('./specs/cartesian/CartesianAxisSpec');
 require('./specs/cartesian/ReferenceLineSpec');
 require('./specs/cartesian/ReferenceDotSpec');
-require('./specs/cartesian/ReferenceAreaSpec');
 
 // polar system chart component
 require('./specs/polar/PolarRadiusAxisSpec');
