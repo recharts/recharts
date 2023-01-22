518f72402997a99e6bb2238466d3915343068d52
JM <proke03@naver.com>
2023-01-22 10:42:49 +0900

    Convert PolarAngleAxisSpec.js -> PolarAngleAxis.spec.tsx

--- test/index.js
+++ test/index.js
@@ -20,7 +20,6 @@ require('./specs/cartesian/ReferenceAreaSpec');
 
 // polar system chart component
 require('./specs/polar/PolarRadiusAxisSpec');
-require('./specs/polar/PolarAngleAxisSpec');
 require('./specs/polar/PolarGridSpec');
 require('./specs/polar/PieSpec');
 require('./specs/polar/RadialBarSpec');
