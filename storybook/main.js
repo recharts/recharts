ff96800333cad4c8ed79d972e9184569747dce70
JM <proke03@naver.com>
2023-01-28 17:10:16 +0900

    Remove stories file extension(js/jsx)

--- storybook/main.js
+++ storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   core: {
