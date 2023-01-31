cd76d9f0c977579fafbf2fd6d321d9424bd32b1d
JM <proke03@naver.com>
2023-01-31 10:33:26 +0900

    Migrate csf2 -> csf3

--- src/stories/cartesian/brush/Brush.stories.tsx
+++ src/stories/cartesian/brush/Brush.stories.tsx
@@ -1,83 +1,84 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Meta } from '@storybook/react';
 import { dateData } from '../../data';
 import { Surface, Brush } from '../../..';
 
-export default {
+export const SimpleBrush = {
   title: 'Recharts/Brush',
   component: Brush,
-} as ComponentMeta<typeof Brush>;
+} as Meta<typeof Brush>;
 
-const Template: ComponentStory<typeof Brush> = args => {
-  const [simple, setSimple] = React.useState({
-    startIndex: 0,
-    endIndex: args.data.length - 1,
-  });
+export const Simple = {
+  render: (args: any) => {
+    const [simple, setSimple] = React.useState({
+      startIndex: 0,
+      endIndex: args.data.length - 1,
+    });
 
-  const [gap, setGap] = React.useState({
-    startIndex: 0,
-    endIndex: args.data.length - 1,
-  });
+    const [gap, setGap] = React.useState({
+      startIndex: 0,
+      endIndex: args.data.length - 1,
+    });
 
-  const handleChange = (res: any) => {
-    setSimple(res);
-  };
+    const handleChange = (res: any) => {
+      setSimple(res);
+    };
 
-  const handleGapChange = (res: any) => {
-    setGap(res);
-  };
+    const handleGapChange = (res: any) => {
+      setGap(res);
+    };
 
-  // eslint-disable-next-line no-shadow
-  const renderTraveller = (props: { x: number; y: number; width: number; height: number }) => {
-    const { x, y, width, height } = props;
+    // eslint-disable-next-line no-shadow
+    const renderTraveller = (props: { x: number; y: number; width: number; height: number }) => {
+      const { x, y, width, height } = props;
+
+      return (
+        <path
+          d={`M${x + width / 2},${y}L${x + width},${y + height / 2}L${x + width / 2},${y + height}L${x},${
+            y + height / 2
+          }Z`}
+          fill="red"
+          stroke="none"
+        />
+      );
+    };
 
     return (
-      <path
-        d={`M${x + width / 2},${y}L${x + width},${y + height / 2}L${x + width / 2},${y + height}L${x},${
-          y + height / 2
-        }Z`}
-        fill="red"
-        stroke="none"
-      />
+      <div>
+        <p>Simple Brush</p>
+        <Surface width={800} height={200}>
+          <Brush
+            startIndex={simple.startIndex}
+            endIndex={simple.endIndex}
+            x={100}
+            y={50}
+            width={400}
+            height={40}
+            data={args.data}
+            onChange={handleChange}
+            traveller={renderTraveller}
+          />
+        </Surface>
+        <p>Brush has specified gap</p>
+        <Surface width={800} height={200}>
+          <Brush
+            startIndex={gap.startIndex}
+            endIndex={gap.endIndex}
+            x={100}
+            y={50}
+            width={400}
+            height={40}
+            data={args.data}
+            gap={5}
+            onChange={handleGapChange}
+          />
+        </Surface>
+      </div>
     );
-  };
+  },
 
-  return (
-    <div>
-      <p>Simple Brush</p>
-      <Surface width={800} height={200}>
-        <Brush
-          startIndex={simple.startIndex}
-          endIndex={simple.endIndex}
-          x={100}
-          y={50}
-          width={400}
-          height={40}
-          data={args.data}
-          onChange={handleChange}
-          traveller={renderTraveller}
-        />
-      </Surface>
-      <p>Brush has specified gap</p>
-      <Surface width={800} height={200}>
-        <Brush
-          startIndex={gap.startIndex}
-          endIndex={gap.endIndex}
-          x={100}
-          y={50}
-          width={400}
-          height={40}
-          data={args.data}
-          gap={5}
-          onChange={handleGapChange}
-        />
-      </Surface>
-    </div>
-  );
-};
-
-export const Simple = Template.bind({});
-Simple.args = {
-  data: dateData,
+  args: {
+    data: dateData,
+  },
 };
