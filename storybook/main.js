ac36741364c0855cbb0c61828ad79246a19b0a02
Coltin Kifer <ckifer@amazon.com>
2023-02-01 11:15:13 -0800

    chore: upgrade storybook, fix lockfile

--- storybook/main.js
+++ storybook/main.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   stories: ['./stories/**/*.stories.mdx', './stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
