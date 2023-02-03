fe5f1ba04e9448e24f817eddb6c2bec5db9153ac
Jang Min <proke03@naver.com>
2023-02-03 10:26:50 +0900

    docs(storybook): add areachart.stories.tsx (#3308)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [x] All new and existing tests passed.
    
    ---------
    
    Co-authored-by: Nikolas Rieble <nikolas-rieble@protonmail.com>

---
+++ storybook/stories/chart/areachart/AreaChart.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip } from '../../../../src';
+import { dateData, TimeSeriesData } from '../../data';
+
+export default {
+  title: 'Recharts/Chart/Area Chart',
+  component: AreaChart,
+} as ComponentMeta<typeof AreaChart>;
+
+const data: TimeSeriesData[] = [];
+dateData.forEach((date: string) => {
+  data.push({
+    date,
+    value: 1 + Math.random(),
+  });
+});
+
+export const Simple = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <AreaChart data={args.data}>
+          <Area dataKey="value" stroke="#2451B7" fill="url(#color)" />
+          <Tooltip />
+          <CartesianGrid opacity={0.1} vertical={false} />
+        </AreaChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data,
+  },
+};
