2732d71809a866338750fe64396a0e86d49c7b9e
Jang Min <jangmin.dev@gmail.com>
2023-02-05 19:49:38 +0900

    Update storybook/stories/chart/barchart/BarChart.stories.tsx
    Co-authored-by: Nikolas Rieble <nikolas-rieble@protonmail.com>

--- storybook/stories/chart/barchart/BarChart.stories.tsx
+++ storybook/stories/chart/barchart/BarChart.stories.tsx
@@ -11,8 +11,8 @@ export const Simple = {
   render: (args: Record<string, any>) => {
     return (
       <ResponsiveContainer width="100%" height={400}>
-        <BarChart width={150} height={40} data={args.data}>
-          <Bar dataKey="uv" fill="#ff7300" background />
+        <BarChart {...args}>
+          <Bar dataKey="uv" />
         </BarChart>
       </ResponsiveContainer>
     );
