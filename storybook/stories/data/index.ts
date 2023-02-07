9a2217ed5f274187e4e323fbc06e2e0d83a8e5aa
Jang Min <proke03@naver.com>
2023-02-07 11:55:50 +0900

    docs(storybook): treemap (#3335)

--- storybook/stories/data/index.ts
+++ storybook/stories/data/index.ts
@@ -2,5 +2,6 @@ export * from './Coordinate';
 export * from './DataProps';
 export * from './NodeAndLink';
 export * from './Page';
+export * from './Size';
 export * from './Tick';
 export * from './Time';
