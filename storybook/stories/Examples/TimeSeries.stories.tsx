8cbd50a9700443002b910bf4b2bdc8a6da9d9fd0
Nikolas Rieble <nikolas.rieble@tulip.co>
2023-02-13 19:21:39 +0100

    Storybook: Create an example for time series data on the XAxis with d3 Scale (#3360)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    The default time series behaviour of recharts is, well, not too great.
    This PR adds two stories to Recharts - One for the default time series
    behaviour of recharts, and a second one which uses d3 scale and
    multiFormat for nicely spaced and formatted xAxis ticks.
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    https://github.com/recharts/recharts/issues/956
    https://github.com/recharts/recharts/issues/3052
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    Default behaviour
    <img width="1132" alt="Screenshot 2023-02-13 at 14 58 42"
    src="https://user-images.githubusercontent.com/27995606/218477309-31b69dad-6c81-4439-8e88-5f59f2c6d115.png">
    
    vs D3 scale
    <img width="1132" alt="Screenshot 2023-02-13 at 14 34 52"
    src="https://user-images.githubusercontent.com/27995606/218477235-4e47ff1d-b73e-4c10-b381-f92317bdf307.png">
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [x] All new and existing tests passed.

---
+++ storybook/stories/Examples/TimeSeries.stories.tsx
@@ -0,0 +1,120 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-nested-ternary */
+import React from 'react';
+import { scaleTime } from 'd3-scale';
+import { timeFormat } from 'd3-time-format';
+import { timeDay, timeHour, timeMinute, timeMonth, timeSecond, timeWeek, timeYear } from 'd3-time';
+import { timeData } from '../data';
+import { ComposedChart, Line, ResponsiveContainer, XAxis } from '../../../src';
+
+export default {
+  component: XAxis,
+};
+
+const StoryTemplate = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <ComposedChart
+          data={timeData}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <XAxis dataKey="x" {...args} domain={['auto', 'auto']} />
+          <Line dataKey="y" />
+        </ComposedChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data: timeData,
+  },
+};
+
+export const DefaultBehaviour = {
+  ...StoryTemplate,
+  parameters: { controls: { include: ['type', 'scale', 'domain', 'data'] } },
+  argTypes: {
+    scale: {
+      options: [undefined, 'auto', 'ordinal', 'time', 'point', 'linear'],
+      control: { type: 'radio' },
+    },
+    type: {
+      options: [undefined, 'category', 'number'],
+      control: { type: 'radio' },
+    },
+  },
+};
+
+// https://github.com/d3/d3-time-format
+const formatMillisecond = timeFormat('.%L'),
+  formatSecond = timeFormat(':%S'),
+  formatMinute = timeFormat('%I:%M'),
+  formatHour = timeFormat('%I %p'),
+  formatDay = timeFormat('%a %d'),
+  formatWeek = timeFormat('%b %d'),
+  formatMonth = timeFormat('%B'),
+  formatYear = timeFormat('%Y');
+
+function multiFormat(date: Date) {
+  return (
+    timeSecond(date) < date
+      ? formatMillisecond
+      : timeMinute(date) < date
+      ? formatSecond
+      : timeHour(date) < date
+      ? formatMinute
+      : timeDay(date) < date
+      ? formatHour
+      : timeMonth(date) < date
+      ? timeWeek(date) < date
+        ? formatDay
+        : formatWeek
+      : timeYear(date) < date
+      ? formatMonth
+      : formatYear
+  )(date);
+}
+
+export const WithD3Scale = {
+  ...StoryTemplate,
+  render: args => {
+    const timeValues = args.data.map(row => row.x);
+    // The d3 scaleTime domain requires numeric values
+    const numericValues = timeValues.map(time => time.valueOf());
+    // With .nice() we extend the domain nicely.
+    const timeScale = scaleTime()
+      .domain([Math.min(...numericValues), Math.max(...numericValues)])
+      .nice();
+
+    const xAxisArgs = {
+      domain: timeScale.domain().map(date => date.valueOf()),
+      scale: timeScale,
+      xAxisType: 'number' as const,
+      ticks: timeScale.ticks(5) as any,
+      tickFormatter: multiFormat,
+    };
+
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <ComposedChart
+          data={timeData}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <XAxis dataKey="x" {...args} {...xAxisArgs} />
+          <Line dataKey="y" />
+        </ComposedChart>
+      </ResponsiveContainer>
+    );
+  },
+  parameters: { controls: { include: ['data'] } },
+};
