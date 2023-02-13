49ba6aa3602192fc9187e8f27997bf1ce6731f2e
Coltin Kifer <coltinkifer19@gmail.com>
2023-02-13 09:57:02 -0800

    storybook: fix needle, add boxplot story (#3357)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    Add the boxplot example to storybook
    
    ## Related Issue
    https://github.com/recharts/recharts/issues/369
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    * Add box plot example
    
    * Add some length to the needle in pie with needle
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ![image](https://user-images.githubusercontent.com/25180830/218228014-bf0b168b-5460-4343-882f-4619c8b4551b.png)
    
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [x] All new and existing tests passed.
    
    ---------
    
    Co-authored-by: Coltin Kifer <ckifer@amazon.com>

--- storybook/stories/Examples/Pie/PieWithNeedle.stories.tsx
+++ storybook/stories/Examples/Pie/PieWithNeedle.stories.tsx
@@ -8,11 +8,10 @@ export default {
 
 const RADIAN = Math.PI / 180;
 const data = [
-  { name: 'A', val: 80, color: '#ff0000' },
-  { name: 'B', val: 45, color: '#00ff00' },
-  { name: 'C', val: 25, color: '#0000ff' },
+  { name: 'A', value: 80, color: '#ff0000' },
+  { name: 'B', value: 45, color: '#00ff00' },
+  { name: 'C', value: 25, color: '#0000ff' },
 ];
-
 const cx = 150;
 const cy = 200;
 const iR = 50;
@@ -21,28 +20,23 @@ const value = 50;
 
 const needle = (value: number, data: any[], cx: number, cy: number, iR: number, oR: number, color: string) => {
   let total = 0;
-  data.forEach((v: { val: number }) => {
-    total += v.val;
+  data.forEach(v => {
+    total += v.value;
   });
-
   const ang = 180.0 * (1 - value / total);
   const length = (iR + 2 * oR) / 3;
   const sin = Math.sin(-RADIAN * ang);
   const cos = Math.cos(-RADIAN * ang);
-
   const r = 5;
   const x0 = cx + 5;
   const y0 = cy + 5;
-  const xba = x0 + r * sin;
-  const yba = y0 - r * cos;
   const xbb = x0 - r * sin;
   const ybb = y0 + r * cos;
   const xp = x0 + length * cos;
-  const yp = y0 + length * sin;
 
   return [
     <circle cx={x0} cy={y0} r={r} fill={color} stroke="none" />,
-    <path d={`M${xba},${yba}L${xbb},${ybb},L${xp},${yp},L${xba},${yba}`} stroke="#none" fill={color} />,
+    <path d={`M${x0},${y0}L${xbb + 65},${ybb - 65},L${xp}`} strokeWidth={2} stroke={color} fill={color} />,
   ];
 };
 
@@ -50,27 +44,25 @@ export const PieWithNeedle = {
   render: (_args: Record<string, any>) => {
     return (
       <ResponsiveContainer width="100%" height={500}>
-        <div style={{ width: '300px', height: '300px', display: 'flex', margin: 'auto' }}>
-          <PieChart width={300} height={300}>
-            <Pie
-              dataKey="val"
-              startAngle={180}
-              endAngle={0}
-              data={data}
-              cx={cx}
-              cy={cy}
-              innerRadius={iR}
-              outerRadius={oR}
-              fill="#8884d8"
-              stroke="none"
-            >
-              {data.map(entry => (
-                <Cell key={entry.name} fill={entry.color} />
-              ))}
-            </Pie>
-            {needle(value, data, cx, cy, iR, oR, '#d0d000')}
-          </PieChart>
-        </div>
+        <PieChart>
+          <Pie
+            dataKey="value"
+            startAngle={180}
+            endAngle={0}
+            data={data}
+            cx={cx}
+            cy={cy}
+            innerRadius={iR}
+            outerRadius={oR}
+            fill="#8884d8"
+            stroke="none"
+          >
+            {data.map(entry => (
+              <Cell key={`cell-${entry.name}`} fill={entry.color} />
+            ))}
+          </Pie>
+          {needle(value, data, cx, cy, iR, oR, '#d0d000')}
+        </PieChart>
       </ResponsiveContainer>
     );
   },
--- storybook/stories/data/DataProps.ts
+++ storybook/stories/data/DataProps.ts
@@ -1,3 +1,24 @@
 export type TimeSeriesData = { date: string; value: number };
 
 export type CategoricalData = { category: string; value: number };
+
+// Used in stacked bar graph
+export type BoxPlotData = {
+  min: number;
+  bottomWhisker: number;
+  bottomBox: number;
+  topBox: number;
+  topWhisker: number;
+  average?: number;
+  size: number; // for average dot size
+};
+
+// Original data of boxplot graph
+export type BoxPlot = {
+  min: number;
+  lowerQuartile: number;
+  median: number;
+  upperQuartile: number;
+  max: number;
+  average?: number;
+};
