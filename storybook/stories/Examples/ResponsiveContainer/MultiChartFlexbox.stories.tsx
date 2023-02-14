c502ce1458a4b15dd08f7ae59b1392a5b231dbc8
Coltin Kifer <ckifer@amazon.com>
2023-02-14 10:24:11 -0800

    storybook: default to light preview, add multi chart container example

---
+++ storybook/stories/Examples/ResponsiveContainer/MultiChartFlexbox.stories.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useTheme } from '@storybook/theming';
+import {
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
+  AreaChart,
+  LineChart,
+  Line,
+} from '../../../../src';
+import { pageData } from '../../data';
+import './style.css';
+
+export default {
+  component: AreaChart,
+};
+
+export const MultiChartFlexbox = {
+  render: (_args: Record<string, any>) => {
+    const theme = useTheme();
+    console.log(theme);
+
+    return (
+      <>
+        <p style={{ color: theme?.color?.defaultText }}>Resize the window to </p>
+        <div className="flex-parent">
+          <ResponsiveContainer width="100%" className="flex-child">
+            <AreaChart
+              data={pageData}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
+          </ResponsiveContainer>
+          <ResponsiveContainer className="flex-child">
+            <LineChart
+              data={pageData}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Line type="monotone" dataKey="amt" stroke="orange" />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </>
+    );
+  },
+  args: {},
+};
