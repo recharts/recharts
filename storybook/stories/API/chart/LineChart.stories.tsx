fdeb1b2f7c55806f28313816850f74e39fa81456
YilunSun <osirisnero@gmail.com>
2023-02-14 11:21:20 +0800

    feat: test interaction using the play function
    https://github.com/recharts/recharts/issues/3299

--- storybook/stories/API/chart/LineChart.stories.tsx
+++ storybook/stories/API/chart/LineChart.stories.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react';
-import { pageData } from '../../data';
-import { Line, LineChart, ResponsiveContainer } from '../../../../src';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { fireEvent } from '@storybook/testing-library';
+import { numberData, pageData } from '../../data';
+import { Brush, Line, LineChart, ResponsiveContainer } from '../../../../src';
 
 export default {
   component: LineChart,
@@ -18,5 +21,30 @@ export const Simple = {
   },
   args: {
     data: pageData,
+  },
+};
+
+export const PlayMoveBrush = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart {...args}>
+          <Line dataKey="uv" />
+          <Brush />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data: numberData,
+  },
+  play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+    setTimeout(() => {
+      const leftBrushSlide = canvasElement.querySelector('.recharts-brush-traveller');
+
+      fireEvent.mouseDown(leftBrushSlide);
+      fireEvent.mouseMove(leftBrushSlide, { clientX: 200 });
+      fireEvent.mouseUp(leftBrushSlide);
+    }, 0);
   },
 };
