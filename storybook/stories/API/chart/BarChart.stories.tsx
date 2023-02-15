8c5e89a384095246eb20cfd86bb4833f58628e7c
Nikolas Rieble <nikolas.rieble@tulip.co>
2023-02-15 03:52:23 +0100

    Storybook: Add BarInBar story (#3371)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    Answering a request on SO
    https://stackoverflow.com/questions/75401714/is-it-possible-to-stack-2-bars-layer-on-top-of-each-other-not-above-each-other
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    <img width="1489" alt="Screenshot 2023-02-14 at 22 42 17"
    src="https://user-images.githubusercontent.com/27995606/218869699-68a7538e-bda0-4c38-921a-492dce57a4f6.png">
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [x] All new and existing tests passed.

--- storybook/stories/API/chart/BarChart.stories.tsx
+++ storybook/stories/API/chart/BarChart.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bar, BarChart, ResponsiveContainer } from '../../../../src';
+import { Bar, BarChart, ResponsiveContainer, XAxis } from '../../../../src';
 import { pageData } from '../../data';
 
 export default {
@@ -12,6 +12,25 @@ export const Simple = {
       <ResponsiveContainer width="100%" height={400}>
         <BarChart {...args}>
           <Bar dataKey="uv" />
+        </BarChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data: pageData,
+  },
+};
+
+export const BarInBar = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart {...args}>
+          <Bar dataKey="uv" fill="green" xAxisId="one" barSize={50} />
+          <XAxis xAxisId="one" />
+          {/* The smaller bar must be rendered in front of the larger one to be visible. */}
+          <Bar dataKey="pv" fill="red" xAxisId="two" barSize={30} />
+          <XAxis xAxisId="two" hide />
         </BarChart>
       </ResponsiveContainer>
     );
