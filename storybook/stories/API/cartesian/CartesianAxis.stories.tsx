c6e5e24047434d2a997f83072ac62afa9b516207
nikolasrieble <nikolas.rieble@tulip.co>
2023-02-21 15:09:43 +0100

    Storybook: Add story about tick positioning for CartesianAxis

--- storybook/stories/API/cartesian/CartesianAxis.stories.tsx
+++ storybook/stories/API/cartesian/CartesianAxis.stories.tsx
@@ -4,11 +4,67 @@ import { ticks } from '../../data';
 
 export default {
   component: CartesianAxis,
+  argTypes: {
+    interval: {
+      options: ['preserveStart', 'preserveStartEnd', 'preserveEnd', 0, 1, 2, 3, 4, 5],
+      control: {
+        type: 'select',
+      },
+    },
+  },
 };
 
 const [surfaceWidth, surfaceHeight] = [500, 500];
 
-export const Simple = {
+export const TickPositioning = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={surfaceHeight}>
+        <Surface
+          width={surfaceWidth}
+          height={surfaceHeight}
+          viewBox={{
+            x: 0,
+            y: 0,
+            width: surfaceWidth,
+            height: surfaceHeight,
+          }}
+        >
+          <CartesianAxis
+            y={50}
+            width={surfaceHeight}
+            height={400}
+            viewBox={{
+              x: 0,
+              y: 0,
+              width: surfaceHeight,
+              height: surfaceHeight,
+            }}
+            ticks={args.data}
+            interval={args.interval}
+          />
+        </Surface>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data: ticks,
+  },
+  parameters: {
+    controls: { include: ['interval'] },
+    docs: {
+      description: {
+        story:
+          'The ticks of a cartesian axis are positioned using the interval field. ' +
+          'The interval computation can be overriden by setting interval to a number, ' +
+          'which signifies how many ticks should not be shown between two visible ticks. ' +
+          'By default, ticks that overflow their assigned space, are not shown. ',
+      },
+    },
+  },
+};
+
+export const Combined = {
   render: (args: Record<string, any>) => {
     return (
       <ResponsiveContainer width="100%" height={surfaceHeight}>
