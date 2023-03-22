5a4d29bcb61965facfbdad301a69b69b4250f035
akamfoad <akamfanaye@gmail.com>
2023-03-22 17:05:04 +0300

    refactor: Trapezoid to function component

--- src/shape/Trapezoid.tsx
+++ src/shape/Trapezoid.tsx
@@ -1,13 +1,13 @@
 /**
  * @fileOverview Rectangle
  */
-import React, { PureComponent, SVGProps } from 'react';
+import React, { SVGProps, useLayoutEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import Animate from 'react-smooth';
 import { AnimationTiming } from '../util/types';
 import { filterProps } from '../util/ReactUtils';
 
-const getTrapezoidPath = (x: number, y: number, upperWidth: number, lowerWidth: number, height: number) => {
+const getTrapezoidPath = (x: number, y: number, upperWidth: number, lowerWidth: number, height: number): string => {
   const widthGap = upperWidth - lowerWidth;
   let path;
   path = `M ${x},${y}`;
@@ -34,114 +34,103 @@ interface TrapezoidProps {
 
 export type Props = SVGProps<SVGPathElement> & TrapezoidProps;
 
-export class Trapezoid extends PureComponent<Props> {
-  static defaultProps = {
-    x: 0,
-    y: 0,
-    upperWidth: 0,
-    lowerWidth: 0,
-    height: 0,
-    isUpdateAnimationActive: false,
-    animationBegin: 0,
-    animationDuration: 1500,
-    animationEasing: 'ease',
-  };
+export const Trapezoid: React.FC<Props> = props => {
+  const pathRef = useRef<SVGPathElement>();
+  const [totalLength, setTotalLength] = useState(-1);
 
-  state = {
-    totalLength: -1,
-  };
-
-  private node: SVGPathElement;
-
-  /* eslint-disable  react/no-did-mount-set-state */
-  componentDidMount() {
-    if (this.node && this.node.getTotalLength) {
+  useLayoutEffect(() => {
+    if (pathRef.current && pathRef.current.getTotalLength) {
       try {
-        const totalLength = this.node.getTotalLength();
+        const pathTotalLength = pathRef.current.getTotalLength();
 
-        if (totalLength) {
-          this.setState({
-            totalLength,
-          });
+        if (pathTotalLength) {
+          setTotalLength(pathTotalLength);
         }
       } catch (err) {
         // calculate total length error
       }
     }
+  }, []);
+
+  const { x, y, upperWidth, lowerWidth, height, className } = props;
+  const { animationEasing, animationDuration, animationBegin, isUpdateAnimationActive } = props;
+
+  if (
+    x !== +x ||
+    y !== +y ||
+    upperWidth !== +upperWidth ||
+    lowerWidth !== +lowerWidth ||
+    height !== +height ||
+    (upperWidth === 0 && lowerWidth === 0) ||
+    height === 0
+  ) {
+    return null;
   }
 
-  render() {
-    const { x, y, upperWidth, lowerWidth, height, className } = this.props;
-    const { totalLength } = this.state;
-    const { animationEasing, animationDuration, animationBegin, isUpdateAnimationActive } = this.props;
+  const layerClass = classNames('recharts-trapezoid', className);
 
-    if (
-      x !== +x ||
-      y !== +y ||
-      upperWidth !== +upperWidth ||
-      lowerWidth !== +lowerWidth ||
-      height !== +height ||
-      (upperWidth === 0 && lowerWidth === 0) ||
-      height === 0
-    ) {
-      return null;
-    }
-
-    const layerClass = classNames('recharts-trapezoid', className);
-
-    if (!isUpdateAnimationActive) {
-      return (
-        <g>
-          <path
-            {...filterProps(this.props, true)}
-            className={layerClass}
-            d={getTrapezoidPath(x, y, upperWidth, lowerWidth, height)}
-          />
-        </g>
-      );
-    }
+  if (!isUpdateAnimationActive) {
     return (
-      <Animate
-        canBegin={totalLength > 0}
-        from={{ upperWidth: 0, lowerWidth: 0, height, x, y }}
-        to={{ upperWidth, lowerWidth, height, x, y }}
-        duration={animationDuration}
-        animationEasing={animationEasing}
-        isActive={isUpdateAnimationActive}
-      >
-        {({
-          upperWidth: currUpperWidth,
-          lowerWidth: currLowerWidth,
-          height: currHeight,
-          x: currX,
-          y: currY,
-        }: {
-          upperWidth: number;
-          lowerWidth: number;
-          height: number;
-          x: number;
-          y: number;
-        }) => (
-          <Animate
-            canBegin={totalLength > 0}
-            from={`0px ${totalLength === -1 ? 1 : totalLength}px`}
-            to={`${totalLength}px 0px`}
-            attributeName="strokeDasharray"
-            begin={animationBegin}
-            duration={animationDuration}
-            easing={animationEasing}
-          >
-            <path
-              {...filterProps(this.props, true)}
-              className={layerClass}
-              d={getTrapezoidPath(currX, currY, currUpperWidth, currLowerWidth, currHeight)}
-              ref={node => {
-                this.node = node;
-              }}
-            />
-          </Animate>
-        )}
-      </Animate>
+      <g>
+        <path
+          {...filterProps(props, true)}
+          className={layerClass}
+          d={getTrapezoidPath(x, y, upperWidth, lowerWidth, height)}
+        />
+      </g>
     );
   }
-}
+  return (
+    <Animate
+      canBegin={totalLength > 0}
+      from={{ upperWidth: 0, lowerWidth: 0, height, x, y }}
+      to={{ upperWidth, lowerWidth, height, x, y }}
+      duration={animationDuration}
+      animationEasing={animationEasing}
+      isActive={isUpdateAnimationActive}
+    >
+      {({
+        upperWidth: currUpperWidth,
+        lowerWidth: currLowerWidth,
+        height: currHeight,
+        x: currX,
+        y: currY,
+      }: {
+        upperWidth: number;
+        lowerWidth: number;
+        height: number;
+        x: number;
+        y: number;
+      }) => (
+        <Animate
+          canBegin={totalLength > 0}
+          from={`0px ${totalLength === -1 ? 1 : totalLength}px`}
+          to={`${totalLength}px 0px`}
+          attributeName="strokeDasharray"
+          begin={animationBegin}
+          duration={animationDuration}
+          easing={animationEasing}
+        >
+          <path
+            {...filterProps(props, true)}
+            className={layerClass}
+            d={getTrapezoidPath(currX, currY, currUpperWidth, currLowerWidth, currHeight)}
+            ref={pathRef}
+          />
+        </Animate>
+      )}
+    </Animate>
+  );
+};
+
+Trapezoid.defaultProps = {
+  x: 0,
+  y: 0,
+  upperWidth: 0,
+  lowerWidth: 0,
+  height: 0,
+  isUpdateAnimationActive: false,
+  animationBegin: 0,
+  animationDuration: 1500,
+  animationEasing: 'ease',
+};
