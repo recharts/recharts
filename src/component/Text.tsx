491645087b836bdaa4ab7bec313f80c1e3962243
Akam Foad <41629832+akamfoad@users.noreply.github.com>
2023-03-22 23:36:28 +0300

    refactor: `Text` to function component (#3463)
    ## Description
    
    - add: react-hooks eslint plugin:
    React hooks eslint plugin was installed but wasn't used. From now on,
    it'll be extended from in the eslint config, so migrating components to
    funtional components and usage of hook will be smoother as it can detect
    misuse of hooks.
    - refactor: `Text` to function component
    - fix: missing hook dependency in `ResponsiveContainer`
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    Refactoring library to be more maintainable - testing refactor-ability
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [x] All new and existing tests passed.

--- src/component/Text.tsx
+++ src/component/Text.tsx
@@ -1,4 +1,4 @@
-import React, { Component, CSSProperties, SVGProps } from 'react';
+import React, { CSSProperties, SVGProps, useMemo } from 'react';
 import reduceCSSCalc from 'reduce-css-calc';
 import classNames from 'classnames';
 import _ from 'lodash';
@@ -19,20 +19,22 @@ interface CalculatedWordWidths {
   spaceWidth: number;
 }
 
-const calculateWordWidths = (props: Props): CalculatedWordWidths => {
+type CalculateWordWidthsParam = Pick<Props, 'children' | 'breakAll' | 'style'>;
+
+const calculateWordWidths = ({ children, breakAll, style }: CalculateWordWidthsParam): CalculatedWordWidths => {
   try {
     let words: string[] = [];
-    if (!_.isNil(props.children)) {
-      if (props.breakAll) {
-        words = props.children.toString().split('');
+    if (!_.isNil(children)) {
+      if (breakAll) {
+        words = children.toString().split('');
       } else {
-        words = props.children.toString().split(BREAKING_SPACES);
+        words = children.toString().split(BREAKING_SPACES);
       }
     }
 
-    const wordsWithComputedWidth = words.map(word => ({ word, width: getStringSize(word, props.style).width }));
+    const wordsWithComputedWidth = words.map(word => ({ word, width: getStringSize(word, style).width }));
 
-    const spaceWidth = props.breakAll ? 0 : getStringSize('\u00A0', props.style).width;
+    const spaceWidth = breakAll ? 0 : getStringSize('\u00A0', style).width;
 
     return { wordsWithComputedWidth, spaceWidth };
   } catch (e) {
@@ -59,24 +61,17 @@ interface Words {
   width?: number;
 }
 
-interface State {
-  wordsByLines?: Array<Words>;
-  prevWidth?: string | number;
-  prevScaleToFit?: boolean;
-  prevChildren?: string | number;
-  prevStyle?: CSSProperties;
-  prevBreakAll?: boolean;
-}
+type CalculateWordsByLinesProps = Pick<Props, 'maxLines' | 'children' | 'style' | 'breakAll'>;
 
 const calculateWordsByLines = (
-  props: Props,
+  { maxLines, children, style, breakAll }: CalculateWordsByLinesProps,
   initialWordsWithComputedWith: Array<WordWithComputedWidth>,
   spaceWidth: number,
   lineWidth: number | string,
   scaleToFit?: boolean,
 ): Array<Words> => {
-  const shouldLimitLines = isNumber(props.maxLines);
-  const text = props.children as string;
+  const shouldLimitLines = isNumber(maxLines);
+  const text = children as string;
 
   const calculate = (words: Array<WordWithComputedWidth> = []) =>
     words.reduce((result, { word, width }) => {
@@ -110,13 +105,14 @@ const calculateWordsByLines = (
     const tempText = text.slice(0, index);
 
     const words = calculateWordWidths({
-      ...props,
+      breakAll,
+      style,
       children: tempText + suffix,
     }).wordsWithComputedWidth;
 
     const result = calculate(words);
 
-    const doesOverflow = result.length > props.maxLines || findLongestLine(result).width > lineWidth;
+    const doesOverflow = result.length > maxLines || findLongestLine(result).width > lineWidth;
 
     return [doesOverflow, result];
   };
@@ -160,132 +156,121 @@ const getWordsWithoutCalculate = (children: React.ReactNode): Array<Words> => {
   return [{ words }];
 };
 
-const getWordsByLines = (props: Props, needCalculate: boolean) => {
+type GetWordsByLinesProps = Pick<Props, 'width' | 'scaleToFit' | 'children' | 'style' | 'breakAll' | 'maxLines'>;
+
+const getWordsByLines = ({ width, scaleToFit, children, style, breakAll, maxLines }: GetWordsByLinesProps) => {
   // Only perform calculations if using features that require them (multiline, scaleToFit)
-  if ((props.width || props.scaleToFit) && !Global.isSsr) {
+  if ((width || scaleToFit) && !Global.isSsr) {
     let wordsWithComputedWidth: Array<WordWithComputedWidth>, spaceWidth: number;
 
-    if (needCalculate) {
-      const wordWidths = calculateWordWidths(props);
+    const wordWidths = calculateWordWidths({ breakAll, children, style });
 
-      if (wordWidths) {
-        const { wordsWithComputedWidth: wcw, spaceWidth: sw } = wordWidths;
+    if (wordWidths) {
+      const { wordsWithComputedWidth: wcw, spaceWidth: sw } = wordWidths;
 
-        wordsWithComputedWidth = wcw;
-        spaceWidth = sw;
-      } else {
-        return getWordsWithoutCalculate(props.children);
-      }
-
-      return calculateWordsByLines(props, wordsWithComputedWidth, spaceWidth, props.width, props.scaleToFit);
-    }
-  }
-  return getWordsWithoutCalculate(props.children);
-};
-
-export class Text extends Component<Props, State> {
-  static defaultProps = {
-    x: 0,
-    y: 0,
-    lineHeight: '1em',
-    capHeight: '0.71em', // Magic number from d3
-    scaleToFit: false,
-    textAnchor: 'start',
-    verticalAnchor: 'end', // Maintain compat with existing charts / default SVG behavior
-    fill: '#808080',
-  };
-
-  state: State = {};
-
-  static getDerivedStateFromProps(nextProps: Props, prevState: State): State {
-    if (
-      nextProps.width !== prevState.prevWidth ||
-      nextProps.scaleToFit !== prevState.prevScaleToFit ||
-      nextProps.children !== prevState.prevChildren ||
-      nextProps.style !== prevState.prevStyle ||
-      nextProps.breakAll !== prevState.prevBreakAll
-    ) {
-      const needCalculate =
-        nextProps.children !== prevState.prevChildren ||
-        nextProps.style !== prevState.prevStyle ||
-        nextProps.breakAll !== prevState.prevBreakAll;
-
-      return {
-        prevWidth: nextProps.width,
-        prevScaleToFit: nextProps.scaleToFit,
-        prevChildren: nextProps.children,
-        prevStyle: nextProps.style,
-        wordsByLines: getWordsByLines(nextProps, needCalculate),
-      };
+      wordsWithComputedWidth = wcw;
+      spaceWidth = sw;
+    } else {
+      return getWordsWithoutCalculate(children);
     }
 
-    return null;
-  }
-
-  render() {
-    const {
-      dx,
-      dy,
-      textAnchor,
-      verticalAnchor,
+    return calculateWordsByLines(
+      { breakAll, children, maxLines, style },
+      wordsWithComputedWidth,
+      spaceWidth,
+      width,
       scaleToFit,
-      angle,
-      lineHeight,
-      capHeight,
-      className,
-      breakAll,
-      ...textProps
-    } = this.props;
-    const { wordsByLines } = this.state;
-
-    if (!isNumOrStr(textProps.x) || !isNumOrStr(textProps.y)) {
-      return null;
-    }
-    const x = (textProps.x as number) + (isNumber(dx as number) ? (dx as number) : 0);
-    const y = (textProps.y as number) + (isNumber(dy as number) ? (dy as number) : 0);
-
-    let startDy: number;
-    switch (verticalAnchor) {
-      case 'start':
-        startDy = reduceCSSCalc(`calc(${capHeight})`);
-        break;
-      case 'middle':
-        startDy = reduceCSSCalc(`calc(${(wordsByLines.length - 1) / 2} * -${lineHeight} + (${capHeight} / 2))`);
-        break;
-      default:
-        startDy = reduceCSSCalc(`calc(${wordsByLines.length - 1} * -${lineHeight})`);
-        break;
-    }
-
-    const transforms = [];
-    if (scaleToFit) {
-      const lineWidth = wordsByLines[0].width;
-      const { width } = this.props;
-      transforms.push(`scale(${(isNumber(width as number) ? (width as number) / lineWidth : 1) / lineWidth})`);
-    }
-    if (angle) {
-      transforms.push(`rotate(${angle}, ${x}, ${y})`);
-    }
-    if (transforms.length) {
-      textProps.transform = transforms.join(' ');
-    }
-
-    return (
-      <text
-        {...filterProps(textProps, true)}
-        x={x}
-        y={y}
-        className={classNames('recharts-text', className)}
-        textAnchor={textAnchor}
-        fill={textProps.fill.includes('url') ? Text.defaultProps.fill : textProps.fill}
-      >
-        {wordsByLines.map((line, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <tspan x={x} dy={index === 0 ? startDy : lineHeight} key={index}>
-            {line.words.join(breakAll ? '' : ' ')}
-          </tspan>
-        ))}
-      </text>
     );
   }
-}
+  return getWordsWithoutCalculate(children);
+};
+
+const textDefaultProps = {
+  x: 0,
+  y: 0,
+  lineHeight: '1em',
+  capHeight: '0.71em', // Magic number from d3
+  scaleToFit: false,
+  textAnchor: 'start',
+  verticalAnchor: 'end', // Maintain compat with existing charts / default SVG behavior
+  fill: '#808080',
+};
+
+export const Text = (props: Props) => {
+  const wordsByLines: Array<Words> = useMemo(() => {
+    return getWordsByLines({
+      breakAll: props.breakAll,
+      children: props.children,
+      maxLines: props.maxLines,
+      scaleToFit: props.scaleToFit,
+      style: props.style,
+      width: props.width,
+    });
+  }, [props.breakAll, props.children, props.maxLines, props.scaleToFit, props.style, props.width]);
+
+  const {
+    dx,
+    dy,
+    textAnchor,
+    verticalAnchor,
+    scaleToFit,
+    angle,
+    lineHeight,
+    capHeight,
+    className,
+    breakAll,
+    ...textProps
+  } = props;
+
+  if (!isNumOrStr(textProps.x) || !isNumOrStr(textProps.y)) {
+    return null;
+  }
+  const x = (textProps.x as number) + (isNumber(dx as number) ? (dx as number) : 0);
+  const y = (textProps.y as number) + (isNumber(dy as number) ? (dy as number) : 0);
+
+  let startDy: number;
+  switch (verticalAnchor) {
+    case 'start':
+      startDy = reduceCSSCalc(`calc(${capHeight})`);
+      break;
+    case 'middle':
+      startDy = reduceCSSCalc(`calc(${(wordsByLines.length - 1) / 2} * -${lineHeight} + (${capHeight} / 2))`);
+      break;
+    default:
+      startDy = reduceCSSCalc(`calc(${wordsByLines.length - 1} * -${lineHeight})`);
+      break;
+  }
+
+  const transforms = [];
+  if (scaleToFit) {
+    const lineWidth = wordsByLines[0].width;
+    const { width } = props;
+    transforms.push(`scale(${(isNumber(width as number) ? (width as number) / lineWidth : 1) / lineWidth})`);
+  }
+  if (angle) {
+    transforms.push(`rotate(${angle}, ${x}, ${y})`);
+  }
+  if (transforms.length) {
+    textProps.transform = transforms.join(' ');
+  }
+
+  return (
+    <text
+      {...filterProps(textProps, true)}
+      x={x}
+      y={y}
+      className={classNames('recharts-text', className)}
+      textAnchor={textAnchor}
+      fill={textProps.fill.includes('url') ? textDefaultProps.fill : textProps.fill}
+    >
+      {wordsByLines.map((line, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <tspan x={x} dy={index === 0 ? startDy : lineHeight} key={index}>
+          {line.words.join(breakAll ? '' : ' ')}
+        </tspan>
+      ))}
+    </text>
+  );
+};
+
+Text.defaultProps = textDefaultProps;
