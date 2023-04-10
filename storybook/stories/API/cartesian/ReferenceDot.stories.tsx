924a301f889e3da2b01d38a10f0c79d49e1529f4
Lukas Konwitschny <lukas.konwitschny@proxora.com>
2023-04-10 18:37:58 +0200

    extract shared reference component props

--- storybook/stories/API/cartesian/ReferenceDot.stories.tsx
+++ storybook/stories/API/cartesian/ReferenceDot.stories.tsx
@@ -8,16 +8,14 @@ import { getStoryArgsFromArgsTypesObject } from '../props/utils';
 import { EventHandlers } from '../props/EventHandlers';
 import { r } from '../props/DotProps';
 import { GeneralStyle } from '../props/Styles';
+import {
+  ReferenceComponentGeneralArgs,
+  ReferenceComponentInternalArgs,
+  ReferenceComponentStyle,
+} from '../props/ReferenceComponentShared';
 
 const GeneralProps: Args = {
-  xAxisId: {
-    description: 'The id of x-axis which is corresponding to the data.',
-    table: { type: { summary: 'string | number' }, category: 'General' },
-  },
-  yAxisId: {
-    description: 'The id of y-axis which is corresponding to the data.',
-    table: { type: { summary: 'string | number' }, category: 'General' },
-  },
+  ...ReferenceComponentGeneralArgs,
   x: {
     description: `If the x-axis specified by xAxisId is a number axis, the type of x must be Number. 
       If the x-axis specified by xAxisId is a category axis, the value of x must be one of 
@@ -52,6 +50,7 @@ const LabelProps: Args = {
 
 const StyleProps: Args = {
   ...GeneralStyle,
+  ...ReferenceComponentStyle,
   shape: {
     description: `If set a ReactElement, the shape of dot can be customized. If set a function, 
     the function will be called to render customized shape.`,
@@ -62,34 +61,6 @@ const StyleProps: Args = {
       category: 'Style',
     },
   },
-  isFront: {
-    description: `If set true, the dot will be rendered in front of bars in BarChart, etc.`,
-    table: { category: 'Style' },
-  },
-  ifOverflow: {
-    description: `Defines how to draw the reference dot if it falls partly outside the canvas. If set to 'discard', 
-    the reference dot will not be drawn at all. If set to 'hidden', the reference dot will be clipped to the canvas. 
-    If set to 'visible', the reference dot will be drawn completely. If set to 'extendDomain', 
-    the domain of the overflown axis will be extended such that the reference dot fits into the canvas.`,
-    table: { type: { summary: "'discard' | 'hidden' | 'visible' | 'extendDomain'" }, category: 'General' },
-    default: 'discard',
-  },
-};
-
-const InternalProps: Args = {
-  xAxis: {
-    description: 'The configuration of the corresponding x-axis, usually calculated internally.',
-    table: { type: { summary: 'Object' }, category: 'Internal' },
-  },
-  yAxis: {
-    description: 'The configuration of the corresponding y-axis, usually calculated internally.',
-    table: { type: { summary: 'Object' }, category: 'Internal' },
-  },
-  clipPathId: {
-    description:
-      "Used as the id for the clip path which is used to clip the reference dot if 'ifOverflow' is set to 'hidden'",
-    table: { type: { summary: 'number | string' }, category: 'Internal' },
-  },
 };
 
 export default {
@@ -97,7 +68,7 @@ export default {
     ...StyleProps,
     ...GeneralProps,
     ...LabelProps,
-    ...InternalProps,
+    ...ReferenceComponentInternalArgs,
     ...EventHandlers,
     // Dot
     r,
