58b6219d5d69f9aeb54778f008ef276f6869a29e
YilunSun <osirisnero@gmail.com>
2023-04-11 20:45:31 +0800

    Storybook: add args table to `Pie` (#3506)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- storybook/stories/API/polar/Pie.stories.tsx
+++ storybook/stories/API/polar/Pie.stories.tsx
@@ -1,8 +1,145 @@
 import React from 'react';
+import { Args } from '@storybook/react';
 import { Pie, ResponsiveContainer, Surface } from '../../../../src';
+import { EventHandlers } from '../props/EventHandlers';
+import { AnimationProps } from '../props/AnimationProps';
+import { getStoryArgsFromArgsTypesObject } from '../props/utils';
+
+const GeneralProps: Args = {
+  cx: {
+    description: `The x-coordinate of center. If set a percentage, 
+      the final value is obtained by multiplying the percentage of container width.`,
+    table: {
+      type: { summary: 'percentage | number', defaultValue: '50%' },
+      category: 'General',
+    },
+  },
+  cy: {
+    description: `The y-coordinate of center. If set a percentage, 
+      the final value is obtained by multiplying the percentage of container height.`,
+    table: {
+      type: { summary: 'percentage | number', defaultValue: '50%' },
+      category: 'General',
+    },
+  },
+  innerRadius: {
+    description: `The inner radius of all the sectors. If set a percentage, the final value is 
+      obtained by multiplying the percentage of maxRadius which is calculated by the width, height, cx, cy.`,
+    table: {
+      type: { summary: 'percentage | number', defaultValue: 0 },
+      category: 'General',
+    },
+  },
+  outerRadius: {
+    description: `The outer radius of all the sectors. If set a percentage, the final value is 
+      obtained by multiplying the percentage of maxRadius which is calculated by the width, height, cx, cy.`,
+    table: {
+      type: { summary: 'percentage | number', defaultValue: '80%' },
+      category: 'General',
+    },
+  },
+
+  startAngle: {
+    description: 'The start angle of first sector.',
+    table: {
+      type: { summary: 'number', defaultValue: 0 },
+      category: 'General',
+    },
+  },
+  endAngle: {
+    description: 'The end angle of last sector, which should be unequal to startAngle.',
+    table: {
+      type: { summary: 'number', defaultValue: 360 },
+      category: 'General',
+    },
+  },
+  minAngle: {
+    description: 'The minimum angle of each nonzero data.',
+    table: {
+      type: { summary: 'number', defaultValue: 0 },
+      category: 'General',
+    },
+  },
+  paddingAngle: {
+    description: 'The angle between two sectors.',
+    table: {
+      type: { summary: 'number', defaultValue: 0 },
+      category: 'General',
+    },
+  },
+  nameKey: {
+    description: "The key of each sector's name.",
+    table: { type: { summary: 'string', defaultValue: 'name' }, category: 'General' },
+  },
+  dataKey: {
+    description: "The key of each sector's value.",
+    table: { type: { summary: 'string | number | Function' }, category: 'General' },
+  },
+  legendType: {
+    description: 'The type of icon in legend. If set to "none", no legend item will be rendered.',
+    table: {
+      type: {
+        summary: `'line' | 'plainline' | 'square' | 'rect'| 'circle' | 'cross' | 'diamond' | 'square' 
+          | 'star' | 'triangle' | 'wye' | 'none'`,
+        defaultValue: 'rect',
+      },
+      category: 'General',
+    },
+  },
+  label: {
+    description: `If false set, labels will not be drawn. If true set, labels will be drawn which have 
+      the props calculated internally. If object set, labels will be drawn which have the props 
+      merged by the internal calculated props and the option. If ReactElement set, the option 
+      can be the custom label element. If set a function, the function will be called to render 
+      customized label.`,
+    table: {
+      type: { summary: 'Boolean | Object | ReactElement | Function', defaultValue: false },
+      category: 'General',
+    },
+  },
+  labelLine: {
+    description: `If false set, label lines will not be drawn. If true set, label lines will be drawn which 
+      have the props calculated internally. If object set, label lines will be drawn which have 
+      the props merged by the internal calculated props and the option. If ReactElement set, 
+      the option can be the custom label line element. If set a function, the function will be 
+      called to render customized label line.`,
+    table: {
+      type: { summary: 'Boolean | Object | ReactElement | Function', defaultValue: false },
+      category: 'General',
+    },
+  },
+  data: {
+    description: 'The source data which each element is an object.',
+    table: { type: { summary: 'Array' }, category: 'General' },
+  },
+  activeIndex: {
+    description: 'The index of active sector in Pie, this option can be changed in mouse event handlers.',
+    table: { type: { summary: 'Array' }, category: 'General' },
+  },
+  activeShape: {
+    description: 'The shape of active sector.',
+    table: { type: { summary: 'Object | ReactElement | Function' }, category: 'General' },
+  },
+  inactiveShape: {
+    description: 'The shape of inactive sector.',
+    table: { type: { summary: 'Object | ReactElement | Function' }, category: 'General' },
+  },
+};
 
 export default {
-  tags: ['autodocs'],
+  argTypes: {
+    ...GeneralProps,
+    ...EventHandlers,
+    ...AnimationProps,
+    // Deprecated
+    dangerouslySetInnerHTML: { table: { category: 'Deprecated' }, hide: true, disable: true },
+    valueKey: {
+      description: "Use 'dataKey' alternatively, The key of each sector's value.",
+      table: { type: { summary: 'string', defaultValue: 'value' }, category: 'Deprecated' },
+      hide: true,
+      disable: true,
+    },
+  },
   component: Pie,
 };
 
@@ -44,6 +181,7 @@ export const Simple = {
     );
   },
   args: {
+    ...getStoryArgsFromArgsTypesObject(GeneralProps),
     data: [
       { name: 'iphone4', value: 120, fill: '#ff7300' },
       { name: 'iphone4s', value: 500, fill: '#e5671a' },
@@ -99,5 +237,8 @@ export const Simple = {
         outerRadius: 200,
       },
     ],
+  },
+  parameters: {
+    controls: { include: Object.keys(GeneralProps) },
   },
 };
