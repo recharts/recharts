abb1c4a92e72c8c2299f3a9b050704b28fa34f4e
YilunSun <osirisnero@gmail.com>
2023-04-14 22:23:23 +0800

    Storybook: add args table to `Treemap` (#3527)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- storybook/stories/API/chart/Treemap.stories.tsx
+++ storybook/stories/API/chart/Treemap.stories.tsx
@@ -1,9 +1,113 @@
 import React from 'react';
+import { Args } from '@storybook/react';
 import { sizeData } from '../../data';
 import { ResponsiveContainer, Treemap } from '../../../../src';
 
+const GeneralProps: Args = {
+  width: {
+    description: 'The width of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  height: {
+    description: 'The height of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  dataKey: {
+    description: 'The key of a group of data which should be unique in a treemap.',
+    table: {
+      type: {
+        summary: 'String | Number | Function',
+        defaultValue: 'value',
+      },
+      category: 'General',
+    },
+  },
+  aspectRatio: {
+    description: "The treemap will try to keep every single rectangle's aspect ratio near the aspectRatio given.",
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  isAnimationActive: {
+    description: 'If set false, animation of area will be disabled.',
+    table: {
+      type: {
+        summary: 'Boolean',
+        defaultValue: true,
+      },
+      category: 'General',
+    },
+  },
+  animationBegin: {
+    description: 'Specifies when the animation should begin, the unit of this option is ms.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 0,
+      },
+      category: 'General',
+    },
+  },
+  animationDuration: {
+    description: 'Specifies the duration of animation, the unit of this option is ms.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 1500,
+      },
+      category: 'General',
+    },
+  },
+  animationEasing: {
+    description: 'The type of easing function.',
+    table: {
+      type: {
+        summary: "'ease' | 'ease-in' | 'ease-out' | 'ease-in-out' | 'linear'",
+        defaultValue: "'ease'",
+      },
+      category: 'General',
+    },
+  },
+  onAnimationStart: {
+    description: 'The customized event handler of animation start',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+  onAnimationEnd: {
+    description: 'The customized event handler of animation end',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+};
+
 export default {
-  tags: ['autodocs'],
+  argTypes: {
+    ...GeneralProps,
+  },
   component: Treemap,
 };
 
