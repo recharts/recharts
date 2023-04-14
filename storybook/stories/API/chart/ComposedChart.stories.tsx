ce82a13c325ba5534053c8acc3739a0501456fc0
YilunSun <osirisnero@gmail.com>
2023-04-14 22:05:37 +0800

    Storybook: change `ComposedChart` time-scale story to right category (#3528)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- storybook/stories/API/chart/ComposedChart.stories.tsx
+++ storybook/stories/API/chart/ComposedChart.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { pageData } from '../../data';
+import { pageData, timeData } from '../../data';
 import {
   Area,
   Bar,
@@ -122,6 +122,52 @@ export const LineBarHorizontal = {
         <Line type="monotone" dataKey="pv" stroke="#ff7300" />
       </>
     ),
+  },
+};
+
+export const LineBarAreaScatterTimeScale = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={500}>
+        <div style={{ width: '600px', margin: 'auto' }}>
+          <p>A ComposedChart of time scale</p>
+          <div className="composed-chart-wrapper">
+            <Composed
+              width={600}
+              height={400}
+              data={args.timeData}
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
+              <CartesianGrid stroke="#f5f5f5" />
+              <XAxis
+                dataKey="x"
+                domain={[args.from.getTime(), args.to.getTime()]}
+                scale="time"
+                type="number"
+                tick={{ fontSize: 10, fill: 'red' }}
+              />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Area type="monotone" dataKey="y" fill="#8884d8" stroke="#8884d8" />
+              <Bar dataKey="y" barSize={20} fill="#413ea0" />
+              <Line type="monotone" dataKey="y" stroke="#ff7300" />
+            </Composed>
+          </div>
+        </div>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    from: new Date('2019-07-04T00:00:00.000Z'),
+    to: new Date('2019-07-10T00:00:00.000Z'),
+    timeData,
+    data: pageData,
   },
 };
 
