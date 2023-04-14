e81a1b07ba9ec7bd640df450c6517976efcf9887
YilunSun <osirisnero@gmail.com>
2023-04-14 22:24:15 +0800

    Storybook: add args table to `Sankey` (#3525)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- storybook/stories/API/chart/Sankey.stories.tsx
+++ storybook/stories/API/chart/Sankey.stories.tsx
@@ -1,9 +1,226 @@
 import React from 'react';
+import { Args } from '@storybook/react';
 import { nodeLinkData } from '../../data';
 import { ResponsiveContainer, Sankey } from '../../../../src';
 
+const GeneralProps: Args = {
+  nameKey: {
+    description: "The key of each sector's name.",
+    table: {
+      type: {
+        summary: 'String',
+        defaultValue: 'name',
+      },
+      category: 'General',
+    },
+  },
+  dataKey: {
+    description: 'The key of a group of data which should be unique in a SankeyChart.',
+    table: {
+      type: {
+        summary: 'String | Number | Function',
+        defaultValue: 'value',
+      },
+      category: 'General',
+    },
+  },
+  width: {
+    description: "The percentage value of the chart's width or a fixed width.",
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '100%',
+      },
+      category: 'General',
+    },
+  },
+  height: {
+    description: "The percentage value of the chart's width or a fixed height.",
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '100%',
+      },
+      category: 'General',
+    },
+  },
+  data: {
+    description: 'The source data, including the array of nodes, and the relationships, represented by links.',
+    table: {
+      type: {
+        summary: 'Object',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  nodePadding: {
+    description: 'The padding between the nodes',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 10,
+      },
+      category: 'General',
+    },
+  },
+  nodeWidth: {
+    description: 'The width of node',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 10,
+      },
+      category: 'General',
+    },
+  },
+  linkWidth: {
+    description: 'The width of link',
+    table: {
+      type: {
+        summary: 'Number',
+      },
+      category: 'General',
+    },
+  },
+  linkCurvature: {
+    description: 'The curvature of width',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 0.5,
+      },
+      category: 'General',
+    },
+  },
+  iterations: {
+    description: 'The number of the iterations between the links',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 32,
+      },
+      category: 'General',
+    },
+  },
+  node: {
+    description: `If set a object, the option is the configuration of nodes. If set a React element, 
+      the option is the custom react element of drawing the nodes.`,
+    table: {
+      type: {
+        summary: 'Object | ReactElement',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  link: {
+    description: `If set a object, the option is the configuration of links. If set a React element, 
+      the option is the custom react element of drawing the links.`,
+    table: {
+      type: {
+        summary: 'Object | ReactElement',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  margin: {
+    description: 'The sizes of whitespace around the container.',
+    table: {
+      type: {
+        summary: 'Object',
+        defaultValue: { top: 5, right: 5, bottom: 5, left: 5 },
+      },
+      category: 'General',
+    },
+  },
+  onClick: {
+    description: 'The customized event handler of click on the area in this group',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+  onMouseEnter: {
+    description: 'The customized event handler of mouseenter on the area in this group',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+  onMouseLeave: {
+    description: 'The customized event handler of mouseleave on the area in this group',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+  sourceX: {
+    description: 'The source number of X-axis',
+    table: {
+      type: {
+        summary: 'Number',
+      },
+      category: 'General',
+    },
+  },
+  sourceY: {
+    description: 'The source number of Y-axis',
+    table: {
+      type: {
+        summary: 'Number',
+      },
+      category: 'General',
+    },
+  },
+  sourceControlX: {
+    description: 'The source control of X-axis',
+    table: {
+      type: {
+        summary: 'Number',
+      },
+      category: 'General',
+    },
+  },
+  targetControlX: {
+    description: 'The target control of X-axis',
+    table: {
+      type: {
+        summary: 'Number',
+      },
+      category: 'General',
+    },
+  },
+  targetX: {
+    description: 'The target of X-axis',
+    table: {
+      type: {
+        summary: 'Number',
+      },
+      category: 'General',
+    },
+  },
+  targetY: {
+    description: 'The target of Y-axis',
+    table: {
+      type: {
+        summary: 'Number',
+      },
+    },
+  },
+};
+
 export default {
-  tags: ['autodocs'],
+  argTypes: {
+    ...GeneralProps,
+  },
   component: Sankey,
 };
 
