c2a2551c830b538ceb1d6d9b6c72336e5c8647e0
YilunSun <osirisnero@gmail.com>
2023-04-16 22:22:28 +0800

    Storybook: add args table to `AreaChart` (#3516)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- storybook/stories/API/chart/AreaChart.stories.tsx
+++ storybook/stories/API/chart/AreaChart.stories.tsx
@@ -1,10 +1,147 @@
 import React from 'react';
 import { curveCardinal } from 'victory-vendor/d3-shape';
+import { Args } from '@storybook/react';
 import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip } from '../../../../src';
 import { pageData } from '../../data';
 
+const GeneralProps: Args = {
+  layout: {
+    description: 'The layout of area in the chart.',
+    table: {
+      type: {
+        summary: '"horizontal" | "vertical"',
+        defaultValue: 'horizontal',
+      },
+      category: 'General',
+    },
+  },
+  syncId: {
+    description: `If any two categorical charts(LineChart, AreaChart, BarChart, ComposedChart) have the same syncId, 
+      these two charts can sync the position tooltip, and the startIndex, endIndex of Brush.`,
+    table: {
+      type: {
+        summary: 'String',
+      },
+      category: 'General',
+    },
+  },
+  syncMethod: {
+    description: `When syncId is provided, allows customization of how the charts will synchronize tooltips and 
+    brushes. Using 'index' (default setting), other charts will reuse current datum's index within the data array. 
+      In cases where data does not have the same length, this might yield unexpected results. In that case use 'value' 
+      which will try to match other charts values, or a fully custom function which will receive tick, 
+      data as argument and should return an index.`,
+    table: {
+      type: {
+        summary: '"index" | "value" | "function"',
+        defaultValue: 'index',
+      },
+      category: 'General',
+    },
+  },
+  width: {
+    description: 'The width of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  height: {
+    description: 'The height of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  data: {
+    description: 'The source data, in which each element is an object.',
+    table: {
+      type: {
+        summary: 'Array',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  margin: {
+    description: 'The sizes of whitespace around the container.',
+    table: {
+      type: {
+        summary: 'Object',
+        defaultValue: { top: 5, right: 5, bottom: 5, left: 5 },
+      },
+      category: 'General',
+    },
+  },
+  stackOffset: {
+    description: `The type of offset function used to generate the lower and upper values in the series array. 
+      The four types are built-in offsets in d3-shape.`,
+    table: {
+      type: {
+        summary: '"expand" | "none" | "wiggle" | "silhouette"',
+        defaultValue: '"none"',
+      },
+      category: 'General',
+    },
+  },
+  baseValue: {
+    description: 'The base value of area.',
+    table: {
+      type: {
+        summary: 'Number | "dataMin" | "dataMax" | "auto"',
+        defaultValue: '"auto"',
+      },
+      category: 'General',
+    },
+  },
+  onClick: {
+    description: 'The customized event handler of click in this chart.',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+  onMouseEnter: {
+    description: 'The customized event handler of mouseenter in this chart.',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+  onMouseMove: {
+    description: 'The customized event handler of mousemove in this chart.',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+  onMouseLeave: {
+    description: 'The customized event handler of mouseleave in this chart.',
+    table: {
+      type: {
+        summary: 'Function',
+      },
+      category: 'General',
+    },
+  },
+};
+
 export default {
-  tags: ['autodocs'],
+  argTypes: {
+    ...GeneralProps,
+  },
   component: AreaChart,
 };
 
