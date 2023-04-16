84664411421c132b6f537800ed58941a9688254b
YilunSun <osirisnero@gmail.com>
2023-04-16 22:36:19 +0800

    Storybook: add args table to `RadarBarChart` (#3524)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    ---------
    
    Co-authored-by: Nikolas Rieble <nikolas-rieble@protonmail.com>

--- storybook/stories/API/chart/RadialBarChart.stories.tsx
+++ storybook/stories/API/chart/RadialBarChart.stories.tsx
@@ -1,9 +1,181 @@
 import React from 'react';
+import { Args } from '@storybook/react';
 import { pageData } from '../../data';
 import { RadialBar, RadialBarChart, ResponsiveContainer } from '../../../../src';
 
+const GeneralProps: Args = {
+  width: {
+    description: 'The width of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  height: {
+    description: 'The height of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  data: {
+    table: {
+      type: {
+        summary: 'Array',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  margin: {
+    description: 'The sizes of whitespace around the container.',
+    table: {
+      type: {
+        summary: 'Object',
+        defaultValue: { top: 5, right: 5, bottom: 5, left: 5 },
+      },
+      category: 'General',
+    },
+  },
+  barCategoryGap: {
+    description: 'The gap between two bar categories, which can be a percent value or a fixed value.',
+    table: {
+      type: {
+        summary: 'Percentage| Number',
+        defaultValue: '10%',
+      },
+      category: 'General',
+    },
+  },
+  barGap: {
+    description: 'The gap between two bars in the same category.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 4,
+      },
+      category: 'General',
+    },
+  },
+  cx: {
+    description: `The x-coordinate of center. If set a percentage, 
+    the final value is obtained by multiplying the percentage of width.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '50%',
+      },
+      category: 'General',
+    },
+  },
+  cy: {
+    description: `The y-coordinate of center. If set a percentage, 
+    the final value is obtained by multiplying the percentage of height.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '50%',
+      },
+      category: 'General',
+    },
+  },
+  startAngle: {
+    description: 'The start angle of all the bars.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 0,
+      },
+      category: 'General',
+    },
+  },
+  endAngle: {
+    description: 'The end angle of all the bars.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 360,
+      },
+      category: 'General',
+    },
+  },
+  innerRadius: {
+    description: `The innerRadius of the radial bar which is most close to the center. If set 
+      a percentage, the final value is obtained by multiplying the percentage of 
+      maxRadius which is calculated by the width, height, cx, cy.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '30%',
+      },
+      category: 'General',
+    },
+  },
+  outerRadius: {
+    description: `The outerRadius of the radial bar which is most far away from the center. 
+      If set a percentage, the final value is obtained by multiplying the percentage 
+      of maxRadius which is calculated by the width, height, cx, cy.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '100%',
+      },
+      category: 'General',
+    },
+  },
+  barSize: {
+    description: `The size of each bar. If the barSize is not specified, the size of bar will 
+      be calculated by the barCategoryGap, barGap and the quantity of bar groups.`,
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  onMouseEnter: {
+    description: 'The function will be called when mouse enter bars.',
+    table: {
+      type: {
+        summary: 'Function',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  onMouseLeave: {
+    description: 'The function will be called when mouse leave bars.',
+    table: {
+      type: {
+        summary: 'Function',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  onClick: {
+    description: 'The function will be called when click bars.',
+    table: {
+      type: {
+        summary: 'Function',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+};
+
 export default {
-  tags: ['autodocs'],
+  argTypes: {
+    ...GeneralProps,
+  },
   component: RadialBarChart,
 };
 
