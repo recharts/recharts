4bb5aaa9ba7fa5602bad59040e818be7c80a8142
YilunSun <osirisnero@gmail.com>
2023-04-16 22:35:40 +0800

    Storybook: add args table to `RadarChart` (#3523)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    ---------
    
    Co-authored-by: Nikolas Rieble <nikolas-rieble@protonmail.com>

--- storybook/stories/API/chart/RadarChart.stories.tsx
+++ storybook/stories/API/chart/RadarChart.stories.tsx
@@ -1,9 +1,160 @@
 import React from 'react';
+import { Args } from '@storybook/react';
 import { pageData } from '../../data';
 import { Radar, RadarChart, ResponsiveContainer } from '../../../../src';
 
+const GeneralProps: Args = {
+  width: {
+    description: 'The width of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  height: {
+    description: 'The height of chart container.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  data: {
+    description: 'The source data, in which each element is an object.',
+    table: {
+      type: {
+        summary: 'Array',
+        defaultValue: undefined,
+      },
+      category: 'General',
+    },
+  },
+  cx: {
+    description: `The x-coordinate of center. If set a percentage, the final value is obtained by 
+      multiplying the percentage of width.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '50%',
+      },
+      category: 'General',
+    },
+  },
+  cy: {
+    description: `The y-coordinate of center. If set a percentage, the final value is obtained by 
+      multiplying the percentage of height.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '50%',
+      },
+      category: 'General',
+    },
+  },
+  startAngle: {
+    description: 'The angle of first radial direction line.',
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: 90,
+      },
+      category: 'General',
+    },
+  },
+  endAngle: {
+    description: `The angle of last point in the circle which should be startAngle - 360 or startAngle + 360. 
+      We'll calculate the direction of chart by 'startAngle' and 'endAngle'.`,
+    table: {
+      type: {
+        summary: 'Number',
+        defaultValue: -270,
+      },
+      category: 'General',
+    },
+  },
+  innerRadius: {
+    description: `The inner radius of first circle grid. If set a percentage, the final value is obtained 
+      by multiplying the percentage of maxRadius which is calculated by the width, height, cx, cy.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: 0,
+      },
+      category: 'General',
+    },
+  },
+  outerRadius: {
+    description: `The outer radius of last circle grid. If set a percentage, the final value is obtained by multiplying 
+      the percentage of maxRadius which is calculated by the width, height, cx, cy.`,
+    table: {
+      type: {
+        summary: 'Percentage | Number',
+        defaultValue: '80%',
+      },
+      category: 'General',
+    },
+  },
+  margin: {
+    description: 'The sizes of whitespace around the container.',
+    table: {
+      type: {
+        summary: 'Object',
+        defaultValue: { top: 0, right: 0, bottom: 0, left: 0 },
+      },
+      category: 'General',
+    },
+  },
+  clockWise: {
+    description: 'The direction of clockwise.',
+    table: {
+      type: {
+        summary: 'Bool',
+        defaultValue: true,
+      },
+      category: 'General',
+    },
+  },
+  onMouseEnter: {
+    description: "The function will be called when mouse enter the 'Radar'.",
+    table: {
+      type: {
+        summary: 'Function',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  onMouseLeave: {
+    description: "The function will be called when mouse leave the 'Radar'.",
+    table: {
+      type: {
+        summary: 'Function',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+  onClick: {
+    description: "The function will be called when click the 'Radar'.",
+    table: {
+      type: {
+        summary: 'Function',
+        defaultValue: null,
+      },
+      category: 'General',
+    },
+  },
+};
+
 export default {
-  tags: ['autodocs'],
+  argTypes: {
+    ...GeneralProps,
+  },
   component: RadarChart,
 };
 
