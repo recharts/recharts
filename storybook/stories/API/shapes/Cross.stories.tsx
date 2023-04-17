5dcb2644ad7d8f72bfa5f6c3ef94144d3cca8b59
nikolasrieble <nikolas.rieble@tulip.co>
2023-04-17 22:28:56 +0200

    Storybook: Add story for Cross

---
+++ storybook/stories/API/shapes/Cross.stories.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Cross, Customized, ComposedChart, ResponsiveContainer } from '../../../../src';
+import { GeneralStyle } from '../props/Styles';
+
+export default {
+  component: Cross,
+  argTypes: {
+    y: {
+      description: 'The y-coordinate of the horizontal line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    x: {
+      description: 'The x-coordinate of the vertical line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    top: {
+      description: 'The y-coordinate of the top left point in the boundary box of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    left: {
+      description: 'The x-coordinate of the top left point in the boundary box of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    height: {
+      description: 'The height of the vertical line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    width: {
+      description: 'The width of the horizontal line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    // TODO: Document further SVGProps as well.
+    ...GeneralStyle,
+  },
+};
+
+export const Simple = {
+  render: (args: Record<string, any>) => {
+    const CustomizedCross = (props: Record<string, any>) => <Cross {...props} />;
+    return (
+      <ResponsiveContainer width="100%" height={500}>
+        <ComposedChart
+          width={500}
+          height={300}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          {/* I have no idea why this is needed. */}
+          <Customized component={() => CustomizedCross(args)} />
+          {/* But this does not work. */}
+          {/* <Cross {...args} /> */}
+        </ComposedChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    y: 100,
+    x: 500,
+    top: 0,
+    left: 0,
+    height: 1000,
+    width: 1000,
+    stroke: '#000',
+    fill: 'none',
+    strokeDasharray: '3 3',
+    strokeWidth: 10,
+    strokeOpacity: 0.5,
+  },
+};
