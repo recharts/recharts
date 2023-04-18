1720700034b51bf319e96a78c014356beea4b8a8
Nikolas Rieble <nikolas.rieble@tulip.co>
2023-04-18 19:35:50 +0200

    Storybook: Add story for Cross (#3539)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    Strangely I could not get the cross to work without the Customized
    wrapper, although I do not even use what Customized provides, namely
    access to chart content.
    
    Either way, this does work, all props are usable controls, and it
    documents the Cross component, lets iterate.
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    ---------
    
    Co-authored-by: Akam Foad <41629832+akamfoad@users.noreply.github.com>

---
+++ storybook/stories/API/shapes/Cross.stories.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Cross, Customized, ComposedChart, ResponsiveContainer } from '../../../../src';
+import { GeneralStyle } from '../props/Styles';
+
+export default {
+  component: Cross,
+  argTypes: {
+    y: {
+      description: 'The y-coordinate of the horizontal line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    x: {
+      description: 'The x-coordinate of the vertical line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    top: {
+      description: 'The y-coordinate of the top left point in the boundary box of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    left: {
+      description: 'The x-coordinate of the top left point in the boundary box of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    height: {
+      description: 'The height of the vertical line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    width: {
+      description: 'The width of the horizontal line of the cross.',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    // TODO: Document further SVGProps as well.
+    ...GeneralStyle,
+  },
+};
+
+export const Simple = {
+  render: (args: Record<string, any>) => {
+    const CustomizedCross = (props: Record<string, any>) => <Cross {...props} />;
+    return (
+      <ResponsiveContainer width="100%" height={500}>
+        <ComposedChart
+          width={500}
+          height={300}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <Customized component={<Cross {...args} />} />
+        </ComposedChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    y: 100,
+    x: 500,
+    top: 0,
+    left: 0,
+    height: 1000,
+    width: 1000,
+    stroke: '#000',
+    fill: 'none',
+    strokeDasharray: '3 3',
+    strokeWidth: 10,
+    strokeOpacity: 0.5,
+  },
+};
