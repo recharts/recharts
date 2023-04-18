59669871ba1806f121960df2f0ef2be690c964b1
Julianna Langston <108109448+julianna-langston@users.noreply.github.com>
2023-04-18 14:02:29 -0400

    Add storybook examples for charts with fill patterns (#3541)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    Add storybook stories for area charts, bar charts, and pie charts,
    showing how to incorporate fill patterns.
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    https://github.com/recharts/recharts/issues/3536
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    Using fill patterns is a common and recommended way to remediate WCAG
    1.4.1 "Use of Color" issues for charts. This example will help recharts
    users who want to use fill patterns as an accessibility feature
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    ![bar chart with fill
    patterns](https://user-images.githubusercontent.com/108109448/232631098-0f142233-4df9-4cc5-8863-72c5ddff5e73.png)
    ![area chart with fill
    patterns](https://user-images.githubusercontent.com/108109448/232631149-dd0c3c33-ad9e-42a3-ae59-bfaf8a592115.png)
    ![pie chart with fill
    patterns](https://user-images.githubusercontent.com/108109448/232631275-35246194-8984-43f4-8cf2-4f5c675854df.png)
    
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [x] All new and existing tests passed.
    
    Co-authored-by: Julianna Langston <julianna@cosaic.io>

--- storybook/stories/API/cartesian/Bar.stories.tsx
+++ storybook/stories/API/cartesian/Bar.stories.tsx
@@ -311,6 +311,49 @@ export const FillGradient = {
   },
 };
 
+export const FillPattern = {
+  render: (args: Record<string, any>) => {
+    const { dataKey1, dataKey2, ...barArgs } = args;
+
+    return (
+      <ResponsiveContainer width="100%" height={surfaceHeight}>
+        <ComposedChart
+          width={surfaceWidth}
+          height={surfaceHeight}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+          data={pageData}
+        >
+          <defs>
+            <pattern id="star" width="10" height="10" patternUnits="userSpaceOnUse">
+              <polygon points="0,0 2,5 0,10 5,8 10,10 8,5 10,0 5,2" />
+            </pattern>
+            <pattern id="stripe" width="4" height="4" patternUnits="userSpaceOnUse" patternTransform="rotate(45)">
+              <rect width="2" height="4" fill="red" />
+            </pattern>
+          </defs>
+          <Bar dataKey={dataKey1} stroke="#8884d8" fillOpacity={1} fill="url(#star)" {...barArgs} />
+          <Bar dataKey={dataKey2} stroke="#82ca9d" fillOpacity={1} fill="url(#stripe)" {...barArgs} />
+        </ComposedChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    dataKey1: 'uv',
+    dataKey2: 'pv',
+    isAnimationActive: false,
+  },
+  parameters: {
+    controls: {
+      include: ['data'],
+    },
+  },
+};
+
 export const Animation = {
   ...Base,
   args: {
