ae776f8927bdd0c601f33da93f521f734a902b28
Coltin Kifer <ckifer@amazon.com>
2023-05-23 11:40:41 -0700

    docs(storybook): adjust shapes to reflect new pattern, adjust wording

--- storybook/stories/API/shapes/Curve.stories.tsx
+++ storybook/stories/API/shapes/Curve.stories.tsx
@@ -346,7 +346,7 @@ export default {
   },
 };
 
-export const Simple = {
+export const API = {
   render: (args: Record<string, any>) => {
     return (
       <ResponsiveContainer width="100%" height={500}>
