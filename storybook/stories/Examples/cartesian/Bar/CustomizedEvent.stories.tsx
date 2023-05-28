db25e0d1985c6630cdd3645b4be0da2c5ca25026
nikolasrieble <nikolas.rieble@tulip.co>
2023-05-28 12:31:43 +0200

    Linting

--- storybook/stories/Examples/cartesian/Bar/CustomizedEvent.stories.tsx
+++ storybook/stories/Examples/cartesian/Bar/CustomizedEvent.stories.tsx
@@ -11,25 +11,28 @@ export const CustomizedEvent = {
     const [activeIndex, setActiveIndex] = useState(1);
     const [surfaceWidth, surfaceHeight] = [600, 300];
     return (
-      <ResponsiveContainer width="100%" height={surfaceHeight}>
-        <ComposedChart
-          width={surfaceWidth}
-          height={surfaceHeight}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
-          }}
-          data={pageData}
-        >
-          <Bar onClick={(_data, index) => setActiveIndex(index)} dataKey="uv" isAnimationActive={false}>
-            {pageData.map((_, index: number) => (
-              <Cell cursor="pointer" fill={index === activeIndex ? '#82ca9d' : '#8884d8'} key={`cell-${index}`} />
-            ))}
-          </Bar>
-        </ComposedChart>
-      </ResponsiveContainer>
+      <>
+        <h2>Clicking a Bar triggers the event</h2>
+        <ResponsiveContainer width="100%" height={surfaceHeight}>
+          <ComposedChart
+            width={surfaceWidth}
+            height={surfaceHeight}
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+            data={pageData}
+          >
+            <Bar onClick={(_data, index) => setActiveIndex(index)} dataKey="uv" isAnimationActive={false}>
+              {pageData.map((_, index: number) => (
+                <Cell cursor="pointer" fill={index === activeIndex ? '#82ca9d' : '#8884d8'} key={`cell-${index}`} />
+              ))}
+            </Bar>
+          </ComposedChart>
+        </ResponsiveContainer>
+      </>
     );
   },
 };
