11435d191debea218b94148cdaacac5efcfe6b0d
nikolasrieble <nikolas.rieble@tulip.co>
2023-06-04 13:39:22 +0200

    Storybook: CartesianGrid - Simplify and follow common pattern

--- storybook/stories/API/cartesian/CartesianGrid.stories.tsx
+++ storybook/stories/API/cartesian/CartesianGrid.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Args } from '@storybook/react';
-import { CartesianGrid, ResponsiveContainer, Surface } from '../../../../src';
+import { CartesianGrid, ResponsiveContainer, ComposedChart } from '../../../../src';
 
 const GeneralProps: Args = {
   x: {
@@ -102,39 +102,23 @@ export default {
   },
 };
 
-const [surfaceWidth, surfaceHeight] = [500, 500];
-
-export const Simple = {
+export const API = {
   render: (args: Record<string, any>) => {
+    const [surfaceWidth, surfaceHeight] = [500, 500];
     return (
       <ResponsiveContainer width="100%" height={surfaceHeight}>
-        <Surface
-          width={surfaceWidth}
-          height={surfaceHeight}
-          viewBox={{
-            x: 0,
-            y: 0,
-            width: surfaceWidth,
-            height: surfaceHeight,
-          }}
-        >
-          <CartesianGrid
-            x={0}
-            y={0}
-            width={500}
-            height={500}
-            fillOpacity={0.4}
-            strokeDasharray={args.strokeDasharray}
-            verticalPoints={args.verticalPoints}
-            horizontalPoints={args.horizontalPoints}
-            verticalFill={args.verticalFill}
-            horizontalFill={args.horizontalFill}
-          />
-        </Surface>
+        <ComposedChart width={surfaceWidth} height={surfaceHeight}>
+          <CartesianGrid {...args} />
+        </ComposedChart>
       </ResponsiveContainer>
     );
   },
   args: {
+    y: 0,
+    x: 0,
+    width: 500,
+    height: 500,
+    fillOpacity: 0.4,
     horizontalPoints: [10, 20, 30, 100, 400],
     verticalPoints: [100, 200, 300, 400],
     verticalFill: ['red', '#eee'],
