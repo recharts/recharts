32174aa56e526a9b05a99d6ed266d232257f18b5
nikolasrieble <nikolas.rieble@tulip.co>
2023-06-04 16:55:32 +0200

    Storybook: YAxis - align with new pattern

--- storybook/stories/API/cartesian/YAxis.stories.tsx
+++ storybook/stories/API/cartesian/YAxis.stories.tsx
@@ -1,16 +1,7 @@
-import React, { ComponentProps, useState } from 'react';
+import React from 'react';
 import { Args } from '@storybook/react';
-import { YAxis, XAxis, Legend, ResponsiveContainer, ComposedChart, Bar } from '../../../../src';
-import { EventHandlers } from '../props/EventHandlers';
-
-const data = [
-  { name: 'Page A', pv: 800, amt: 1400 },
-  { name: 'Page B', pv: 967, amt: 1506 },
-  { name: 'Page C', pv: 1098, amt: 989 },
-  { name: 'Page D', pv: 1200, amt: 1228 },
-  { name: 'Page E', pv: 1108, amt: 1100 },
-  { name: 'Page F', pv: 680, amt: 1700 },
-];
+import { YAxis, XAxis, Line, ResponsiveContainer, LineChart, CartesianGrid, Tooltip, Legend } from '../../../../src';
+import { coordinateWithValueData } from '../../data';
 
 const GeneralProps: Args = {
   yAxisId: {
@@ -29,99 +20,35 @@ const GeneralProps: Args = {
     table: { type: { summary: 'AxisDomain' }, category: 'General' },
   },
 };
-
-const EventProps: Args = {
-  onMouseDown: EventHandlers.onMouseDown,
-  onMouseUp: EventHandlers.onMouseUp,
-  onMouseMove: EventHandlers.onMouseMove,
-  onMouseOver: EventHandlers.onMouseOver,
-  onMouseOut: EventHandlers.onMouseOut,
-  onMouseEnter: EventHandlers.onMouseEnter,
-  onMouseLeave: EventHandlers.onMouseLeave,
-};
-
 export default {
+  component: YAxis,
   argTypes: {
     ...GeneralProps,
-    ...EventProps,
   },
-  component: YAxis,
 };
 
-export const Simple = {
-  render: (args: ComponentProps<typeof YAxis> & { data: any }) => {
-    const allKeys = Object.keys(args.data[0]);
-    const [activeKeys, setActiveKeys] = useState(allKeys);
-
-    /*
-     * Toggles displayed bars when clicking on a legend item
-     */
-    const handleLegendClick: ComponentProps<typeof Legend>['onClick'] = e => {
-      const key: string = e.dataKey;
-      setActiveKeys(prev => (prev.includes(key) ? prev.filter(k => k !== key) : [...prev, key]));
-    };
-
+export const API = {
+  render: (args: Record<string, any>) => {
     return (
       <ResponsiveContainer width="100%" height={500}>
-        <ComposedChart data={args.data} margin={{ top: 20, right: 80, left: 20 }}>
-          <XAxis dataKey="name" scale="band" />
-          <YAxis label={{ value: 'Index', angle: -90, position: 'insideLeft' }} includeHidden={args.includeHidden} />
-          <Legend onClick={handleLegendClick} />
-          <Bar dataKey="pv" fill="blue" hide={!activeKeys.includes('pv')} />
-          <Bar dataKey="amt" fill="green" hide={!activeKeys.includes('amt')} />
-        </ComposedChart>
+        <LineChart width={600} height={300} data={coordinateWithValueData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis />
+          <YAxis {...args} />
+          <Tooltip />
+          <Legend />
+          <Line dataKey="y" />
+        </LineChart>
       </ResponsiveContainer>
     );
   },
   args: {
-    data,
-  },
-  parameters: {
-    controls: { include: ['data', 'includeHidden'] },
-    docs: {
-      description: {
-        story: 'A simple Y Axis. Click on the legend items to toggle their bars on and off.',
-      },
-    },
-  },
-};
-
-export const WithIncludeHidden = {
-  render: (args: ComponentProps<typeof YAxis> & { data: any }) => {
-    const allKeys = Object.keys(args.data[0]);
-    const [activeKeys, setActiveKeys] = useState(allKeys);
-
-    /*
-     * Toggles displayed bars when clicking on a legend item
-     */
-    const handleLegendClick: ComponentProps<typeof Legend>['onClick'] = e => {
-      const key: string = e.dataKey;
-      setActiveKeys(prev => (prev.includes(key) ? prev.filter(k => k !== key) : [...prev, key]));
-    };
-
-    return (
-      <ResponsiveContainer width="100%" height={500}>
-        <ComposedChart data={args.data} margin={{ top: 20, right: 80, left: 20 }}>
-          <XAxis dataKey="name" scale="band" />
-          <YAxis label={{ value: 'Index', angle: -90, position: 'insideLeft' }} includeHidden={args.includeHidden} />
-          <Legend onClick={handleLegendClick} />
-          <Bar dataKey="pv" fill="blue" hide={!activeKeys.includes('pv')} />
-          <Bar dataKey="amt" fill="green" hide={!activeKeys.includes('amt')} />
-        </ComposedChart>
-      </ResponsiveContainer>
-    );
-  },
-  args: {
-    data,
-    includeHidden: true,
-  },
-  parameters: {
-    controls: { include: ['data', 'includeHidden'] },
-    docs: {
-      description: {
-        story: `A simple Y Axis. Click on the legend items to toggle their bars on and off,
-                and notice how the YAxis domain stays the same, compared to the Simple story above.`,
-      },
-    },
+    dataKey: 'pv',
+    domain: [0, 300],
+    type: 'number',
+    allowDataOverflow: true,
+    tickMargin: 20,
+    angle: 45,
+    label: { value: 'The Axis Label', position: 'insideBottomRight', offset: 0 },
   },
 };
