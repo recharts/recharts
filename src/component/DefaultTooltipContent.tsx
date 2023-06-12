6a0c0a1d8cf9e6172063c101cca51b3929455c79
Gabriel Mercier <gabriel.mercier@freeup.world>
2023-06-12 15:36:46 +0100

    Fix for unique keys to be given to tooltip items. Active point now allows index selection (for LineChartSyncId)

--- src/component/DefaultTooltipContent.tsx
+++ src/component/DefaultTooltipContent.tsx
@@ -127,7 +127,11 @@ export class DefaultTooltipContent<TValue extends ValueType, TName extends NameT
                 }
                 return (
                   // eslint-disable-next-line react/no-array-index-key
-                  <li className="recharts-tooltip-item" key={`tooltip-item-${i}`} style={finalItemStyle}>
+                  <li
+                    className="recharts-tooltip-item"
+                    key={`tooltip-item-${payloadLabel}-${i}`}
+                    style={finalItemStyle}
+                  >
                     {isNumOrStr(name) ? <span className="recharts-tooltip-item-name">{name}</span> : null}
                     {isNumOrStr(name) ? <span className="recharts-tooltip-item-separator">{separator}</span> : null}
                     <span className="recharts-tooltip-item-value">{value}</span>
