b2d162afdcd88517322144ce8944f652d3c570dc
Gabriel Mercier <gabriel.mercier@freeup.world>
2023-06-12 18:51:20 +0100

    Added storybook example as a TimeSeries

--- storybook/stories/Examples/TimeSeries.stories.tsx
+++ storybook/stories/Examples/TimeSeries.stories.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { scaleTime } from 'd3-scale';
 import { timeFormat } from 'd3-time-format';
 import { timeDay, timeHour, timeMinute, timeMonth, timeSecond, timeWeek, timeYear } from 'd3-time';
-import { timeData } from '../data';
+import { timeData, temperatureData, particulateData } from '../data';
 import { ComposedChart, Line, ResponsiveContainer, XAxis, Tooltip } from '../../../src';
 import { Props as XAxisProps } from '../../../src/cartesian/XAxis';
 
@@ -40,9 +40,45 @@ const StoryTemplate = {
   },
 };
 
+interface MultipleDataSourceArgs {
+  temperatureData: { begin: Date; temp: number }[];
+  particulateData: { begin: Date; counts: number }[];
+  allowSelectNearestValue: boolean;
+}
+
+const MultipleDataSourceStoryTemplate = {
+  render: (args: MultipleDataSourceArgs) => {
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <ComposedChart
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <XAxis dataKey="begin" {...args} domain={['auto', 'auto']} />
+          <Line data={temperatureData} key="temperature" dataKey="temp" />
+          <Line data={particulateData} key="particulate" dataKey="counts" />
+        </ComposedChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    allowSelectNearestValue: true,
+    temperatureData,
+    particulateData,
+  },
+};
+
 export const DefaultBehaviour = {
   ...StoryTemplate,
-  parameters: { controls: { include: ['type', 'scale', 'domain', 'data'] } },
+  parameters: {
+    controls: {
+      include: ['type', 'scale', 'domain', 'data'],
+    },
+  },
   argTypes: {
     scale: {
       options: [undefined, 'auto', 'ordinal', 'time', 'point', 'linear'],
@@ -123,4 +159,57 @@ export const WithD3Scale = {
     );
   },
   parameters: { controls: { include: ['data'] } },
+};
+
+export const MultipleDataSourceTooltip = {
+  ...MultipleDataSourceStoryTemplate,
+
+  parameters: {
+    controls: {
+      include: ['allowSelectNearestValue', 'temperatureData', 'particulateData'],
+    },
+  },
+  argTypes: {
+    allowSelectNearestValue: {
+      control: 'boolean',
+    },
+  },
+  render: (args: MultipleDataSourceArgs) => {
+    const timeValues = args.particulateData.map(row => row.begin);
+
+    // The d3 scaleTime domain requires numeric values
+    const numericValues = timeValues.map(time => time.valueOf());
+    // With .nice() we extend the domain nicely.
+    const timeScale = scaleTime()
+      .domain([Math.min(...numericValues), Math.max(...numericValues)])
+      .nice();
+
+    const xAxisArgs: XAxisProps = {
+      domain: timeScale.domain().map(date => date.valueOf()),
+      scale: timeScale,
+      type: 'number',
+      ticks: timeScale.ticks(5).map(date => date.valueOf()),
+      tickFormatter: multiFormat,
+      // allowSelectNearestValue: args.allowSelectNearestValue,
+    };
+
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <ComposedChart
+          // data={timeData}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <XAxis dataKey="begin" {...args} {...xAxisArgs} />
+          <Line data={temperatureData} key="temperature" dataKey="temp" />
+          <Line data={particulateData} key="particulate" dataKey="counts" />
+          <Tooltip />
+        </ComposedChart>
+      </ResponsiveContainer>
+    );
+  },
 };
