98299319bf1ff8c0082cf60d4c01a68c589ff3fd
“Nikolas <nikolas@rieble.com>
2023-08-13 20:06:23 +0200

    Remove victory vendor from demo

--- demo/component/AreaChart.tsx
+++ demo/component/AreaChart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import * as React from 'react';
 import * as _ from 'lodash';
 import {
@@ -14,7 +15,7 @@ import {
   LabelList,
   Label,
 } from 'recharts';
-import { curveCardinal } from 'victory-vendor/d3-shape';
+import { curveCardinal } from 'd3-shape';
 import { changeNumberOfData } from './utils';
 
 const data = [
