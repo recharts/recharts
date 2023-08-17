b4491bd35152c8cdb1555a5c5b54620bf478998b
YilunSun <osirisnero@gmail.com>
2023-08-17 01:42:45 -0400

    Storybook: Add customized `Legend` event for `RadialBar` (#3707)

--- storybook/stories/data/Page.ts
+++ storybook/stories/data/Page.ts
@@ -95,4 +95,49 @@ const subjectData = [
   { subject: 'History', A: 65, B: 85, fullMark: 150 },
 ];
 
-export { pageData, numberData, subjectData };
+const pageDataWithFillColor = [
+  {
+    name: '18-24',
+    uv: 31.47,
+    pv: 2400,
+    fill: '#8884d8',
+  },
+  {
+    name: '25-29',
+    uv: 26.69,
+    pv: 4567,
+    fill: '#83a6ed',
+  },
+  {
+    name: '30-34',
+    uv: 15.69,
+    pv: 1398,
+    fill: '#8dd1e1',
+  },
+  {
+    name: '35-39',
+    uv: 8.22,
+    pv: 9800,
+    fill: '#82ca9d',
+  },
+  {
+    name: '40-49',
+    uv: 8.63,
+    pv: 3908,
+    fill: '#a4de6c',
+  },
+  {
+    name: '50+',
+    uv: 2.63,
+    pv: 4800,
+    fill: '#d0ed57',
+  },
+  {
+    name: 'unknow',
+    uv: 6.67,
+    pv: 4800,
+    fill: '#ffc658',
+  },
+];
+
+export { pageData, numberData, subjectData, pageDataWithFillColor };
