05f63f7dd693f7e5937cc24eb744f716149797a3
akamfoad <akamfanaye@gmail.com>
2023-08-20 16:08:47 +0300

    add: use new generate in composed charts

--- src/chart/ComposedChart.tsx
+++ src/chart/ComposedChart.tsx
@@ -1,7 +1,8 @@
 /**
  * @fileOverview Composed Chart
  */
-import { generateCategoricalChart } from './generateCategoricalChart';
+// import { generateCategoricalChart } from './generateCategoricalChart';
+import { generateCategoricalChartFunctional } from './generateCategoricalChartFunctional';
 import { Area } from '../cartesian/Area';
 import { Bar } from '../cartesian/Bar';
 import { Line } from '../cartesian/Line';
@@ -11,7 +12,7 @@ import { YAxis } from '../cartesian/YAxis';
 import { ZAxis } from '../cartesian/ZAxis';
 import { formatAxisMap } from '../util/CartesianUtils';
 
-export const ComposedChart = generateCategoricalChart({
+export const ComposedChart = generateCategoricalChartFunctional({
   chartName: 'ComposedChart',
   GraphicalChild: [Line, Area, Bar, Scatter],
   axisComponents: [
