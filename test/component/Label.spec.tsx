4ba3a478b60d9e02f298d6dc16480d8c062c6ba8
Coltin Kifer <coltinkifer19@gmail.com>
2023-08-23 13:14:18 -0700

    refactor(default-params): label and labellist (#3713)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    refactor label and labellist to use default params
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    #3615 
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    resolve defaultProps issues with function components
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    - run storybook and go through every example to check for errors
    - analyze area where default value would be used
    - these are never used directly after finding component without
    invocation by `Label/LabelList` or by `cloneElement` - these are safe
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have added tests to cover my changes.
    - [x] All new and existing tests passed.
    
    Co-authored-by: Coltin Kifer <ckifer@amazon.com>

--- test/component/Label.spec.tsx
+++ test/component/Label.spec.tsx
@@ -124,4 +124,36 @@ describe('<Label />', () => {
     );
     expect(container.querySelectorAll('.recharts-line .recharts-line-curve').length).toEqual(1);
   });
+
+  it('Renders label by label props with animation disabled', () => {
+    const { container } = render(
+      <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+        <Line
+          type="monotone"
+          dataKey="uv"
+          stroke="#ff7300"
+          label={{ value: 'hello', position: 'center' }}
+          isAnimationActive={false}
+        />
+      </LineChart>,
+    );
+
+    expect(container.querySelectorAll('.recharts-line .recharts-line-curve').length).toEqual(1);
+    expect(screen.getByText(/400/i)).toBeInTheDocument();
+  });
+
+  it('Renders label by label props with animation enabled', () => {
+    jest.useFakeTimers();
+
+    const { container } = render(
+      <LineChart width={400} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+        <Line type="monotone" dataKey="uv" stroke="#ff7300" label={{ value: 'hello', position: 'center' }} />
+      </LineChart>,
+    );
+    // run all timers so animation finishes and labels render
+    jest.runAllTimers();
+
+    expect(container.querySelectorAll('.recharts-line .recharts-line-curve').length).toEqual(1);
+    expect(screen.getByText(/400/i)).toBeInTheDocument();
+  });
 });
