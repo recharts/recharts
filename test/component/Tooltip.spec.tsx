08b32e14f0fc8fb6bb5e0187bb96181534ac912c
Andrew Angelle <andrewangelle@gmail.com>
2023-09-03 10:52:29 -0400

    add unit test

--- test/component/Tooltip.spec.tsx
+++ test/component/Tooltip.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 
-import { Area, AreaChart, Tooltip } from '../../src';
+import { Area, AreaChart, ComposedChart, Line, Tooltip, XAxis, YAxis } from '../../src';
 import { mockMouseEvent } from '../helper/mockMouseEvent';
 
 describe('<Tooltip />', () => {
@@ -54,8 +54,52 @@ describe('<Tooltip />', () => {
       </div>,
     );
 
-    const chart = container.querySelector('.recharts-wrapper');
-    const mouseOverEvent = mockMouseEvent('mouseover', chart!, { pageX: 200, pageY: 200 });
+    const chart = (container.querySelector('.recharts-wrapper') ?? {}) as Element;
+    const mouseOverEvent = mockMouseEvent('mouseover', chart, { pageX: 200, pageY: 200 });
+    mouseOverEvent.fire();
+
+    // After the mouse over event over the chart, the tooltip wrapper still is not set to visible,
+    // but the content is already created based on the nearest data point.
+    const tooltipContentName = container.querySelector('.recharts-tooltip-item-name');
+    const tooltipContentValue = container.querySelector('.recharts-tooltip-item-value');
+    expect(tooltipContentName).toBeInTheDocument();
+    expect(tooltipContentValue).toBeInTheDocument();
+  });
+
+  test('Mouse over renders content with multiple data sets', () => {
+    const areaData = [
+      { category: 'A', value: 0.2 },
+      { category: 'B', value: 0.3 },
+      { category: 'B', value: 0.5 },
+      { category: 'C', value: 0.6 },
+      { category: 'C', value: 0.7 },
+      { category: 'D', value: 0.4 },
+    ];
+
+    const lineData = [
+      { category: 'A', value: null },
+      { category: 'B', value: null },
+      { category: 'B', value: null },
+      { category: 'C', value: 0.2 },
+      { category: 'C', value: 0.4 },
+      { category: 'D', value: 0.6 },
+    ];
+
+    const { container } = render(
+      <div role="main" style={{ width: '400px', height: '400px' }}>
+        <ComposedChart width={400} height={400} data={areaData}>
+          <XAxis dataKey="category" type="category" />
+          <YAxis dataKey="value" />
+          <Tooltip />
+
+          <Area dataKey="value" />
+          <Line dataKey="value" data={lineData} />
+        </ComposedChart>
+      </div>,
+    );
+
+    const chart = (container.querySelector('.recharts-wrapper') ?? {}) as Element;
+    const mouseOverEvent = mockMouseEvent('mouseover', chart, { pageX: 200, pageY: 200 });
     mouseOverEvent.fire();
 
     // After the mouse over event over the chart, the tooltip wrapper still is not set to visible,
