fa8d18263738a24d382acb8a90382a693b7c56b5
Andrew Angelle <andrewangelle@gmail.com>
2023-09-08 08:20:17 -0400

    chore: add comments to change in getTooltipContent

--- src/chart/generateCategoricalChart.tsx
+++ src/chart/generateCategoricalChart.tsx
@@ -232,6 +232,11 @@ const getTooltipContent = (
       return result;
     }
 
+    /**
+     * Fixes: https://github.com/recharts/recharts/issues/3669
+     * Defaulting to chartData below to fix an edge case where the tooltip does not include data from all charts
+     * when a separate dataset is passed to chart prop data and specified on Line/Area/etc prop data
+     */
     const { data = chartData } = child.props;
     let payload;
 
