0a0dbd91a21794e44485f089fa6ca0d1d15c4ba4
Andrew Angelle <andrewangelle@gmail.com>
2023-09-15 19:49:11 -0400

    feat: Implement activeBar for Bar component (#3756)
    <!--- Provide a general summary of your changes in the Title above -->
    ## Description
    Implementing support for `activeBar` prop on Bar component. I attempted
    to follow the same behavior of activeDot for a Line.
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    #3723 
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    Feature request for a consistent interface between Line/Bar for
    customizing the active element that renders via the Tooltip
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    Unit tests and storybook
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    <img width="964" alt="Screenshot 2023-09-13 at 9 19 29 AM"
    src="https://github.com/recharts/recharts/assets/24420033/582fd859-edae-43f5-a346-ae5c358e7515">
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.
    - [x] I have added tests to cover my changes.
    - [x] All new and existing tests passed.

--- src/chart/generateCategoricalChart.tsx
+++ src/chart/generateCategoricalChart.tsx
@@ -1,10 +1,11 @@
-import React, { Component, cloneElement, isValidElement, createElement } from 'react';
+import React, { Component, cloneElement, isValidElement, createElement, ComponentProps, ReactElement } from 'react';
 import classNames from 'classnames';
 import _, { isArray, isBoolean, isNil } from 'lodash';
 import invariant from 'tiny-invariant';
 import { getTicks } from '../cartesian/getTicks';
 import { Surface } from '../container/Surface';
 import { Layer } from '../container/Layer';
+import { Bar } from '../cartesian/Bar';
 import { Tooltip } from '../component/Tooltip';
 import { Legend } from '../component/Legend';
 import { Curve } from '../shape/Curve';
@@ -51,6 +52,7 @@ import {
   parseDomainOfCategoryAxis,
   getTooltipItem,
 } from '../util/ChartUtils';
+import { ActiveBar } from '../util/BarUtils';
 import { detectReferenceElementsDomain } from '../util/DetectReferenceElementsDomain';
 import { inRangeOfSector, polarToCartesian } from '../util/PolarUtils';
 import { shallowEqual } from '../util/ShallowEqual';
@@ -71,6 +73,13 @@ import {
   AxisDomain,
 } from '../util/types';
 import { AccessibilityManager } from './AccessibilityManager';
+
+export type GraphicalItem<Props = Record<string, any>> = ReactElement<
+  Props,
+  string | React.JSXElementConstructor<Props>
+> & {
+  item: ReactElement<Props, string | React.JSXElementConstructor<Props>>;
+};
 
 const ORIENT_MAP = {
   xAxis: ['bottom', 'top'],
@@ -2078,19 +2087,51 @@ export const generateCategoricalChart = ({
       return result;
     };
 
+    renderActiveBar = ({
+      item,
+      childIndex,
+    }: {
+      item: GraphicalItem<ComponentProps<typeof Bar>>;
+      childIndex: number;
+    }) => {
+      const { key } = item.props;
+      const { activeBar, dataKey } = item.item.props;
+
+      /**
+       * Default to inheriting props from the parent bar
+       */
+      const inheritedProps = {
+        ...item.item.props,
+        ...item.props.data[childIndex],
+      };
+
+      const barProps = {
+        index: childIndex,
+        dataKey,
+        fill: getMainColorOfGraphicItem(item.item),
+        strokeWidth: 2,
+        stroke: '#fff',
+        key: `${key}-activeBar-${childIndex}`,
+        ...inheritedProps,
+        ...filterProps(activeBar),
+        ...adaptEventHandlers(activeBar),
+      } as ComponentProps<typeof Bar>;
+
+      return <ActiveBar option={activeBar} {...barProps} />;
+    };
+
     renderGraphicChild = (element: React.ReactElement, displayName: string, index: number): any[] => {
       const item = this.filterFormatItem(element, displayName, index);
       if (!item) {
         return null;
       }
-
       const tooltipEventType = this.getTooltipEventType();
       const { isTooltipActive, tooltipAxis, activeTooltipIndex, activeLabel } = this.state;
       const { children } = this.props;
       const tooltipItem = findChildByType(children, Tooltip);
       const { points, isRange, baseLine } = item.props;
-      const { activeDot, hide } = item.item.props;
-      const hasActive = !hide && isTooltipActive && tooltipItem && activeDot && activeTooltipIndex >= 0;
+      const { activeDot, hide, activeBar } = item.item.props;
+      const hasActive = !hide && isTooltipActive && tooltipItem && (activeDot || activeBar) && activeTooltipIndex >= 0;
       let itemEvents = {};
 
       if (tooltipEventType !== 'axis' && tooltipItem && tooltipItem.props.trigger === 'click') {
@@ -2123,8 +2164,12 @@ export const generateCategoricalChart = ({
           activePoint = findEntryInArray(points, specifiedKey, activeLabel);
           basePoint = isRange && baseLine && findEntryInArray(baseLine, specifiedKey, activeLabel);
         } else {
-          activePoint = points[activeTooltipIndex];
+          activePoint = points?.[activeTooltipIndex];
           basePoint = isRange && baseLine && baseLine[activeTooltipIndex];
+        }
+
+        if (activeBar) {
+          return [graphicalItem, this.renderActiveBar({ item, childIndex: activeTooltipIndex })];
         }
 
         if (!_.isNil(activePoint)) {
