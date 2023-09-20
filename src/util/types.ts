af9c94e03165d250552c7cfcb48977b2c879bbc1
“Nikolas <nikolas@rieble.com>
2023-09-20 15:48:30 +0300

    Add comment to AxisInterval type

--- src/util/types.ts
+++ src/util/types.ts
@@ -1114,6 +1114,13 @@ export interface BaseAxisProps {
   className?: string;
 }
 
+/** Defines how ticks are places and whether / how tick collisions are handled.
+ * 'preserveStart' keeps the left tick on collision and ensures that the first tick is always shown.
+ * 'preserveEnd' keeps the right tick on collision and ensures that the last tick is always shown.
+ * 'preserveStartEnd' keeps the left tick on collision and ensures that the first and last ticks are always shown.
+ * 'equidistantPreserveStart' computes which ticks are shown according to the 'preserveStart' strategy and
+ *  then selects a number N such that every nTh tick will be shown.
+ */
 export type AxisInterval = number | 'preserveStart' | 'preserveEnd' | 'preserveStartEnd' | 'equidistantPreserveStart';
 
 export interface TickItem {
