926caac4fa303e892e554c04d9a1768de76f9507
Brandon Ly <brandonly@lostcoding.com>
2023-09-25 17:50:24 -0500

    Remove unused import

--- test/component/Label.spec.tsx
+++ test/component/Label.spec.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { vi } from 'vitest';
 
 import { Label, Line, LineChart, ReferenceLine, Surface } from '../../src';
 import { cleanupMockAnimation, mockAnimation } from '../helper/animation-frame-helper';
