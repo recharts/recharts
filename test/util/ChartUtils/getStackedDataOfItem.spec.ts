0a0ad7b4fe1364ab0b9e697485694fc61e83a308
Pavel Vanecek <corkscreewe@gmail.com>
2023-10-13 17:07:39 +0900

    Add createStackGroups helper function

--- test/util/ChartUtils/getStackedDataOfItem.spec.ts
+++ test/util/ChartUtils/getStackedDataOfItem.spec.ts
@@ -8,6 +8,21 @@ function makeItem<T>(stackId: T): ReactElement {
   };
 }
 
+function createStackGroups<T>(
+  key: PropertyKey,
+  stackedData: ReadonlyArray<T>,
+  items: Array<ReactElement>,
+): Record<StackId, GenericChildStackGroup<T>> {
+  return {
+    [key]: {
+      stackedData,
+      items,
+      numericAxisId: '',
+      cateAxisId: '',
+    },
+  };
+}
+
 describe('getStackedDataOfItem', () => {
   it('should return null if stackId is undefined', () => {
     // @ts-expect-error incomplete mock of ReactElement
@@ -24,14 +39,7 @@ describe('getStackedDataOfItem', () => {
   it('should return undefined if stack group is undefined', () => {
     const item = makeItem<string>('a');
     const stackedData: Array<string> = [];
-    const stackGroups: Record<StackId, GenericChildStackGroup<string>> = {
-      a: {
-        stackedData,
-        items: [item],
-        numericAxisId: '',
-        cateAxisId: '',
-      },
-    };
+    const stackGroups = createStackGroups('a', stackedData, [item]);
     const result = getStackedDataOfItem(item, stackGroups);
     expect(result).toBe(undefined);
   });
@@ -40,14 +48,7 @@ describe('getStackedDataOfItem', () => {
     const item = makeItem<string>('a');
     const group = Symbol('my mock group');
     const items: Array<ReactElement> = [];
-    const stackGroups: Record<StackId, GenericChildStackGroup<typeof group>> = {
-      a: {
-        stackedData: [group],
-        items,
-        numericAxisId: '',
-        cateAxisId: '',
-      },
-    };
+    const stackGroups = createStackGroups('a', [group], items);
     const result = getStackedDataOfItem(item, stackGroups);
     expect(result).toBe(null);
   });
@@ -56,14 +57,7 @@ describe('getStackedDataOfItem', () => {
     const item1 = makeItem<string>('a');
     const item2 = makeItem<string>('a');
     const group = Symbol('my mock group');
-    const stackGroups: Record<StackId, GenericChildStackGroup<typeof group>> = {
-      a: {
-        stackedData: [group],
-        items: [item1],
-        numericAxisId: '',
-        cateAxisId: '',
-      },
-    };
+    const stackGroups = createStackGroups('a', [group], [item1]);
     const result = getStackedDataOfItem(item2, stackGroups);
     expect(result).toBe(null);
   });
@@ -71,14 +65,7 @@ describe('getStackedDataOfItem', () => {
   it('should return stackedData if stack group contains the item matched by string', () => {
     const item = makeItem<string>('a');
     const group = Symbol('my mock group');
-    const stackGroups: Record<StackId, GenericChildStackGroup<typeof group>> = {
-      a: {
-        stackedData: [group],
-        items: [item],
-        numericAxisId: '',
-        cateAxisId: '',
-      },
-    };
+    const stackGroups = createStackGroups('a', [group], [item]);
     const result = getStackedDataOfItem(item, stackGroups);
     expect(result).toBe(group);
   });
@@ -86,9 +73,7 @@ describe('getStackedDataOfItem', () => {
   it('should return stackedData if stack group contains the item matched by number', () => {
     const item = makeItem<number>(7);
     const group = Symbol('my mock group');
-    const stackGroups: Record<StackId, GenericChildStackGroup<typeof group>> = {
-      7: { stackedData: [group], items: [item], numericAxisId: '', cateAxisId: '' },
-    };
+    const stackGroups = createStackGroups(7, [group], [item]);
     const result = getStackedDataOfItem(item, stackGroups);
     expect(result).toBe(group);
   });
@@ -96,9 +81,7 @@ describe('getStackedDataOfItem', () => {
   it('should return null if stack group contains the item matched by symbol', () => {
     const item = makeItem<symbol>(Symbol.for('mock stack ID'));
     const group = Symbol('my mock group');
-    const stackGroups: Record<StackId, GenericChildStackGroup<typeof group>> = {
-      [Symbol.for('mock stack ID')]: { stackedData: [group], items: [item], cateAxisId: '', numericAxisId: '' },
-    };
+    const stackGroups = createStackGroups(Symbol.for('mock stack ID'), [group], [item]);
     const result = getStackedDataOfItem(item, stackGroups);
     expect(result).toBe(null);
   });
