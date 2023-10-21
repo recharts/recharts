52f258a8050103fee517cffe9e3f8f17a273ef9a
“Nikolas <nikolas@rieble.com>
2023-10-21 20:48:39 +0200

    Import config helper from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [react()],
   test: {
