4e307edfb39a08a4173ac6f31364211718c390d5
Seungwoo Hong <qksn1541@gmail.com>
2023-10-22 21:12:03 +0900

    fix event data

--- src/chart/generateCategoricalChart.tsx
+++ src/chart/generateCategoricalChart.tsx
@@ -1593,9 +1593,7 @@ export const generateCategoricalChart = ({
           mouse = this.getMouseInfo(e as React.MouseEvent);
         }
 
-        const handler = event;
-
-        handler(mouse, e);
+        event(mouse ?? {}, e);
       }
     };
 
