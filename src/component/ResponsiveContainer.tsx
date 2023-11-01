8c7a37ae55fcde3dec98724b10bcfd64ea24f492
Hong Seungwoo <qksn1541@gmail.com>
2023-11-01 15:37:03 +0900

    Fix ResponsiveContainer (#3921)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] I have added a storybook story or extended an existing story to
    show my changes
    - [x] All new and existing tests passed.

--- src/component/ResponsiveContainer.tsx
+++ src/component/ResponsiveContainer.tsx
@@ -15,8 +15,10 @@ import React, {
   useCallback,
 } from 'react';
 import throttle from 'lodash/throttle';
+import { isElement } from 'react-is';
 import { isPercent } from '../util/DataUtils';
 import { warn } from '../util/LogUtils';
+import { getDisplayName } from '../util/ReactUtils';
 
 export interface Props {
   aspect?: number;
@@ -162,11 +164,30 @@ export const ResponsiveContainer = forwardRef<HTMLDivElement, Props>(
         aspect,
       );
 
-      return cloneElement(children, {
-        width: calculatedWidth,
-        height: calculatedHeight,
-        // calculate the actual size and override it.
-        style: { height: '100%', width: '100%', maxHeight: calculatedHeight, maxWidth: calculatedWidth },
+      const isCharts =
+        !Array.isArray(children) && isElement(children) && getDisplayName(children.type).endsWith('Chart');
+
+      return React.Children.map(children, child => {
+        if (isElement(child)) {
+          return cloneElement(child, {
+            width: calculatedWidth,
+            height: calculatedHeight,
+            // calculate the actual size and override it.
+            ...(isCharts
+              ? {
+                  style: {
+                    height: '100%',
+                    width: '100%',
+                    maxHeight: calculatedHeight,
+                    maxWidth: calculatedWidth,
+                    // keep components style
+                    ...child.props.style,
+                  },
+                }
+              : {}),
+          });
+        }
+        return child;
       });
     }, [aspect, children, height, maxHeight, minHeight, minWidth, sizes, width]);
 
