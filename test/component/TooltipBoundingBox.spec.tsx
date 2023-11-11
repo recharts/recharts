829a4b89b36028e7a95052705297bd7cadffbce6
Pavel Vaněček <corkscreewe+github@gmail.com>
2023-11-11 14:16:12 +1100

    Add TooltipBoundingBox component

---
+++ test/component/TooltipBoundingBox.spec.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, it, expect } from 'vitest';
+import { TooltipBoundingBox, TooltipBoundingBoxProps } from '../../src/component/TooltipBoundingBox';
+
+describe('TooltipBoundingBox', () => {
+  const defaultProps: TooltipBoundingBoxProps = {
+    active: true,
+    hasPayload: true,
+    children: 'Hello world!',
+    coordinate: undefined,
+  };
+  it('should render children when active prop is true', () => {
+    render(<TooltipBoundingBox {...defaultProps} />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).toBeVisible();
+  });
+
+  it('should hide children when active prop is false', () => {
+    render(<TooltipBoundingBox {...defaultProps} active={false} />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).not.toBeVisible();
+  });
+
+  it('should hide children when there is no payload', () => {
+    render(<TooltipBoundingBox {...defaultProps} hasPayload={false} />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).not.toBeVisible();
+  });
+
+  it('should hide children when dismissed using Escape key', () => {
+    render(<TooltipBoundingBox {...defaultProps} active={false} />);
+    userEvent.keyboard('{Escape}');
+    screen.debug();
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).not.toBeVisible();
+  });
+});
