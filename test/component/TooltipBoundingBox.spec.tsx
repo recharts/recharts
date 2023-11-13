9e473fe2dcca0d25677ac79149ba452ac15b757a
Pavel Vaněček <corkscreewe+github@gmail.com>
2023-11-13 20:32:51 +1100

    Split Tooltip into TooltipBoundingBox (#3951)
    ## Description
    
    I think this is an opportunity to have two clearly defined components
    with split responsibility. One doing the position (BoundingBox) and one
    doing the content (Tooltip).
    
    ## Related Issue
    
    ## Motivation and Context
    
    ## How Has This Been Tested?
    
    unit tests
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    - [X] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [X] I have added tests to cover my changes.
    - [ ] I have added a storybook story or extended an existing story to
    show my changes
    - [X] All new and existing tests passed.

---
+++ test/component/TooltipBoundingBox.spec.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, it, expect } from 'vitest';
+import { TooltipBoundingBox, TooltipBoundingBoxProps } from '../../src/component/TooltipBoundingBox';
+
+describe('TooltipBoundingBox', () => {
+  const defaultProps: TooltipBoundingBoxProps = {
+    active: true,
+    hasPayload: true,
+    children: 'Hello world!',
+    coordinate: undefined,
+    allowEscapeViewBox: {
+      x: false,
+      y: false,
+    },
+    animationDuration: 0,
+    animationEasing: 'ease',
+    isAnimationActive: false,
+    offset: 0,
+    position: undefined,
+    reverseDirection: {
+      x: false,
+      y: false,
+    },
+    useTranslate3d: false,
+    viewBox: undefined,
+    wrapperStyle: undefined,
+  };
+  it('should render children when active prop is true', () => {
+    render(<TooltipBoundingBox {...defaultProps} />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).toBeVisible();
+  });
+
+  it('should hide children when active prop is false', () => {
+    render(<TooltipBoundingBox {...defaultProps} active={false} />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).not.toBeVisible();
+  });
+
+  it('should hide children when there is no payload', () => {
+    render(<TooltipBoundingBox {...defaultProps} hasPayload={false} />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).not.toBeVisible();
+  });
+
+  it('should hide children when dismissed using Escape key', () => {
+    render(<TooltipBoundingBox {...defaultProps} active={false} />);
+    userEvent.keyboard('{Escape}');
+    screen.debug();
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello world!')).not.toBeVisible();
+  });
+});
