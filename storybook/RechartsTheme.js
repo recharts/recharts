7b2ec86a405743e45ea38310f475265e876c4906
Caleb Diehl <cdiehl@ap.org>
2023-12-10 19:16:05 -0800

    add theme

---
+++ storybook/RechartsTheme.js
@@ -0,0 +1,38 @@
+import { create } from '@storybook/theming/create';
+import logo from './recharts_logo.png';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Recharts',
+  brandUrl: 'https://recharts.org/en-US/',
+  brandImage: logo,
+  brandTarget: '_self',
+
+  fontBase: '"Source Sans Pro", "Hiragino Sans GB", Arial, sans-serif',
+  fontCode: '"Roboto Mono", Consolas, "Liberation Mono", "Courier New", Courier, monospace',
+
+  colorPrimary: '#1281ca',
+  colorSecondary: '#2e96ea',
+
+  // UI
+  appBg: '#FCFCFC',
+  appContentBg: '#ffffff',
+  appPreviewBg: '#ffffff',
+  appBorderColor: '#eee',
+  appBorderRadius: 5,
+
+  // Text colors
+  textColor: '#666',
+  textInverseColor: '#333',
+
+  // Toolbar default and active colors
+  barTextColor: '#333',
+  barSelectedColor: '#1181CA',
+  barBg: '#FCFCFC',
+
+  // Form colors
+  inputBg: '#F5F5F5',
+  inputBorder: '#999',
+  inputTextColor: '#333',
+  inputBorderRadius: 2,
+});
---
+++ storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import RechartsTheme from './RechartsTheme';
+
+addons.setConfig({
+  theme: RechartsTheme,
+});
