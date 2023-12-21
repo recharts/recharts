c97e31999112b3e5dc22e7ec16fd02840448797e
Coltin Kifer <ckifer@amazon.com>
2023-12-21 06:37:36 +0000

    fix: tests that are logging errors

--- test/util/FunnelUtils.spec.tsx
+++ test/util/FunnelUtils.spec.tsx
@@ -25,19 +25,25 @@ describe('funnelUtils', () => {
     expect(res).toEqual(mockRes);
   });
   it('<FunnelTrapezoid /> returns a trapezoid shape with no options', () => {
-    const { container } = render(<FunnelTrapezoid option={{}} {...mockProps} isActive={Boolean(true)} />);
+    const { container } = render(
+      <svg width={100} height={100}>
+        <FunnelTrapezoid option={{}} {...mockProps} isActive={Boolean(true)} />
+      </svg>,
+    );
     expect(container.querySelector('g')).toHaveClass('recharts-active-shape');
   });
   it('<FunnelTrapezoid /> returns a trapezoid shape with x & y from options', () => {
     const { container } = render(
-      <FunnelTrapezoid
-        option={{ x: 10, y: 10 }}
-        {...{
-          isActive: true,
-          height: 100,
-        }}
-        isActive={Boolean(true)}
-      />,
+      <svg width={100} height={100}>
+        <FunnelTrapezoid
+          option={{ x: 10, y: 10 }}
+          {...{
+            isActive: true,
+            height: 100,
+          }}
+          isActive={Boolean(true)}
+        />
+      </svg>,
     );
     expect(container.querySelector('g')).toHaveClass('recharts-active-shape');
   });
