70f730083bd8e06b44d5449e0aae68301336d885
Anuj Sharma <19ucc109@lnmiit.ac.in>
2024-01-10 21:51:39 +0530

    fix: Errorbar breaking at category XAxis type! (#4042)

--- src/cartesian/ErrorBar.tsx
+++ src/cartesian/ErrorBar.tsx
@@ -2,6 +2,7 @@
  * @fileOverview Render a group of error bar
  */
 import React, { SVGProps } from 'react';
+import invariant from 'tiny-invariant';
 import { Layer } from '../container/Layer';
 import { Props as XAxisProps } from './XAxis';
 import { Props as YAxisProps } from './YAxis';
@@ -49,6 +50,12 @@ export type Props = SVGProps<SVGLineElement> & ErrorBarProps;
 export function ErrorBar(props: Props) {
   const { offset, layout, width, dataKey, data, dataPointFormatter, xAxis, yAxis, ...others } = props;
   const svgProps = filterProps(others);
+
+  invariant(
+    !(props.direction === 'x' && xAxis.type !== 'number'),
+    'ErrorBar requires Axis type property to be "number".',
+  );
+
   const errorBars = data.map((entry: any) => {
     const { x, y, value, errorVal } = dataPointFormatter(entry, dataKey);
 
