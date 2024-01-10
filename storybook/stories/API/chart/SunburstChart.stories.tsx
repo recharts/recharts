b0c71e3b8558065f24ec9f3a1ca18eba0e58a25c
Caleb Diehl <59377951+csdiehl@users.noreply.github.com>
2024-01-09 23:12:15 -0800

    Sunburst chart (#4037)
    ## Description
    
    Adds an entirely new chart type, the Sunburst Chart - resolves #3842. 
    
    Note: this is a WIP. I've added a Typescript component for a responsive
    sunburst chart, a story to demonstrate the functionality, and mock data.
    I still need to add event handlers, and a tooltip - I wanted to give
    this a chance to be reviewed before changes grow too large, and I'm
    looking for guidance on the best way to integrate these elements to
    match the style of existing components.
    
    ## Related Issue
    
    #3842 
    
    ## Motivation and Context
    
    This adds a popular and useful new hierarchical chart type that is not
    addressed by any existing component in the library.
    
    ## How Has This Been Tested?
    
    I have not added tests yet. 
    
    ## Screenshots (if appropriate):
    
    Current state of the chart - responsive dimensions and labels, still
    needs interactivity.
    
    <img width="660" alt="Screenshot 2024-01-03 at 6 59 24 PM"
    src="https://github.com/recharts/recharts/assets/59377951/6fbad951-f864-42c0-9c55-d70a4bf76b6e">
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [X] My code follows the code style of this project.
    - [X] My change requires a change to the documentation.
    - [X] I have updated the documentation accordingly.
    - [X] I have added tests to cover my changes.
    - [X] I have added a storybook story or extended an existing story to
    show my changes
    - [X] All new and existing tests passed.

---
+++ storybook/stories/API/chart/SunburstChart.stories.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import { ResponsiveContainer, SunburstChart, Tooltip } from '../../../../src';
+import { SunburstData } from '../../../../src/chart/SunburstChart';
+import { CategoricalChartProps, ChartSizeProps, data, dataKey, margin } from '../props/ChartProps';
+
+const { innerRadius, outerRadius, cx, cy, startAngle, endAngle, onClick, onMouseEnter, onMouseLeave, className } =
+  CategoricalChartProps;
+
+export default {
+  argTypes: {
+    data,
+    margin,
+    ...ChartSizeProps,
+    dataKey,
+    innerRadius,
+    outerRadius,
+    cx,
+    cy,
+    startAngle,
+    endAngle,
+    onClick,
+    onMouseEnter,
+    onMouseLeave,
+    className,
+  },
+  component: SunburstChart,
+};
+
+const hierarchy: SunburstData = {
+  name: 'Root',
+  value: 100,
+  children: [
+    {
+      name: 'Child1',
+      fill: '#264653',
+      value: 30,
+      children: [
+        {
+          name: 'third child',
+          value: 10,
+        },
+        {
+          name: 'another child',
+          value: 5,
+        },
+        {
+          name: 'next child',
+          value: 15,
+          children: [
+            {
+              name: 'third level child',
+              value: 5,
+            },
+            {
+              name: 'third level child',
+              value: 5,
+            },
+            {
+              name: 'third level child',
+              value: 5,
+              children: [{ name: 'level 4', value: 2 }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Child2',
+      fill: '#2a9d8f',
+      value: 20,
+      children: [
+        {
+          name: 'another child',
+          value: 10,
+        },
+        {
+          name: 'next child',
+          value: 10,
+          children: [
+            { name: 'level 3 of child 2', value: 5 },
+            { name: 'level 3 of child 2', value: 3 },
+            { name: 'level 3 of child 2', value: 2 },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Child3',
+      fill: '#e9c46a',
+      value: 20,
+    },
+    {
+      name: 'Child4',
+      fill: '#F4A261',
+      value: 10,
+      children: [
+        { name: 'child4 child', value: 5 },
+        { name: 'child4 child', value: 5 },
+      ],
+    },
+    {
+      name: 'Child5',
+      fill: '#e76f51',
+      value: 20,
+    },
+  ],
+};
+
+export const Sunburst = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={450}>
+        <SunburstChart {...args} />
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data: hierarchy,
+    innerRadius: 40,
+  },
+};
+
+export const WithStartAndEndAngle = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={450}>
+        <SunburstChart {...args} />
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data: hierarchy,
+    innerRadius: 40,
+    startAngle: 90,
+    endAngle: 270,
+  },
+};
+
+export const WithTooltip = {
+  render: (args: Record<string, any>) => {
+    return (
+      <ResponsiveContainer width="100%" height={450}>
+        <SunburstChart {...args}>
+          <Tooltip />
+        </SunburstChart>
+      </ResponsiveContainer>
+    );
+  },
+  args: {
+    data: hierarchy,
+    innerRadius: 40,
+  },
+};
