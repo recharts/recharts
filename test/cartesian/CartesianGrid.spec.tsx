d89dad523d98f019471a9e3e3e7f6868860ba51e
Pavel Vanecek <corkscreewe@gmail.com>
2024-01-20 10:30:57 +1100

    Little bit of structure to the test file

--- test/cartesian/CartesianGrid.spec.tsx
+++ test/cartesian/CartesianGrid.spec.tsx
@@ -7,184 +7,193 @@ describe('<CartesianGrid />', () => {
   const horizontalPoints = [10, 20, 30, 100, 400];
   const verticalPoints = [100, 200, 300, 400];
 
-  test('Render 5 horizontal lines and 4 vertical lines in simple CartesianGrid', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>,
-    );
-    expect.soft(container.querySelectorAll('line')).toHaveLength(9);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
-      .toHaveLength(horizontalPoints.length);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
-      .toHaveLength(verticalPoints.length);
+  describe('basic features', () => {
+    test('Render 5 horizontal lines and 4 vertical lines in simple CartesianGrid', () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+          />
+        </Surface>,
+      );
+      expect.soft(container.querySelectorAll('line')).toHaveLength(9);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
+        .toHaveLength(horizontalPoints.length);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
+        .toHaveLength(verticalPoints.length);
+    });
+
+    test("Don't render any lines when verticalPoints and horizontalPoints are empty", () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid width={500} height={500} />
+        </Surface>,
+      );
+      expect(container.querySelectorAll('line')).toHaveLength(0);
+    });
+
+    test.each([0, -1, NaN, Infinity])("Don't render any lines when width is %s", w => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid width={w} height={500} verticalPoints={verticalPoints} horizontalPoints={horizontalPoints} />
+        </Surface>,
+      );
+      expect(container.querySelectorAll('line')).toHaveLength(0);
+    });
+
+    test.each([0, -1, NaN, Infinity])("Don't render any lines when height is %s", h => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid width={h} height={500} verticalPoints={verticalPoints} horizontalPoints={horizontalPoints} />
+        </Surface>,
+      );
+      expect(container.querySelectorAll('line')).toHaveLength(0);
+    });
   });
 
-  test("Don't render any lines when verticalPoints and horizontalPoints are empty", () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid width={500} height={500} />
-      </Surface>,
-    );
-    expect(container.querySelectorAll('line')).toHaveLength(0);
+  describe('horizontalPoints without generator', () => {
+    it('should not render any lines if horizontal=false', () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            horizontal={false}
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+          />
+        </Surface>,
+      );
+      expect.soft(container.querySelectorAll('line')).toHaveLength(verticalPoints.length);
+      expect.soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line')).toHaveLength(0);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
+        .toHaveLength(verticalPoints.length);
+    });
+
+    it('should render all lines if horizontal=true', () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            horizontal
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+          />
+        </Surface>,
+      );
+      expect.soft(container.querySelectorAll('line')).toHaveLength(9);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
+        .toHaveLength(horizontalPoints.length);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
+        .toHaveLength(verticalPoints.length);
+    });
+
+    it('should render all lines if horizontal is undefined', () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+          />
+        </Surface>,
+      );
+      expect.soft(container.querySelectorAll('line')).toHaveLength(9);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
+        .toHaveLength(horizontalPoints.length);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
+        .toHaveLength(verticalPoints.length);
+    });
   });
 
-  test.each([0, -1, NaN, Infinity])("Don't render any lines when width is %s", w => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid width={w} height={500} verticalPoints={verticalPoints} horizontalPoints={horizontalPoints} />
-      </Surface>,
-    );
-    expect(container.querySelectorAll('line')).toHaveLength(0);
+  describe('verticalPoints without generator', () => {
+    it('should not render any lines if vertical=false', () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            vertical={false}
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+          />
+        </Surface>,
+      );
+      expect.soft(container.querySelectorAll('line')).toHaveLength(horizontalPoints.length);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
+        .toHaveLength(horizontalPoints.length);
+      expect.soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line')).toHaveLength(0);
+    });
+
+    it('should render all lines if vertical=true', () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            vertical
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+          />
+        </Surface>,
+      );
+      expect.soft(container.querySelectorAll('line')).toHaveLength(9);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
+        .toHaveLength(horizontalPoints.length);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
+        .toHaveLength(verticalPoints.length);
+    });
+
+    it('should render all lines if vertical is undefined', () => {
+      const { container } = render(
+        <Surface width={500} height={500}>
+          <CartesianGrid
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            verticalPoints={verticalPoints}
+            horizontalPoints={horizontalPoints}
+          />
+        </Surface>,
+      );
+      expect.soft(container.querySelectorAll('line')).toHaveLength(9);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
+        .toHaveLength(horizontalPoints.length);
+      expect
+        .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
+        .toHaveLength(verticalPoints.length);
+    });
   });
 
-  test.each([0, -1, NaN, Infinity])("Don't render any lines when height is %s", h => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid width={h} height={500} verticalPoints={verticalPoints} horizontalPoints={horizontalPoints} />
-      </Surface>,
-    );
-    expect(container.querySelectorAll('line')).toHaveLength(0);
-  });
-
-  it('should not render any lines if horizontal=false', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          horizontal={false}
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>,
-    );
-    expect.soft(container.querySelectorAll('line')).toHaveLength(verticalPoints.length);
-    expect.soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line')).toHaveLength(0);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
-      .toHaveLength(verticalPoints.length);
-  });
-
-  it('should render all lines if horizontal=true', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          horizontal
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>,
-    );
-    expect.soft(container.querySelectorAll('line')).toHaveLength(9);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
-      .toHaveLength(horizontalPoints.length);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
-      .toHaveLength(verticalPoints.length);
-  });
-
-  it('should render all lines if horizontal is undefined', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>,
-    );
-    expect.soft(container.querySelectorAll('line')).toHaveLength(9);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
-      .toHaveLength(horizontalPoints.length);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
-      .toHaveLength(verticalPoints.length);
-  });
-
-  it('should not render any lines if vertical=false', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          vertical={false}
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>,
-    );
-    expect.soft(container.querySelectorAll('line')).toHaveLength(horizontalPoints.length);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
-      .toHaveLength(horizontalPoints.length);
-    expect.soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line')).toHaveLength(0);
-  });
-
-  it('should render all lines if vertical=true', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          vertical
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>,
-    );
-    expect.soft(container.querySelectorAll('line')).toHaveLength(9);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
-      .toHaveLength(horizontalPoints.length);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
-      .toHaveLength(verticalPoints.length);
-  });
-
-  it('should render all lines if vertical is undefined', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <CartesianGrid
-          x={0}
-          y={0}
-          width={500}
-          height={500}
-          verticalPoints={verticalPoints}
-          horizontalPoints={horizontalPoints}
-        />
-      </Surface>,
-    );
-    expect.soft(container.querySelectorAll('line')).toHaveLength(9);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-horizontal line'))
-      .toHaveLength(horizontalPoints.length);
-    expect
-      .soft(container.querySelectorAll('.recharts-cartesian-grid-vertical line'))
-      .toHaveLength(verticalPoints.length);
-  });
+  describe.todo('horizontalCoordinatesGenerator');
+  describe.todo('verticalCoordinatesGenerator');
 });
