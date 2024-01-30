0aaa4149c270f1dc0a557a13c250fdf7a801b63b
Pavel Vanecek <corkscreewe@gmail.com>
2024-01-30 23:39:11 +1100

    More Bar tests

--- test/cartesian/Bar.spec.tsx
+++ test/cartesian/Bar.spec.tsx
@@ -1,5 +1,5 @@
 import React, { ComponentType, ReactNode } from 'react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test, it, vi } from 'vitest';
 import { render } from '@testing-library/react';
 import uniqueId from 'lodash/uniqueId';
 import {
@@ -8,6 +8,8 @@ import {
   BarChart,
   ComposedChart,
   FunnelChart,
+  Legend,
+  LegendType,
   LineChart,
   PieChart,
   RadarChart,
@@ -153,43 +155,210 @@ describe.each(chartsThatSupportBar)('<Bar /> as a child of $testName', ({ ChartE
   });
 
   describe('label', () => {
-    it('should draw default labels when label = true', () => {
-      const { container } = render(
-        <ChartElement width={500} height={500}>
-          <Bar isAnimationActive={false} data={data} label dataKey="value" />
-        </ChartElement>,
-      );
-      const labels = container.querySelectorAll('.recharts-text.recharts-label');
-      expect(labels).toHaveLength(data.length);
-      labels.forEach(l => {
-        expect(l).toHaveAttribute('x', expect.any(String));
-        expect(l).toHaveAttribute('y', '75');
-        expect(l).toHaveAttribute('height', '50');
-        expect(l).toHaveAttribute('offset', '5');
-        expect(l).toHaveAttribute('text-anchor', 'middle');
-        expect(l).toHaveAttribute('width', '20');
-        expect(l).toHaveAttribute('fill', '#808080');
+    describe('as boolean', () => {
+      it('should draw default labels when label = true', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500}>
+            <Bar isAnimationActive={false} data={data} label dataKey="value" />
+          </ChartElement>,
+        );
+        const labels = container.querySelectorAll('.recharts-text.recharts-label');
+        expect(labels).toHaveLength(data.length);
+        labels.forEach(l => {
+          expect(l).toHaveAttribute('x', expect.any(String));
+          expect(l).toHaveAttribute('y', '75');
+          expect(l).toHaveAttribute('height', '50');
+          expect(l).toHaveAttribute('offset', '5');
+          expect(l).toHaveAttribute('text-anchor', 'middle');
+          expect(l).toHaveAttribute('width', '20');
+          expect(l).toHaveAttribute('fill', '#808080');
+        });
+      });
+
+      it('should not draw labels while animating', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500}>
+            <Bar isAnimationActive data={data} label dataKey="value" />
+          </ChartElement>,
+        );
+        const labels = container.querySelectorAll('.recharts-text.recharts-label');
+        expect(labels).toHaveLength(0);
+      });
+
+      it('should not draw labels when label = false', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500}>
+            <Bar isAnimationActive={false} data={data} label={false} dataKey="value" />
+          </ChartElement>,
+        );
+        const labels = container.querySelectorAll('.recharts-text.recharts-label');
+        expect(labels).toHaveLength(0);
       });
     });
 
-    it('should not draw labels while animating', () => {
-      const { container } = render(
-        <ChartElement width={500} height={500}>
-          <Bar isAnimationActive data={data} label dataKey="value" />
-        </ChartElement>,
-      );
-      const labels = container.querySelectorAll('.recharts-text.recharts-label');
-      expect(labels).toHaveLength(0);
+    describe('as svg properties object', () => {
+      it('should draw labels and add extra props from the object', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500}>
+            <Bar
+              isAnimationActive={false}
+              data={data}
+              label={{
+                fill: 'red',
+                elevation: 9,
+              }}
+              dataKey="value"
+            />
+          </ChartElement>,
+        );
+        const labels = container.querySelectorAll('.recharts-text.recharts-label');
+        expect(labels).toHaveLength(data.length);
+        labels.forEach(l => {
+          expect(l).toHaveAttribute('x', expect.any(String));
+          expect(l).toHaveAttribute('y', '75');
+          expect(l).toHaveAttribute('height', '50');
+          expect(l).toHaveAttribute('offset', '5');
+          expect(l).toHaveAttribute('text-anchor', 'middle');
+          expect(l).toHaveAttribute('width', '20');
+          expect(l).toHaveAttribute('fill', 'red');
+          expect(l).toHaveAttribute('elevation', '9');
+        });
+      });
+
+      it('should overwrite the recharts-label className but keep recharts-text className', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500}>
+            <Bar
+              isAnimationActive={false}
+              data={data}
+              label={{
+                className: 'my-test-class',
+              }}
+              dataKey="value"
+            />
+          </ChartElement>,
+        );
+        const labels = container.querySelectorAll('.recharts-text.recharts-label');
+        expect(labels).toHaveLength(0);
+        const customLabels = container.querySelectorAll('.my-test-class');
+        expect(customLabels).toHaveLength(data.length);
+        customLabels.forEach(l => {
+          expect(l).toHaveAttribute('class', 'recharts-text my-test-class');
+        });
+      });
     });
 
-    it('should not draw labels when label = false', () => {
+    describe('as function', () => {
+      it('should pass props to the function', () => {
+        const spy = vi.fn().mockReturnValue(null);
+        render(
+          <ChartElement width={500} height={500}>
+            <Bar isAnimationActive={false} data={data} label={spy} dataKey="value" />
+          </ChartElement>,
+        );
+        expect(spy).toHaveBeenCalledTimes(data.length);
+        expect(spy).toBeCalledWith(
+          {
+            content: spy,
+            height: 50,
+            index: expect.any(Number),
+            offset: 5,
+            parentViewBox: undefined,
+            textBreakAll: undefined,
+            value: expect.any(Number),
+            viewBox: {
+              height: 50,
+              width: 20,
+              x: expect.any(Number),
+              y: 50,
+            },
+            width: 20,
+            x: expect.any(Number),
+            y: 50,
+          },
+          {}, // this object arrives as a second argument, I am not sure where that comes from
+        );
+      });
+
+      it('should render what the function returned', () => {
+        const labelFn = () => <g className="my-mock-class" />;
+        const { container } = render(
+          <ChartElement width={500} height={500}>
+            <Bar isAnimationActive={false} data={data} label={labelFn} dataKey="value" />
+          </ChartElement>,
+        );
+        const labels = container.querySelectorAll('.my-mock-class');
+        expect(labels).toHaveLength(data.length);
+        labels.forEach(l => {
+          expect.soft(l).not.toHaveAttribute('x');
+          expect.soft(l).not.toHaveAttribute('y');
+          expect.soft(l).not.toHaveAttribute('height');
+          expect.soft(l).not.toHaveAttribute('offset');
+          expect.soft(l).not.toHaveAttribute('text-anchor');
+          expect.soft(l).not.toHaveAttribute('width');
+          expect.soft(l).not.toHaveAttribute('fill');
+        });
+      });
+    });
+
+    describe('as a custom Element', () => {
+      it(`should render what the function returned, and then inject extra sneaky props in it 
+                - but not all of them, and not the same as in the other ways of rendering labels`, () => {
+        const MyLabel = <g className="my-mock-class" />;
+        const { container } = render(
+          <ChartElement width={500} height={500}>
+            <Bar isAnimationActive={false} data={data} label={MyLabel} dataKey="value" />
+          </ChartElement>,
+        );
+        const labels = container.querySelectorAll('.my-mock-class');
+        expect(labels).toHaveLength(data.length);
+        labels.forEach(l => {
+          expect.soft(l).toHaveAttribute('x', expect.any(String));
+          expect.soft(l).toHaveAttribute('y', '50'); // this number is different from the other renders - not sure why
+          expect.soft(l).toHaveAttribute('height', '50');
+          expect.soft(l).toHaveAttribute('offset', '5');
+          expect.soft(l).not.toHaveAttribute('text-anchor', 'middle');
+          expect.soft(l).toHaveAttribute('width', '20');
+          expect.soft(l).not.toHaveAttribute('fill', '#808080');
+        });
+      });
+    });
+  });
+
+  describe('legendType', () => {
+    const allLegendTypesExceptNone: ReadonlyArray<LegendType> = [
+      'circle',
+      'cross',
+      'diamond',
+      'line',
+      'plainline',
+      'rect',
+      'square',
+      'star',
+      'triangle',
+      'wye',
+    ];
+
+    test.each(allLegendTypesExceptNone)('should render legendType %s', legendType => {
       const { container } = render(
         <ChartElement width={500} height={500}>
-          <Bar isAnimationActive={false} data={data} label={false} dataKey="value" />
+          <Bar data={data} dataKey="value" legendType={legendType} />
+          <Legend />
         </ChartElement>,
       );
-      const labels = container.querySelectorAll('.recharts-text.recharts-label');
-      expect(labels).toHaveLength(0);
+      const legendIcon = container.querySelectorAll('.recharts-legend-item-text');
+      expect(legendIcon).toHaveLength(1);
+    });
+
+    it('should not render any legend if legendType = none', () => {
+      const { container } = render(
+        <ChartElement width={500} height={500}>
+          <Bar data={data} dataKey="value" legendType="none" />
+          <Legend />
+        </ChartElement>,
+      );
+      const legendIcon = container.querySelectorAll('.recharts-legend-item-text');
+      expect(legendIcon).toHaveLength(0);
     });
   });
 });
