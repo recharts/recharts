221af4caceb8de65c7df5a6a7f453dd8ad19eb9c
Pavel Vanecek <corkscreewe@gmail.com>
2024-01-31 21:02:37 +1100

    Parametrize Area tests

--- test/cartesian/Area.spec.tsx
+++ test/cartesian/Area.spec.tsx
@@ -1,16 +1,51 @@
-import React, { FC } from 'react';
+import React, { ComponentType, FC, ReactNode } from 'react';
 import { render, screen } from '@testing-library/react';
-import { Area, Surface } from '../../src';
+import {
+  Area,
+  AreaChart,
+  BarChart,
+  ComposedChart,
+  FunnelChart,
+  LineChart,
+  PieChart,
+  RadarChart,
+  RadialBarChart,
+  ScatterChart,
+} from '../../src';
 
-describe('<Area />', () => {
-  const data = [
-    { x: 10, y: 50, value: 100 },
-    { x: 50, y: 50, value: 100 },
-    { x: 90, y: 50, value: 100 },
-    { x: 130, y: 50, value: 100 },
-    { x: 170, y: 50, value: 100 },
-  ];
+type TestCase = {
+  ChartElement: ComponentType<{ children?: ReactNode; width?: number; height?: number; data?: any[] }>;
+  testName: string;
+};
 
+const chartsThatSupportArea: ReadonlyArray<TestCase> = [
+  { ChartElement: ComposedChart, testName: 'ComposedChart' },
+  { ChartElement: AreaChart, testName: 'AreaElement' },
+];
+
+const chartsThatDoNotSupportArea: ReadonlyArray<TestCase> = [
+  { ChartElement: BarChart, testName: 'BarChart' },
+  { ChartElement: LineChart, testName: 'LineChart' },
+  { ChartElement: ScatterChart, testName: 'ScatterChart' },
+  { ChartElement: PieChart, testName: 'PieChart' },
+  { ChartElement: RadarChart, testName: 'RadarChart' },
+  { ChartElement: RadialBarChart, testName: 'RadialBarChart' },
+  { ChartElement: ScatterChart, testName: 'ScatterChart' },
+  { ChartElement: FunnelChart, testName: 'FunnelChart' },
+  // Treemap and Sankey do not accept children
+  // { ChartElement: Treemap, testName: 'Treemap' },
+  // { ChartElement: Sankey, testName: 'Sankey' },
+];
+
+const data = [
+  { x: 10, y: 50, value: 100 },
+  { x: 50, y: 50, value: 100 },
+  { x: 90, y: 50, value: 100 },
+  { x: 130, y: 50, value: 100 },
+  { x: 170, y: 50, value: 100 },
+];
+
+describe.each(chartsThatSupportArea)('<Area /> as a child of $testName', ({ ChartElement }) => {
   test('Render customized label when label is set to be a function', () => {
     const renderLabel = (props: { index: number; x: number; y: number }) => {
       const { index, x, y } = props;
@@ -23,9 +58,9 @@ describe('<Area />', () => {
     };
 
     render(
-      <Surface width={500} height={500}>
-        <Area dataKey="value" isAnimationActive={false} baseLine={200} points={data} label={renderLabel} />
-      </Surface>,
+      <ChartElement width={500} height={500} data={data}>
+        <Area dataKey="value" isAnimationActive={false} baseLine={200} label={renderLabel} />
+      </ChartElement>,
     );
 
     expect(screen.getAllByText('Customized Label')).toHaveLength(data.length);
@@ -43,15 +78,14 @@ describe('<Area />', () => {
     };
 
     render(
-      <Surface width={500} height={500}>
+      <ChartElement width={500} height={500} data={data}>
         <Area
           dataKey="value"
           isAnimationActive={false}
           baseLine={200}
-          points={data}
           label={({ index, x, y }: { index: number; x: number; y: number }) => <Label index={index} x={x} y={y} />}
         />
-      </Surface>,
+      </ChartElement>,
     );
 
     expect(screen.getAllByText('Customized Label')).toHaveLength(data.length);
@@ -65,9 +99,9 @@ describe('<Area />', () => {
     };
 
     render(
-      <Surface width={500} height={500}>
-        <Area dataKey="value" isAnimationActive={false} baseLine={200} points={data} dot={renderDot} />
-      </Surface>,
+      <ChartElement width={500} height={500} data={data}>
+        <Area dataKey="value" isAnimationActive={false} baseLine={200} dot={renderDot} />
+      </ChartElement>,
     );
 
     expect(screen.getAllByRole('cell')).toHaveLength(data.length);
@@ -79,15 +113,14 @@ describe('<Area />', () => {
     );
 
     render(
-      <Surface width={500} height={500}>
+      <ChartElement width={500} height={500} data={data}>
         <Area
           dataKey="value"
           isAnimationActive={false}
           baseLine={200}
-          points={data}
           dot={({ cx, cy }) => <CustomizedDot key={`${cx}-${cy}`} cx={cx} cy={cy} />}
         />
-      </Surface>,
+      </ChartElement>,
     );
 
     expect(screen.getAllByRole('cell')).toHaveLength(data.length);
@@ -95,12 +128,11 @@ describe('<Area />', () => {
 
   test('Does not throw when dot is null', () => {
     const { container } = render(
-      <Surface width={500} height={500}>
+      <ChartElement width={500} height={500} data={data}>
         {/* Test that the error Cannot read properties of null (reading 'clipDot') does not appear in JS projects */}
-        {/* eslint-disable-next-line */}
-        {/* @ts-ignore */}
-        <Area dataKey="value" points={data} dot={null} />
-      </Surface>,
+        {/* @ts-expect-error TypeScript correctly flags this as an error, but we want to have a test for it regardless */}
+        <Area dataKey="value" dot={null} />
+      </ChartElement>,
     );
 
     expect(container.querySelectorAll('.recharts-area-area')).toHaveLength(1);
@@ -108,11 +140,11 @@ describe('<Area />', () => {
     expect(container.querySelectorAll('.recharts-area-dot')).toHaveLength(0);
   });
 
-  test("Don't render any path when data is empty", () => {
+  test.each([{ myData: undefined }, { myData: [] }])("Don't render any path when data is $myData", ({ myData }) => {
     const { container } = render(
-      <Surface width={500} height={500}>
-        <Area dataKey="value" points={undefined} />
-      </Surface>,
+      <ChartElement width={500} height={500} data={myData}>
+        <Area dataKey="value" />
+      </ChartElement>,
     );
 
     expect(container.querySelectorAll('.recharts-area-area')).toHaveLength(0);
@@ -122,9 +154,9 @@ describe('<Area />', () => {
 
   test('renders the curve with the stroke on top (2nd) of the curve with the fill (1st)', () => {
     const { container } = render(
-      <Surface width={500} height={500}>
-        <Area dataKey="value" points={data} baseLine={200} stroke="teal" fill="teal" />
-      </Surface>,
+      <ChartElement width={500} height={500} data={data}>
+        <Area dataKey="value" baseLine={200} stroke="teal" fill="teal" />
+      </ChartElement>,
     );
 
     const curveCount = container.querySelectorAll('.recharts-curve').length;
@@ -134,5 +166,17 @@ describe('<Area />', () => {
     expect(container.querySelectorAll('.recharts-curve')[0]).toHaveAttribute('fill', 'teal');
     expect(container.querySelectorAll('.recharts-curve')[curveCount - 1]).toHaveAttribute('stroke', 'teal');
     expect(container.querySelectorAll('.recharts-curve')[curveCount - 1]).toHaveAttribute('fill', 'none');
+  });
+});
+
+describe.each(chartsThatDoNotSupportArea)('<Area /> as a child of $testName', ({ ChartElement }) => {
+  it('should not render any curves', () => {
+    const { container } = render(
+      <ChartElement width={500} height={500} data={data}>
+        <Area dataKey="value" baseLine={200} stroke="teal" fill="teal" />
+      </ChartElement>,
+    );
+
+    expect(container.querySelectorAll('.recharts-curve')).toHaveLength(0);
   });
 });
