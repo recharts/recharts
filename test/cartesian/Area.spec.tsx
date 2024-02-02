2957222cdeea5950c59985319da822d7c81ac876
Pavel Vaněček <corkscreewe@gmail.com>
2024-02-03 03:14:10 +1100

    Area tests (#4109)
    ## Description
    
    I will finish the TODO tests later
    
    ## Related Issue
    
    https://github.com/recharts/recharts/discussions/3717
    
    ## Motivation and Context
    
    TDD
    
    ## How Has This Been Tested?
    
    npm test
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    - [X] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [X] I have added tests to cover my changes.
    - [ ] I have added a storybook story or extended an existing story to
    show my changes
    - [X] All new and existing tests passed.

--- test/cartesian/Area.spec.tsx
+++ test/cartesian/Area.spec.tsx
@@ -1,4 +1,5 @@
 import React, { ComponentType, FC, ReactNode } from 'react';
+import { describe, test, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import {
   Area,
@@ -11,10 +12,18 @@ import {
   RadarChart,
   RadialBarChart,
   ScatterChart,
+  YAxis,
 } from '../../src';
+import { LayoutType } from '../../src/util/types';
 
 type TestCase = {
-  ChartElement: ComponentType<{ children?: ReactNode; width?: number; height?: number; data?: any[] }>;
+  ChartElement: ComponentType<{
+    children?: ReactNode;
+    width?: number;
+    height?: number;
+    data?: any[];
+    layout?: LayoutType;
+  }>;
   testName: string;
 };
 
@@ -46,98 +55,238 @@ const data = [
 ];
 
 describe.each(chartsThatSupportArea)('<Area /> as a child of $testName', ({ ChartElement }) => {
-  test('Render customized label when label is set to be a function', () => {
-    const renderLabel = (props: { index: number; x: number; y: number }) => {
-      const { index, x, y } = props;
+  describe('label', () => {
+    test('Render customized label when label is set to be a function', () => {
+      const renderLabel = (props: { index: number; x: number; y: number }) => {
+        const { index, x, y } = props;
 
-      return (
-        <text key={index} x={x} y={y} className="customized-label">
-          Customized Label
-        </text>
+        return (
+          <text key={index} x={x} y={y} className="customized-label">
+            Customized Label
+          </text>
+        );
+      };
+
+      render(
+        <ChartElement width={500} height={500} data={data}>
+          <Area dataKey="value" isAnimationActive={false} baseLine={200} label={renderLabel} />
+        </ChartElement>,
       );
-    };
 
-    render(
-      <ChartElement width={500} height={500} data={data}>
-        <Area dataKey="value" isAnimationActive={false} baseLine={200} label={renderLabel} />
-      </ChartElement>,
-    );
+      expect(screen.getAllByText('Customized Label')).toHaveLength(data.length);
+    });
 
-    expect(screen.getAllByText('Customized Label')).toHaveLength(data.length);
-  });
+    test('Render customized label when label is set to be a react element', () => {
+      const Label: FC<{ index: number; x: number; y: number }> = props => {
+        const { index, x, y } = props;
 
-  test('Render customized label when label is set to be a react element', () => {
-    const Label: FC<{ index: number; x: number; y: number }> = props => {
-      const { index, x, y } = props;
+        return (
+          <text key={index} x={x} y={y} className="customized-label">
+            Customized Label
+          </text>
+        );
+      };
 
-      return (
-        <text key={index} x={x} y={y} className="customized-label">
-          Customized Label
-        </text>
+      render(
+        <ChartElement width={500} height={500} data={data}>
+          <Area
+            dataKey="value"
+            isAnimationActive={false}
+            baseLine={200}
+            label={({ index, x, y }: { index: number; x: number; y: number }) => <Label index={index} x={x} y={y} />}
+          />
+        </ChartElement>,
       );
-    };
 
-    render(
-      <ChartElement width={500} height={500} data={data}>
-        <Area
-          dataKey="value"
-          isAnimationActive={false}
-          baseLine={200}
-          label={({ index, x, y }: { index: number; x: number; y: number }) => <Label index={index} x={x} y={y} />}
-        />
-      </ChartElement>,
-    );
-
-    expect(screen.getAllByText('Customized Label')).toHaveLength(data.length);
+      expect(screen.getAllByText('Customized Label')).toHaveLength(data.length);
+    });
   });
 
-  test('Render customized dot when dot is set to be a function', () => {
-    const renderDot = (props: { cx: number; cy: number }) => {
-      const { cx, cy } = props;
+  describe('dot', () => {
+    test('Render customized dot when dot is set to be a function', () => {
+      const renderDot = (props: { cx: number; cy: number }) => {
+        const { cx, cy } = props;
 
-      return <circle role="cell" key={cx} x={cx} y={cy} r={5} className="customized-dot" />;
-    };
+        return <circle role="cell" key={cx} x={cx} y={cy} r={5} className="customized-dot" />;
+      };
 
-    render(
-      <ChartElement width={500} height={500} data={data}>
-        <Area dataKey="value" isAnimationActive={false} baseLine={200} dot={renderDot} />
-      </ChartElement>,
-    );
+      render(
+        <ChartElement width={500} height={500} data={data}>
+          <Area dataKey="value" isAnimationActive={false} baseLine={200} dot={renderDot} />
+        </ChartElement>,
+      );
 
-    expect(screen.getAllByRole('cell')).toHaveLength(data.length);
+      expect(screen.getAllByRole('cell')).toHaveLength(data.length);
+    });
+
+    test('Render customized dot when dot is set to be a react element', () => {
+      const CustomizedDot: FC<{ cx?: number; cy?: number }> = ({ cx, cy }) => (
+        <circle role="cell" cx={cx} cy={cy} r={5} className="customized-dot" />
+      );
+
+      render(
+        <ChartElement width={500} height={500} data={data}>
+          <Area
+            dataKey="value"
+            isAnimationActive={false}
+            baseLine={200}
+            dot={({ cx, cy }) => <CustomizedDot key={`${cx}-${cy}`} cx={cx} cy={cy} />}
+          />
+        </ChartElement>,
+      );
+
+      expect(screen.getAllByRole('cell')).toHaveLength(data.length);
+    });
+
+    test('Does not throw when dot is null', () => {
+      const { container } = render(
+        <ChartElement width={500} height={500} data={data}>
+          {/* Test that the error Cannot read properties of null (reading 'clipDot') does not appear in JS projects */}
+          {/* @ts-expect-error TypeScript correctly flags this as an error, but we want to have a test for it regardless */}
+          <Area dataKey="value" dot={null} />
+        </ChartElement>,
+      );
+
+      expect(container.querySelectorAll('.recharts-area-area')).toHaveLength(1);
+      expect(container.querySelectorAll('.recharts-area-curve')).toHaveLength(1);
+      expect(container.querySelectorAll('.recharts-area-dot')).toHaveLength(0);
+    });
   });
 
-  test('Render customized dot when dot is set to be a react element', () => {
-    const CustomizedDot: FC<{ cx?: number; cy?: number }> = ({ cx, cy }) => (
-      <circle role="cell" cx={cx} cy={cy} r={5} className="customized-dot" />
-    );
+  describe('baseValue', () => {
+    describe('horizontal layout with numeric Y axis', () => {
+      const layout = 'horizontal';
+      it('should default baseValue to zero if no domain is defined in the axis', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data} layout={layout}>
+            <Area dataKey="value" />
+            <YAxis />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute('d', 'M65,5L172.5,5L280,5L387.5,5L495,5L495,495L387.5,495L280,495L172.5,495L65,495Z');
+        expect.soft(curves[1]).toHaveAttribute('d', 'M65,5L172.5,5L280,5L387.5,5L495,5');
+      });
 
-    render(
-      <ChartElement width={500} height={500} data={data}>
-        <Area
-          dataKey="value"
-          isAnimationActive={false}
-          baseLine={200}
-          dot={({ cx, cy }) => <CustomizedDot key={`${cx}-${cy}`} cx={cx} cy={cy} />}
-        />
-      </ChartElement>,
-    );
+      it('should default baseValue to the first number in domain', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data} layout={layout}>
+            <Area dataKey="value" />
+            <YAxis domain={[20, 300]} />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute(
+            'd',
+            'M65,355L172.5,355L280,355L387.5,355L495,355L495,495L387.5,495L280,495L172.5,495L65,495Z',
+          );
+        expect.soft(curves[1]).toHaveAttribute('d', 'M65,355L172.5,355L280,355L387.5,355L495,355');
+      });
 
-    expect(screen.getAllByRole('cell')).toHaveLength(data.length);
-  });
+      it('should set baseValue to dataMin', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data} layout={layout}>
+            <Area dataKey="value" baseValue="dataMin" />
+            <YAxis domain={[20, 300]} />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute(
+            'd',
+            'M65,355L172.5,355L280,355L387.5,355L495,355L495,495L387.5,495L280,495L172.5,495L65,495Z',
+          );
+        expect.soft(curves[1]).toHaveAttribute('d', 'M65,355L172.5,355L280,355L387.5,355L495,355');
+      });
 
-  test('Does not throw when dot is null', () => {
-    const { container } = render(
-      <ChartElement width={500} height={500} data={data}>
-        {/* Test that the error Cannot read properties of null (reading 'clipDot') does not appear in JS projects */}
-        {/* @ts-expect-error TypeScript correctly flags this as an error, but we want to have a test for it regardless */}
-        <Area dataKey="value" dot={null} />
-      </ChartElement>,
-    );
+      it('should set baseValue to dataMax', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data} layout={layout}>
+            <Area dataKey="value" baseValue="dataMax" />
+            <YAxis domain={[20, 300]} />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute('d', 'M65,355L172.5,355L280,355L387.5,355L495,355L495,5L387.5,5L280,5L172.5,5L65,5Z');
+        expect.soft(curves[1]).toHaveAttribute('d', 'M65,355L172.5,355L280,355L387.5,355L495,355');
+      });
+    });
 
-    expect(container.querySelectorAll('.recharts-area-area')).toHaveLength(1);
-    expect(container.querySelectorAll('.recharts-area-curve')).toHaveLength(1);
-    expect(container.querySelectorAll('.recharts-area-dot')).toHaveLength(0);
+    describe.todo('horizontal layout with categorical Y axis');
+    describe.todo('vertical layout with numeric X axis');
+    describe.todo('vertical layout with categorical X axis');
+
+    describe('with no axes', () => {
+      it('should default baseValue to 0', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data}>
+            <Area dataKey="value" />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute('d', 'M5,5L127.5,5L250,5L372.5,5L495,5L495,495L372.5,495L250,495L127.5,495L5,495Z');
+        expect.soft(curves[1]).toHaveAttribute('d', 'M5,5L127.5,5L250,5L372.5,5L495,5');
+      });
+
+      it('should set baseValue to a number', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data}>
+            <Area dataKey="value" baseValue={9} />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute(
+            'd',
+            'M5,5L127.5,5L250,5L372.5,5L495,5L495,450.9L372.5,450.9L250,450.9L127.5,450.9L5,450.9Z',
+          );
+        expect.soft(curves[1]).toHaveAttribute('d', 'M5,5L127.5,5L250,5L372.5,5L495,5');
+      });
+
+      it('should set baseValue to 0 when dataMin', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data}>
+            <Area dataKey="value" baseValue="dataMin" />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute('d', 'M5,5L127.5,5L250,5L372.5,5L495,5L495,495L372.5,495L250,495L127.5,495L5,495Z');
+        expect.soft(curves[1]).toHaveAttribute('d', 'M5,5L127.5,5L250,5L372.5,5L495,5');
+      });
+
+      it('should set baseValue to dataMax', () => {
+        const { container } = render(
+          <ChartElement width={500} height={500} data={data}>
+            <Area dataKey="value" baseValue="dataMax" />
+          </ChartElement>,
+        );
+        const curves = container.querySelectorAll('.recharts-curve');
+        expect(curves).toHaveLength(2);
+        expect
+          .soft(curves[0])
+          .toHaveAttribute('d', 'M5,5L127.5,5L250,5L372.5,5L495,5L495,5L372.5,5L250,5L127.5,5L5,5Z');
+        expect.soft(curves[1]).toHaveAttribute('d', 'M5,5L127.5,5L250,5L372.5,5L495,5');
+      });
+    });
   });
 
   test.each([{ myData: undefined }, { myData: [] }])("Don't render any path when data is $myData", ({ myData }) => {
