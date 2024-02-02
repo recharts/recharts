bbbec476f1406bb3f000a59c0d4bd937328f7c1b
Buddhika Geasman <bgeasman@gmail.com>
2024-02-01 23:30:45 -0600

    Add util functions to retrieve svg tags and respective attributes

---
+++ src/util/svgUtils/SVGUtils.ts
@@ -0,0 +1,28 @@
+import camelCase from 'lodash/camelCase';
+
+// @Types
+import { SVGElementAttributeTypes } from './types';
+
+/**
+ * As React props are in camelCase this ensures that the svg elements and their attributes are in camelCase
+ * @param {SVGElementAttributeTypes} svgElementAttributes - An object with svg elements and their attributes in kebab-case
+ * @returns {SVGElementAttributeTypes} - An object with svg elements and their attributes in camelCase
+ */
+export const svgElementAttributesInCamelCase = (
+  svgElementAttributes: SVGElementAttributeTypes,
+): SVGElementAttributeTypes => {
+  return Object.keys(svgElementAttributes).reduce((acc, key: keyof SVGElementAttributeTypes) => {
+    acc[key] = svgElementAttributes[key].map(camelCase);
+
+    return acc;
+  }, {} as SVGElementAttributeTypes);
+};
+
+/**
+ *
+ * @param {SVGElementAttributeTypes} svgElementAttributes :An object that mapped svg elements to their respective attributes
+ * @returns {string[]} - An array of svg element tags
+ */
+export const SVGElementTags = (svgElementAttributes: SVGElementAttributeTypes) => {
+  return Object.keys(svgElementAttributes).slice(1);
+};
