a93fa69fd0bdf5b1581a207593ec7677049cdc87
Pavel Vanecek <corkscreewe@gmail.com>
2024-02-10 14:48:15 +1100

    Fix storybook, add label test

--- test/cartesian/ReferenceArea.spec.tsx
+++ test/cartesian/ReferenceArea.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { describe, test, expect } from 'vitest';
+import { describe, it, test, expect } from 'vitest';
 import { render } from '@testing-library/react';
 import { BarChart, ReferenceArea, Bar, XAxis, YAxis, LabelProps } from '../../src';
 
@@ -119,79 +119,146 @@ describe('<ReferenceArea />', () => {
     expect(container.querySelectorAll('.recharts-reference-area-rect')).toHaveLength(1);
   });
 
-  test('Render 1 line and 1 label when label is set to be a function', () => {
-    const renderLabel = (props: LabelProps) => {
-      const { x, y } = props;
-
-      return (
-        <text className="customized-reference-area-label" x={x} y={y}>
-          My custom label text
-        </text>
+  describe('label', () => {
+    it('should render label defined as a string', () => {
+      const { container, getByText } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" orientation="top" />
+          <YAxis tickCount={7} orientation="right" />
+          <Bar dataKey="uv" />
+          <ReferenceArea x1="201106" x2="201110" fill="#666" label="My label text" />
+        </BarChart>,
       );
-    };
-    const { container, getByText } = render(
-      <BarChart
-        width={1100}
-        height={250}
-        barGap={2}
-        barSize={6}
-        data={data}
-        margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
-      >
-        <XAxis dataKey="name" />
-        <YAxis tickCount={7} />
-        <Bar dataKey="uv" />
-        <ReferenceArea x1="201106" x2="201110" fill="#666" label={renderLabel} />
-      </BarChart>,
-    );
-    expect(container.querySelectorAll('.recharts-reference-area-rect')).toHaveLength(1);
-    expect(container.querySelectorAll('.customized-reference-area-label')).toHaveLength(1);
-    expect(getByText('My custom label text')).toBeVisible();
-  });
+      expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
+      expect(getByText('My label text')).toBeVisible();
+    });
 
-  test("Don't render label when label is true", () => {
-    const { container } = render(
-      <BarChart
-        width={1100}
-        height={250}
-        barGap={2}
-        barSize={6}
-        data={data}
-        margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
-      >
-        <XAxis dataKey="name" />
-        <YAxis tickCount={7} />
-        <Bar dataKey="uv" />
-        <ReferenceArea x1="201106" x2="201110" fill="#666" label />
-      </BarChart>,
-    );
+    it('should render label defined as a number', () => {
+      const { container, getByText } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" orientation="top" />
+          <YAxis tickCount={7} orientation="right" />
+          <Bar dataKey="uv" />
+          <ReferenceArea x1="201106" x2="201110" fill="#666" label={2024} />
+        </BarChart>,
+      );
+      expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
+      expect(getByText('2024')).toBeVisible();
+    });
 
-    expect(container.querySelectorAll('.recharts-label')).toHaveLength(0);
-  });
+    it('should render label defined as a Label props object', () => {
+      const { container, getByText } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" orientation="top" />
+          <YAxis tickCount={7} orientation="right" />
+          <Bar dataKey="uv" />
+          <ReferenceArea
+            x1="201106"
+            x2="201110"
+            fill="#666"
+            label={{ value: 'This is not documented by it works perfectly fine', offset: 8 }}
+          />
+        </BarChart>,
+      );
+      expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
+      expect(getByText('This is not documented by it works perfectly fine')).toBeVisible();
+    });
 
-  test('Render custom label when label is react element', () => {
-    const Label = ({ text, ...props }: { text: string }) => <text {...props}>{text}</text>;
-    const { getByText } = render(
-      <BarChart
-        width={1100}
-        height={250}
-        barGap={2}
-        barSize={6}
-        data={data}
-        margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
-      >
-        <XAxis dataKey="name" />
-        <YAxis tickCount={7} />
-        <Bar dataKey="uv" />
-        <ReferenceArea
-          x1="201106"
-          x2="201110"
-          fill="#666"
-          label={<Label text="Custom Text" />}
-          ifOverflow="extendDomain"
-        />
-      </BarChart>,
-    );
-    expect(getByText('Custom Text')).toBeVisible();
+    test('Render 1 line and 1 label when label is a function', () => {
+      const renderLabel = (props: LabelProps) => {
+        const { x, y } = props;
+
+        return (
+          <text className="customized-reference-area-label" x={x} y={y}>
+            My custom label text
+          </text>
+        );
+      };
+      const { container, getByText } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis tickCount={7} />
+          <Bar dataKey="uv" />
+          <ReferenceArea x1="201106" x2="201110" fill="#666" label={renderLabel} />
+        </BarChart>,
+      );
+      expect(container.querySelectorAll('.recharts-reference-area-rect')).toHaveLength(1);
+      expect(container.querySelectorAll('.customized-reference-area-label')).toHaveLength(1);
+      expect(getByText('My custom label text')).toBeVisible();
+    });
+
+    test.each([true, false])('should not render label when label=%s', label => {
+      const { container } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis tickCount={7} />
+          <Bar dataKey="uv" />
+          <ReferenceArea x1="201106" x2="201110" fill="#666" label={label} />
+        </BarChart>,
+      );
+
+      expect(container.querySelectorAll('.recharts-label')).toHaveLength(0);
+    });
+
+    test('Render custom label when label is react element', () => {
+      const Label = ({ text, ...props }: { text: string }) => <text {...props}>{text}</text>;
+      const { getByText } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis tickCount={7} />
+          <Bar dataKey="uv" />
+          <ReferenceArea
+            x1="201106"
+            x2="201110"
+            fill="#666"
+            label={<Label text="Custom Text" />}
+            ifOverflow="extendDomain"
+          />
+        </BarChart>,
+      );
+      expect(getByText('Custom Text')).toBeVisible();
+    });
   });
 });
