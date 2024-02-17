2d1ac37b292864bce9711fcdb04de8d4b33851f6
Pavel Vanecek <corkscreewe@gmail.com>
2024-02-17 13:44:50 +1100

    Add tests for build output files

--- scripts/buildOutput.test.ts
+++ scripts/buildOutput.test.ts
@@ -1,16 +1,80 @@
 import { existsSync } from 'fs';
-import { describe, test, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import { globSync } from 'glob';
 
 import packageJson from '../package.json';
 
+/**
+ * If you see this test failing it means the build output has changed.
+ *
+ * If that is intentional - for example, you added a new file, or a new Chart type, all good!
+ * Change the test and run build again.
+ * If the test failure is in snapshots, then update the snapshots by running this command:
+ *
+ *    npm run test-build-output -- --update
+ *
+ * If you did not expect the output to change, then consider going back and review your changes.
+ * We do not want to npm publish too many files, or not enough files.
+ */
 describe('expected folder structure', () => {
-  const expectedFolders: ReadonlyArray<string> = ['lib', 'es6', 'umd', 'types'];
+  describe('package.json', () => {
+    it('should include output files', () => {
+      expect(packageJson.files).toHaveLength(5);
+      expect(packageJson.files).toContain('*.md');
+      expect(packageJson.files).toContain('types');
+      expect(packageJson.files).toContain('es6');
+      expect(packageJson.files).toContain('umd');
+      expect(packageJson.files).toContain('lib');
+    });
 
-  test.each(expectedFolders)('build output should include folder %s', folderName => {
-    expect(existsSync(folderName)).toBe(true);
+    it('should point to a main file', () => {
+      expect(packageJson.main).toEqual('lib/index');
+      expect(existsSync(`./${packageJson.main}.js`)).toBe(true);
+    });
+
+    it('should point to a main module file', () => {
+      expect(packageJson.module).toEqual('es6/index');
+      expect(existsSync(`./${packageJson.module}.js`)).toBe(true);
+    });
+
+    it('should point to a jsnext:main file', () => {
+      expect(packageJson['jsnext:main']).toEqual('es6/index');
+      expect(existsSync(`./${packageJson['jsnext:main']}.js`)).toBe(true);
+    });
+
+    it('should point to a types file', () => {
+      expect(packageJson.types).toEqual('types/index.d.ts');
+      expect(existsSync(`./${packageJson.types}`)).toBe(true);
+    });
   });
 
-  test.each(expectedFolders)('package.json should include folder %s', folderName => {
-    expect(packageJson.files).toContain(folderName);
+  describe('types folder output', () => {
+    it('should have expected files and no more', () => {
+      expect(globSync('types/**/*', { dot: true })).toMatchFileSnapshot('./snapshots/typesFiles.txt');
+    });
+  });
+
+  describe('es6 folder output', () => {
+    it('should have expected files and no more', () => {
+      expect(globSync('es6/**/*', { dot: true })).toMatchFileSnapshot('./snapshots/es6Files.txt');
+    });
+  });
+
+  describe('markdown files output', () => {
+    it('should have expected files and no more', () => {
+      expect(globSync('*.md', { dot: true })).toMatchFileSnapshot('./snapshots/markdownFiles.txt');
+    });
+  });
+
+  describe('umd folder output', () => {
+    it('should have expected files and no more', () => {
+      expect(globSync('umd/**/*', { dot: true })).toMatchFileSnapshot('./snapshots/umdFiles.txt');
+    });
+  });
+
+  describe('lib folder output', () => {
+    it('should have expected files and no more', () => {
+      expect(globSync('lib/**/*', { dot: true })).toMatchFileSnapshot('./snapshots/libFiles.txt');
+    });
   });
 });
