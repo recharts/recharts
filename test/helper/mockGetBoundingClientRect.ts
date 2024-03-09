ae54cf9101367deac08dcb2eabff499c04b410b6
Pavel Vaněček <corkscreewe@gmail.com>
2024-03-10 06:37:45 +1100

    Add mockGetBoundingClientRect helper function (#4274)

---
+++ test/helper/mockGetBoundingClientRect.ts
@@ -0,0 +1,26 @@
+/**
+ * getBoundingClientRect always returns 0 in jsdom, we can't test for actual returned string size
+ * Execution order matters
+ * https://github.com/jsdom/jsdom/issues/1590#issuecomment-578350151
+ *
+ * @param rect overrides getBoundingClientRect return value in the mock. jsdom by design returns all zeroes
+ * @returns cleanup function, convenient for beforeAll or beforeEach
+ */
+export function mockGetBoundingClientRect(rect: Partial<DOMRect>) {
+  const original = Element.prototype.getBoundingClientRect;
+  Element.prototype.getBoundingClientRect = () => ({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    toJSON: () => '{}',
+    ...rect,
+  });
+  return function cleanup() {
+    Element.prototype.getBoundingClientRect = original;
+  };
+}
