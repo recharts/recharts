abbab8ef7d6fc0cd1c4bc90f577cf16860ae83bf
108yen <kazuking.1911@gmail.com>
2024-03-14 21:09:44 +0900

    fix(Area): fixed a bug where className was not assigned to areaDot

--- src/cartesian/Area.tsx
+++ src/cartesian/Area.tsx
@@ -288,7 +288,8 @@ export class Area extends PureComponent<Props, State> {
     } else if (isFunction(option)) {
       dotItem = option(props);
     } else {
-      dotItem = <Dot {...props} className="recharts-area-dot" />;
+      const className = clsx('recharts-area-dot', option ? (option as DotProps).className : '');
+      dotItem = <Dot {...props} className={className} />;
     }
 
     return dotItem;
