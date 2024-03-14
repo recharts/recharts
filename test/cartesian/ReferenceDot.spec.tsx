21eedaf001fa5abbb8723d2ee567513d028c88c3
Pavel Vanecek <corkscreewe@gmail.com>
2024-03-14 21:43:37 +0900

    Update ReferenceDot tests

--- test/cartesian/ReferenceDot.spec.tsx
+++ test/cartesian/ReferenceDot.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { describe, expect, it, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { BarChart, ReferenceDot, Bar, XAxis, YAxis } from '../../src';
+import { Bar, BarChart, ReferenceDot, XAxis, YAxis } from '../../src';
 
 describe('<ReferenceDot />', () => {
   const data = [
@@ -33,8 +34,17 @@ describe('<ReferenceDot />', () => {
         <ReferenceDot x="201106" y={3} stroke="#666" label="201106" />
       </BarChart>,
     );
-    expect(container.querySelectorAll('.recharts-reference-dot-dot')).toHaveLength(1);
-    expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
+    expect(container.querySelectorAll('.recharts-reference-dot .recharts-reference-dot-dot')).toHaveLength(1);
+    const labels = container.querySelectorAll('.recharts-label');
+    expect(labels).toHaveLength(1);
+    const label = labels[0];
+    expect.soft(label.getAttributeNames().sort()).toEqual(['class', 'fill', 'offset', 'text-anchor', 'x', 'y']);
+    expect(label.getAttribute('x')).toEqual('472.72727272727275');
+    expect(label.getAttribute('y')).toEqual('86.66666666666667');
+    expect(label.getAttribute('fill')).toEqual('#808080');
+    expect(label.getAttribute('class')).toEqual('recharts-text recharts-label');
+    expect(label.getAttribute('text-anchor')).toEqual('middle');
+    expect(label.textContent).toEqual('201106');
   });
 
   test("Don't render any dot or label when reference dot is outside domain in ReferenceDot", () => {
@@ -55,6 +65,67 @@ describe('<ReferenceDot />', () => {
     );
     expect(container.querySelectorAll('.recharts-reference-dot-dot')).toHaveLength(0);
     expect(container.querySelectorAll('.recharts-label')).toHaveLength(0);
+  });
+
+  test("Don't render any dot or label when x is not defined", () => {
+    const { container } = render(
+      <BarChart
+        width={1100}
+        height={250}
+        barGap={2}
+        barSize={6}
+        data={data}
+        margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis tickCount={7} />
+        <Bar dataKey="uv" />
+        <ReferenceDot y={3} stroke="#666" label="201106" />
+      </BarChart>,
+    );
+    expect(container.querySelectorAll('.recharts-reference-dot-dot')).toHaveLength(0);
+    expect(container.querySelectorAll('.recharts-label')).toHaveLength(0);
+  });
+
+  test("Don't render any dot or label when y is not defined", () => {
+    const { container } = render(
+      <BarChart
+        width={1100}
+        height={250}
+        barGap={2}
+        barSize={6}
+        data={data}
+        margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis tickCount={7} />
+        <Bar dataKey="uv" />
+        <ReferenceDot x="201104" stroke="#666" label="201106" />
+      </BarChart>,
+    );
+    expect(container.querySelectorAll('.recharts-reference-dot-dot')).toHaveLength(0);
+    expect(container.querySelectorAll('.recharts-label')).toHaveLength(0);
+  });
+
+  it('adds a clipPath attribute when ifOverflow is "hidden"', () => {
+    const { container } = render(
+      <BarChart
+        width={1100}
+        height={250}
+        barGap={2}
+        barSize={6}
+        data={data}
+        margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis tickCount={7} />
+        <Bar dataKey="uv" />
+        <ReferenceDot x="201106" y={20} stroke="#666" label="201106" ifOverflow="hidden" />
+      </BarChart>,
+    );
+    expect(container.querySelectorAll('.recharts-reference-dot-dot')[0].getAttribute('clip-path')).toMatch(
+      /url\(#recharts(\d+)-clip\)/,
+    );
   });
 
   test('Render 1 line and 1 label when ifOverflow is `extendDomain` in ReferenceDot', () => {
@@ -159,5 +230,97 @@ describe('<ReferenceDot />', () => {
       </BarChart>,
     );
     expect(container.querySelectorAll('.recharts-reference-dot-dot')).toHaveLength(0);
+  });
+
+  describe('shape as a React Element', () => {
+    it('should render whatever the shape returns, and pass in extra sneaky props', () => {
+      const { container } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis tickCount={7} />
+          <Bar dataKey="uv" />
+          <ReferenceDot
+            x="201106"
+            y={20}
+            stroke="#666"
+            fill="#999"
+            r={6}
+            fillOpacity="0.3"
+            shape={<circle className="custom-dot" />}
+            ifOverflow="extendDomain"
+          />
+        </BarChart>,
+      );
+      const myCustomDot = container.querySelector('.custom-dot');
+      expect(myCustomDot).toBeInTheDocument();
+      expect
+        .soft(myCustomDot.getAttributeNames().sort())
+        .toEqual(['class', 'cx', 'cy', 'fill', 'fill-opacity', 'r', 'stroke', 'stroke-width', 'x', 'y']);
+      expect.soft(myCustomDot.getAttribute('fill')).toEqual('#999');
+      expect.soft(myCustomDot.getAttribute('r')).toEqual('6');
+      expect.soft(myCustomDot.getAttribute('cx')).toEqual('472.72727272727275');
+      expect.soft(myCustomDot.getAttribute('cy')).toEqual('20');
+      expect.soft(myCustomDot.getAttribute('stroke')).toEqual('#666');
+      expect.soft(myCustomDot.getAttribute('stroke-width')).toEqual('1');
+      expect.soft(myCustomDot.getAttribute('fill-opacity')).toEqual('0.3');
+      expect.soft(myCustomDot.getAttribute('class')).toEqual('custom-dot');
+      expect.soft(myCustomDot.getAttribute('x')).toEqual('201106');
+      expect.soft(myCustomDot.getAttribute('y')).toEqual('20');
+    });
+  });
+
+  describe('shape as a React Component', () => {
+    it('should render whatever the Component returns, and pass in props', () => {
+      const Shape = (props: unknown) => {
+        expect(props).toEqual({
+          clipPath: undefined,
+          cx: 472.72727272727275,
+          cy: 20,
+          fill: '#999',
+          fillOpacity: '0.3',
+          r: 6,
+          stroke: '#666',
+          strokeWidth: 1,
+          x: '201106',
+          y: 20,
+        });
+        return <circle className="custom-dot" />;
+      };
+      const { container } = render(
+        <BarChart
+          width={1100}
+          height={250}
+          barGap={2}
+          barSize={6}
+          data={data}
+          margin={{ top: 20, right: 60, bottom: 0, left: 20 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis tickCount={7} />
+          <Bar dataKey="uv" />
+          <ReferenceDot
+            x="201106"
+            y={20}
+            stroke="#666"
+            fill="#999"
+            r={6}
+            fillOpacity="0.3"
+            shape={Shape}
+            ifOverflow="extendDomain"
+          />
+        </BarChart>,
+      );
+      const myCustomDot = container.querySelector('.custom-dot');
+      expect(myCustomDot).toBeInTheDocument();
+      expect.soft(myCustomDot.getAttributeNames().sort()).toEqual(['class']);
+      expect.soft(myCustomDot.getAttribute('class')).toEqual('custom-dot');
+    });
   });
 });
