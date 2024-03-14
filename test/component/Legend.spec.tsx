recharts/recharts

commit fe5e153bd30091bc19ecb58032d5039672be8c15

getLegendProps does not compute payload (#4273)

src/component/Legend.tsx
@@ -12,9 +12,7 @@ function defaultUniqBy(entry: Payload) {
 
 function LegendContent(props: Props) {
   const contextPayload = useLegendPayload();
-  // We are in the process of refactoring all charts to Context; once that's done we can get rid of props.payload.
-  const preferredPayload = contextPayload.length > 0 ? contextPayload : props.payload;
-  const finalPayload = getUniqPayload(preferredPayload, props.payloadUniqBy, defaultUniqBy);
+  const finalPayload = getUniqPayload(contextPayload, props.payloadUniqBy, defaultUniqBy);
   const contentProps = {
     ...props,
     payload: finalPayload,

test/component/Legend.spec.tsx
@@ -340,29 +340,11 @@ describe('<Legend />', () => {
   ];
 
   describe('outside of chart context', () => {
-    test('Render 4 legend items in simple Legend', () => {
+    it('should ignore payload prop', () => {
       const { container } = render(<Legend width={500} height={30} payload={categoricalData} />);
 
-      expect(container.querySelectorAll('.recharts-default-legend')).toHaveLength(1);
-      expect(container.querySelectorAll('.recharts-default-legend .recharts-legend-item')).toHaveLength(4);
-    });
-
-    test('Does not render items with a type of `none`', () => {
-      const dataWithNone = [
-        { value: 'Apple', color: '#ff7300' },
-        { value: 'Samsung', color: '#bb7300' },
-        { value: 'Huawei', color: '#887300' },
-        { value: 'Sony', type: 'none' },
-      ];
-      const { container } = render(<Legend width={500} height={30} payload={dataWithNone as any} />);
-
-      expect(container.querySelectorAll('.recharts-default-legend')).toHaveLength(1);
-      expect(container.querySelectorAll('.recharts-default-legend .recharts-legend-item')).toHaveLength(3);
-    });
-
-    test('Renders payload value correctly when passed as a static value', () => {
-      render(<Legend payload={[{ value: 'item name' }]} />);
-      screen.getByText(/item name/i);
+      expect(container.querySelectorAll('.recharts-default-legend')).toHaveLength(0);
+      expect(container.querySelectorAll('.recharts-default-legend .recharts-legend-item')).toHaveLength(0);
     });
   });
 
@@ -1058,6 +1040,22 @@ describe('<Legend />', () => {
         tc => tc.legendType === 'rect',
       );
       assertExpectedAttributes(container, selector, expectedAttributes);
+    });
+
+    it('should not render items with a type of `none`', () => {
+      const { container, queryByText } = render(
+        <BarChart width={500} height={500} data={categoricalData}>
+          <Legend />
+          <Bar dataKey="value" legendType="star" />
+          <Bar dataKey="color" legendType="none" />
+        </BarChart>,
+      );
+
+      expect.soft(queryByText('value')).toBeInTheDocument();
+      expect.soft(queryByText('color')).not.toBeInTheDocument();
+      const legendItems = assertHasLegend(container);
+      expect(legendItems).toHaveLength(1);
+      expect(legendItems[0].textContent).toBe('value');
     });
 
     it('should render a legend item even if the dataKey does not match anything from the data', () => {

src/chart/PieChart.tsx
@@ -12,7 +12,6 @@ export const PieChart = generateCategoricalChart({
   GraphicalChild: Pie,
   validateTooltipEventTypes: ['item'],
   defaultTooltipEventType: 'item',
-  legendContent: 'children',
   axisComponents: [
     { axisType: 'angleAxis', AxisComp: PolarAngleAxis },
     { axisType: 'radiusAxis', AxisComp: PolarRadiusAxis },

src/chart/RadialBarChart.tsx
@@ -10,7 +10,6 @@ import { RadialBar } from '../polar/RadialBar';
 export const RadialBarChart = generateCategoricalChart({
   chartName: 'RadialBarChart',
   GraphicalChild: RadialBar,
-  legendContent: 'children',
   defaultTooltipEventType: 'axis',
   validateTooltipEventTypes: ['axis', 'item'],
   axisComponents: [

src/chart/generateCategoricalChart.tsx
@@ -852,7 +852,6 @@ export const generateCategoricalChart = ({
   defaultTooltipEventType = 'axis',
   validateTooltipEventTypes = ['axis'],
   axisComponents,
-  legendContent,
   formatAxisMap,
   defaultProps,
 }: CategoricalChartOptions) => {
@@ -1826,15 +1825,12 @@ export const generateCategoricalChart = ({
      * @return {ReactElement}            The instance of Legend
      */
     renderLegend = (): React.ReactElement => {
-      const { formattedGraphicalItems } = this.state;
       const { children, width, height } = this.props;
       const margin = this.props.margin || {};
       const legendWidth: number = width - (margin.left || 0) - (margin.right || 0);
       const props = getLegendProps({
         children,
-        formattedGraphicalItems,
         legendWidth,
-        legendContent,
       });
 
       if (!props) {

src/component/DefaultLegendContent.tsx
@@ -45,6 +45,9 @@ interface InternalProps {
   layout?: LayoutType;
   align?: HorizontalAlignmentType;
   verticalAlign?: VerticalAlignmentType;
+  /**
+   * @deprecated Legend.payload prop is not doing anything. Legend is set from data on other graphical elements like Bar, Line, and Area.
+   */
   payload?: Array<Payload>;
   inactiveColor?: string;
   formatter?: Formatter;

commit 7b229058c4bb841b2f460025c65cbad44d2a5a1f

Legend bounding box update fix (#4287)
## Description

Fixing the legend offset problem introduced in
https://github.com/recharts/recharts/pull/4273

The problem is that after my changes, the component that reads and
updates the `boundingClientRect` and the component that re-renders when
payload changes, are no longer the same component.

In this PR I have moved both the bounding box, plus payload read, to the
same component so that it can call `onBBoxUpdate` again.

In my opinion the `onBBoxUpdate` should not exist in the first place
because all it does is it passes the width and height back to Legend as
`width` and `height` props. But that's a refactor for another day.

## Related Issue

https://github.com/recharts/recharts/pull/4273

## Motivation and Context

Fixing 3.x regression

## How Has This Been Tested?

npm test
storybook

## Screenshots (if appropriate):
<img width="877" alt="image"
src="https://github.com/recharts/recharts/assets/1100170/ba30120f-692f-4ca7-9578-37bf2e428291">
<img width="886" alt="image"
src="https://github.com/recharts/recharts/assets/1100170/6d5d977f-888b-43e1-a68d-170c1c851005">

## Types of changes

- [x] Bug fix (non-breaking change which fixes an issue)
- [ ] New feature (non-breaking change which adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)

## Checklist:

- [x] My code follows the code style of this project.
- [ ] My change requires a change to the documentation.
- [ ] I have updated the documentation accordingly.
- [x] I have added tests to cover my changes.
- [ ] I have added a storybook story or extended an existing story to
show my changes
- [x] All new and existing tests passed.

src/component/Legend.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, CSSProperties } from 'react';
+import React, { PureComponent, CSSProperties, useState, useCallback } from 'react';
 import { DefaultLegendContent, Payload, Props as DefaultProps } from './DefaultLegendContent';
 
 import { isNumber } from '../util/DataUtils';
@@ -10,11 +10,15 @@ function defaultUniqBy(entry: Payload) {
   return entry.value;
 }
 
-function LegendContent(props: Props) {
-  const contextPayload = useLegendPayload();
+type ContentProps = Props & {
+  contextPayload: Payload[];
+};
+
+function LegendContent(props: ContentProps) {
+  const { contextPayload, ...otherProps } = props;
   const finalPayload = getUniqPayload(contextPayload, props.payloadUniqBy, defaultUniqBy);
   const contentProps = {
-    ...props,
+    ...otherProps,
     payload: finalPayload,
   };
 
@@ -25,12 +29,47 @@ function LegendContent(props: Props) {
     return React.createElement(props.content as any, contentProps);
   }
 
-  const { ref, ...otherProps } = contentProps;
+  const { ref, ...propsWithoutRef } = contentProps;
 
-  return <DefaultLegendContent {...otherProps} />;
+  return <DefaultLegendContent {...propsWithoutRef} />;
 }
 
 const EPS = 1;
+
+type BoundingBox = {
+  width: number;
+  height: number;
+};
+
+function getDefaultPosition(style: React.CSSProperties, props: Props, box: BoundingBox) {
+  const { layout, align, verticalAlign, margin, chartWidth, chartHeight } = props;
+  let hPos, vPos;
+
+  if (
+    !style ||
+    ((style.left === undefined || style.left === null) && (style.right === undefined || style.right === null))
+  ) {
+    if (align === 'center' && layout === 'vertical') {
+      hPos = { left: ((chartWidth || 0) - box.width) / 2 };
+    } else {
+      hPos = align === 'right' ? { right: (margin && margin.right) || 0 } : { left: (margin && margin.left) || 0 };
+    }
+  }
+
+  if (
+    !style ||
+    ((style.top === undefined || style.top === null) && (style.bottom === undefined || style.bottom === null))
+  ) {
+    if (verticalAlign === 'middle') {
+      vPos = { top: ((chartHeight || 0) - box.height) / 2 };
+    } else {
+      vPos =
+        verticalAlign === 'bottom' ? { bottom: (margin && margin.bottom) || 0 } : { top: (margin && margin.top) || 0 };
+    }
+  }
+
+  return { ...hPos, ...vPos };
+}
 
 export type Props = DefaultProps & {
   wrapperStyle?: CSSProperties;
@@ -53,6 +92,42 @@ interface State {
   boxHeight: number;
 }
 
+function LegendWrapper(props: Props) {
+  const contextPayload = useLegendPayload();
+  const [lastBoundingBox, setLastBoundingBox] = useState<BoundingBox>({ width: 0, height: 0 });
+  const { width, height, wrapperStyle, onBBoxUpdate } = props;
+  const updateBoundingBox = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (node != null && node.getBoundingClientRect) {
+        const box = node.getBoundingClientRect();
+        if (Math.abs(box.width - lastBoundingBox.width) > EPS || Math.abs(box.height - lastBoundingBox.height) > EPS) {
+          setLastBoundingBox({ width: box.width, height: box.height });
+          if (onBBoxUpdate) {
+            onBBoxUpdate(box);
+          }
+        }
+      }
+    },
+    // The contextPayload is not used directly inside the hook, but we need the onBBoxUpdate call
+    // when the payload changes, therefore it's here as a dependency.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [lastBoundingBox.width, lastBoundingBox.height, onBBoxUpdate, contextPayload],
+  );
+  const outerStyle: CSSProperties = {
+    position: 'absolute',
+    width: width || 'auto',
+    height: height || 'auto',
+    ...getDefaultPosition(wrapperStyle, props, lastBoundingBox),
+    ...wrapperStyle,
+  };
+
+  return (
+    <div className="recharts-legend-wrapper" style={outerStyle} ref={updateBoundingBox}>
+      <LegendContent {...props} contextPayload={contextPayload} />
+    </div>
+  );
+}
+
 export class Legend extends PureComponent<Props, State> {
   static displayName = 'Legend';
 
@@ -62,8 +137,6 @@ export class Legend extends PureComponent<Props, State> {
     align: 'center',
     verticalAlign: 'bottom',
   };
-
-  private wrapperNode: HTMLDivElement;
 
   static getWithHeight(
     item: { props: { layout?: LayoutType; height?: number; width?: number } },
@@ -85,106 +158,7 @@ export class Legend extends PureComponent<Props, State> {
     return null;
   }
 
-  lastBoundingBox = {
-    width: -1,
-    height: -1,
-  };
-
-  public componentDidMount() {
-    this.updateBBox();
-  }
-
-  public componentDidUpdate() {
-    this.updateBBox();
-  }
-
-  private updateBBox() {
-    const { onBBoxUpdate } = this.props;
-
-    if (this.wrapperNode && this.wrapperNode.getBoundingClientRect) {
-      const box = this.wrapperNode.getBoundingClientRect();
-
-      if (
-        Math.abs(box.width - this.lastBoundingBox.width) > EPS ||
-        Math.abs(box.height - this.lastBoundingBox.height) > EPS
-      ) {
-        this.lastBoundingBox.width = box.width;
-        this.lastBoundingBox.height = box.height;
-        if (onBBoxUpdate) {
-          onBBoxUpdate(box);
-        }
-      }
-    } else if (this.lastBoundingBox.width !== -1 || this.lastBoundingBox.height !== -1) {
-      this.lastBoundingBox.width = -1;
-      this.lastBoundingBox.height = -1;
-      if (onBBoxUpdate) {
-        onBBoxUpdate(null);
-      }
-    }
-  }
-
-  private getBBoxSnapshot() {
-    if (this.lastBoundingBox.width >= 0 && this.lastBoundingBox.height >= 0) {
-      return { ...this.lastBoundingBox };
-    }
-
-    return { width: 0, height: 0 };
-  }
-
-  private getDefaultPosition(style: CSSProperties) {
-    const { layout, align, verticalAlign, margin, chartWidth, chartHeight } = this.props;
-    let hPos, vPos;
-
-    if (
-      !style ||
-      ((style.left === undefined || style.left === null) && (style.right === undefined || style.right === null))
-    ) {
-      if (align === 'center' && layout === 'vertical') {
-        const box = this.getBBoxSnapshot();
-        hPos = { left: ((chartWidth || 0) - box.width) / 2 };
-      } else {
-        hPos = align === 'right' ? { right: (margin && margin.right) || 0 } : { left: (margin && margin.left) || 0 };
-      }
-    }
-
-    if (
-      !style ||
-      ((style.top === undefined || style.top === null) && (style.bottom === undefined || style.bottom === null))
-    ) {
-      if (verticalAlign === 'middle') {
-        const box = this.getBBoxSnapshot();
-        vPos = { top: ((chartHeight || 0) - box.height) / 2 };
-      } else {
-        vPos =
-          verticalAlign === 'bottom'
-            ? { bottom: (margin && margin.bottom) || 0 }
-            : { top: (margin && margin.top) || 0 };
-      }
-    }
-
-    return { ...hPos, ...vPos };
-  }
-
   public render() {
-    const { width, height, wrapperStyle } = this.props;
-    const outerStyle: CSSProperties = {
-      position: 'absolute',
-      width: width || 'auto',
-      height: height || 'auto',
-      ...this.getDefaultPosition(wrapperStyle),
-      ...wrapperStyle,
-    };
-
-    return (
-      <div
-        className="recharts-legend-wrapper"
-        style={outerStyle}
-        ref={node => {
-          this.wrapperNode = node;
-        }}
-      >
-        <LegendContent {...this.props} />
-      </div>
-    );
+    return <LegendWrapper {...this.props} />;
   }
 }

test/component/Legend.spec.tsx
@@ -22,7 +22,9 @@ import {
   ScatterChart,
 } from '../../src';
 import { testChartLayoutContext } from '../util/context';
-import { mockGetBoundingClientRect } from '../helper/mockGetBoundingClientRect';
+import { mockGetBoundingClientRect, restoreGetBoundingClientRect } from '../helper/mockGetBoundingClientRect';
+import { LegendPayloadProvider } from '../../src/context/legendPayloadContext';
+import { exampleLegendPayload, MockLegendPayload } from '../helper/MockLegendPayload';
 
 function assertHasLegend(container: HTMLElement) {
   expect(container.querySelectorAll('.recharts-default-legend')).toHaveLength(1);
@@ -305,6 +307,7 @@ function assertExpectedAttributes(
 }
 
 describe('<Legend />', () => {
+  afterEach(restoreGetBoundingClientRect);
   const categoricalData = [
     { value: 'Apple', color: '#ff7300' },
     { value: 'Samsung', color: '#bb7300' },
@@ -348,6 +351,46 @@ describe('<Legend />', () => {
 
       expect(container.querySelectorAll('.recharts-default-legend')).toHaveLength(0);
       expect(container.querySelectorAll('.recharts-default-legend .recharts-legend-item')).toHaveLength(0);
+    });
+  });
+
+  describe('onBBoxUpdate', () => {
+    it('should call onBBoxUpdate once on mount', () => {
+      const onBBoxUpdate = vi.fn();
+      mockGetBoundingClientRect({ width: 50, height: 15 });
+      render(<Legend onBBoxUpdate={onBBoxUpdate} />);
+      expect(onBBoxUpdate).toHaveBeenCalledTimes(1);
+      expect(onBBoxUpdate).toHaveBeenCalledWith(expect.objectContaining({ width: 50, height: 15 }));
+    });
+
+    it('should call onBBoxUpdate if the payload changes', () => {
+      const onBBoxUpdate = vi.fn();
+      mockGetBoundingClientRect({ width: 50, height: 15 });
+      const { rerender } = render(
+        <LegendPayloadProvider>
+          <Legend onBBoxUpdate={onBBoxUpdate} />
+        </LegendPayloadProvider>,
+      );
+
+      expect(onBBoxUpdate).toHaveBeenCalledTimes(1);
+      expect(onBBoxUpdate).toHaveBeenCalledWith(expect.objectContaining({ width: 50, height: 15 }));
+      mockGetBoundingClientRect({ width: 50, height: 25 });
+      rerender(
+        <LegendPayloadProvider>
+          <MockLegendPayload payload={exampleLegendPayload} />
+          <Legend onBBoxUpdate={onBBoxUpdate} />
+        </LegendPayloadProvider>,
+      );
+      expect(onBBoxUpdate).toHaveBeenCalledTimes(2);
+      mockGetBoundingClientRect({ width: 50, height: 0 });
+      rerender(
+        <LegendPayloadProvider>
+          <MockLegendPayload payload={[]} />
+          <Legend onBBoxUpdate={onBBoxUpdate} />
+        </LegendPayloadProvider>,
+      );
+      expect(onBBoxUpdate).toHaveBeenCalledTimes(3);
+      expect(onBBoxUpdate).toHaveBeenCalledWith(expect.objectContaining({ width: 50, height: 0 }));
     });
   });
 
@@ -1061,6 +1104,50 @@ describe('<Legend />', () => {
       expect(legendItems[0].textContent).toBe('value');
     });
 
+    it('should push away Bars to make space', () => {
+      Element.prototype.getBoundingClientRect = () => ({
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 10,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        toJSON: () => '{}',
+      });
+
+      const { container, rerender } = render(
+        <BarChart width={500} height={500} data={numericalData}>
+          <Legend />
+          <Bar isAnimationActive={false} dataKey="percent" />
+        </BarChart>,
+      );
+      expect(container.querySelectorAll('.recharts-default-legend')).toHaveLength(1);
+      const bars1 = container.querySelectorAll('.recharts-bar-rectangle path');
+      expect.soft(bars1).toHaveLength(numericalData.length);
+      const bar1 = bars1[0];
+      expect(bar1).toBeInTheDocument();
+      expect.soft(bar1.getAttribute('d')).toBe('M 13.166666666666668,437 h 65 v 48 h -65 Z');
+      expect.soft(bar1.getAttribute('height')).toBe('48');
+      expect.soft(bar1.getAttribute('y')).toBe('437');
+
+      rerender(
+        <BarChart width={500} height={500} data={numericalData}>
+          <Bar isAnimationActive={false} dataKey="percent" />
+        </BarChart>,
+      );
+      // debug();
+      expect(container.querySelectorAll('.recharts-default-legend')).toHaveLength(0);
+      const bars2 = container.querySelectorAll('.recharts-bar-rectangle path');
+      expect.soft(bars2).toHaveLength(numericalData.length);
+      const bar2 = bars2[0];
+      expect.soft(bar2).toBeInTheDocument();
+      expect.soft(bar2.getAttribute('d')).toBe('M 13.166666666666668,446 h 65 v 49 h -65 Z');
+      expect.soft(bar2.getAttribute('height')).toBe('49');
+      expect.soft(bar2.getAttribute('y')).toBe('446');
+    });
+
     it('should render a legend item even if the dataKey does not match anything from the data', () => {
       const { container, getByText } = render(
         <BarChart width={500} height={500} data={numericalData}>
@@ -1339,7 +1426,6 @@ describe('<Legend />', () => {
         align: LegendProps['align'];
         verticalAlign: LegendProps['verticalAlign'];
         layout: LegendProps['layout'];
-        expectedStyle: string;
         expectedStyleOnSecondRender: string;
       };
 
@@ -1348,126 +1434,108 @@ describe('<Legend />', () => {
           align: 'center',
           verticalAlign: 'top',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; left: 17px; top: 11px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; left: 17px; top: 11px;',
         },
         {
           align: 'left',
           verticalAlign: 'top',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; left: 17px; top: 11px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; left: 17px; top: 11px;',
         },
         {
           align: 'right',
           verticalAlign: 'top',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; right: 13px; top: 11px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; right: 13px; top: 11px;',
         },
         {
           align: 'center',
           verticalAlign: 'bottom',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; left: 17px; bottom: 19px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; left: 17px; bottom: 19px;',
         },
         {
           align: 'left',
           verticalAlign: 'bottom',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; left: 17px; bottom: 19px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; left: 17px; bottom: 19px;',
         },
         {
           align: 'right',
           verticalAlign: 'bottom',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; right: 13px; bottom: 19px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; right: 13px; bottom: 19px;',
         },
         {
           align: 'center',
           verticalAlign: 'middle',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; left: 17px; top: 350px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; left: 17px; top: 335.5px;',
         },
         {
           align: 'left',
           verticalAlign: 'middle',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; left: 17px; top: 350px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; left: 17px; top: 335.5px;',
         },
         {
           align: 'right',
           verticalAlign: 'middle',
           layout: 'horizontal',
-          expectedStyle: 'position: absolute; width: 470px; height: auto; right: 13px; top: 350px;',
           expectedStyleOnSecondRender: 'position: absolute; width: 470px; height: auto; right: 13px; top: 335.5px;',
         },
         {
           align: 'center',
           verticalAlign: 'top',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; left: 250px; top: 11px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; left: 238.5px; top: 11px;',
         },
         {
           align: 'left',
           verticalAlign: 'top',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; left: 17px; top: 11px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; left: 17px; top: 11px;',
         },
         {
           align: 'right',
           verticalAlign: 'top',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; right: 13px; top: 11px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; right: 13px; top: 11px;',
         },
         {
           align: 'center',
           verticalAlign: 'bottom',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; left: 250px; bottom: 19px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; left: 238.5px; bottom: 19px;',
         },
         {
           align: 'left',
           verticalAlign: 'bottom',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; left: 17px; bottom: 19px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; left: 17px; bottom: 19px;',
         },
         {
           align: 'right',
           verticalAlign: 'bottom',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; right: 13px; bottom: 19px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; right: 13px; bottom: 19px;',
         },
         {
           align: 'center',
           verticalAlign: 'middle',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; left: 250px; top: 350px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; left: 238.5px; top: 335.5px;',
         },
         {
           align: 'left',
           verticalAlign: 'middle',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; left: 17px; top: 350px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; left: 17px; top: 335.5px;',
         },
         {
           align: 'right',
           verticalAlign: 'middle',
           layout: 'vertical',
-          expectedStyle: 'position: absolute; width: auto; height: auto; right: 13px; top: 350px;',
           expectedStyleOnSecondRender: 'position: absolute; width: auto; height: auto; right: 13px; top: 335.5px;',
         },
       ];
@@ -1486,7 +1554,7 @@ describe('<Legend />', () => {
 
       test.each(layoutPositionCartesianTests)(
         'should calculate position for align=$align, verticalAlign=$verticalAlign, layout=$layout',
-        ({ align, verticalAlign, layout, expectedStyle, expectedStyleOnSecondRender }) => {
+        ({ align, verticalAlign, layout, expectedStyleOnSecondRender }) => {
           mockGetBoundingClientRect({
             width: 23,
             height: 29,
@@ -1506,7 +1574,7 @@ describe('<Legend />', () => {
           expect(wrapper).toBeInTheDocument();
           expect.soft(wrapper.getAttributeNames()).toEqual(['class', 'style']);
           expect.soft(wrapper.getAttribute('class')).toBe('recharts-legend-wrapper');
-          expect.soft(wrapper.getAttribute('style')).toBe(expectedStyle);
+          expect.soft(wrapper.getAttribute('style')).toBe(expectedStyleOnSecondRender);
           /*
            * Because the bounding box is set as a class property instead of a state,
            * reading the legend width and height does not trigger re-render!

test/helper/MockLegendPayload.tsx
@@ -1,0 +1,16 @@
+import { ReactElement, useCallback } from 'react';
+import { useLegendPayloadDispatch } from '../../src/context/legendPayloadContext';
+import { Payload as LegendPayload } from '../../src/component/DefaultLegendContent';
+
+export const exampleLegendPayload: LegendPayload[] = [
+  {
+    type: 'cross',
+    value: 'test',
+  },
+];
+
+export function MockLegendPayload({ payload }: { payload: LegendPayload[] }): ReactElement {
+  const computeLegendPayload = useCallback(() => payload, [payload]);
+  useLegendPayloadDispatch(computeLegendPayload, undefined);
+  return null;
+}

test/helper/mockGetBoundingClientRect.ts
@@ -1,3 +1,5 @@
+const original = Element.prototype.getBoundingClientRect;
+
 /**
  * getBoundingClientRect always returns 0 in jsdom, we can't test for actual returned string size
  * Execution order matters
@@ -7,8 +9,7 @@
  * @returns cleanup function, convenient for beforeAll or beforeEach
  */
 export function mockGetBoundingClientRect(rect: Partial<DOMRect>) {
-  const original = Element.prototype.getBoundingClientRect;
-  Element.prototype.getBoundingClientRect = () => ({
+  const mockDomRect = {
     x: 0,
     y: 0,
     width: 0,
@@ -17,10 +18,17 @@ export function mockGetBoundingClientRect(rect: Partial<DOMRect>) {
     right: 0,
     bottom: 0,
     left: 0,
-    toJSON: () => '{}',
     ...rect,
+  };
+  Element.prototype.getBoundingClientRect = () => ({
+    ...mockDomRect,
+    toJSON: () => JSON.stringify(mockDomRect),
   });
   return function cleanup() {
     Element.prototype.getBoundingClientRect = original;
   };
+}
+
+export function restoreGetBoundingClientRect() {
+  Element.prototype.getBoundingClientRect = original;
 }