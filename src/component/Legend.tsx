61761869b5fd221f7b3b0c719fbf4c8866678c7b
Pavel Vanecek <corkscreewe@gmail.com>
2024-03-21 21:32:09 +0900

    Add comment

--- src/component/Legend.tsx
+++ src/component/Legend.tsx
@@ -94,6 +94,19 @@ interface State {
   boxHeight: number;
 }
 
+/**
+ * Use this to listen to bounding box changes.
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+ *
+ * Very useful for reading actual sizes of DOM elements.
+ *
+ * Be aware that this is difficult to test for. jsdom, by design, returns all zeroes!
+ * If you want to write tests check out the utility function `mockGetBoundingClientRect`
+ *
+ * @param onUpdate this is extra callback that's called with the full DOMRect. Note that this receives different object than the return value.
+ * @param extraDependencies use this to trigger new DOM dimensions read when any of these change. Good for things like payload and label, that will re-render something down in the children array, but you want to read the bounding box of a parent.
+ * @returns [lastBoundingBox, updateBoundingBox] most recent value, and setter. Pass the setter to a DOM element ref like this: `<div ref={updateBoundingBox}>`
+ */
 function useGetBoundingClientRect(
   onUpdate: (domRect: DOMRect | null) => void,
   extraDependencies: ReadonlyArray<unknown>,
