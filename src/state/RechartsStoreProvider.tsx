2a9981a0c4a271cf0a055b1ce1fb792b432a4078
Pavel Vanecek <corkscreewe@gmail.com>
2024-04-15 21:02:22 +0900

    Add comment explaining the use of ref instead of static store
    Followup from https://github.com/recharts/recharts/pull/4399#discussion_r1565289444

--- src/state/RechartsStoreProvider.tsx
+++ src/state/RechartsStoreProvider.tsx
@@ -9,6 +9,17 @@ type RechartsStoreProviderProps = {
 };
 
 export function RechartsStoreProvider({ preloadedState, children, reduxStoreName }: RechartsStoreProviderProps) {
+  /*
+   * Why the ref? Redux official documentation recommends to use store as a singleton,
+   * and reuse that everywhere: https://redux-toolkit.js.org/api/configureStore#basic-example
+   *
+   * Which is correct! Except that is considering deploying Redux in an app.
+   * Recharts as a library supports multiple charts on the same page.
+   * And each of these charts needs its own store independent of others!
+   *
+   * The alternative is to have everything in the store keyed by the chart id.
+   * Which would make working with everything a little bit more painful because we need the chart id everywhere.
+   */
   const storeRef = useRef(null);
   if (storeRef.current == null) {
     storeRef.current = createRechartsStore(preloadedState, reduxStoreName);
