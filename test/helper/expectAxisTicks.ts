9f4d210fdc9c761990af2faea480480d11c442e0
Pavel Vaněček <corkscreewe+github@gmail.com>
2024-06-05 03:04:57 +1000

    Add more XAxis domain tests (#4625)

--- test/helper/expectAxisTicks.ts
+++ test/helper/expectAxisTicks.ts
@@ -1,6 +1,6 @@
 import { assertNotNull } from './assertNotNull';
 
-export function expectXAxisTicks(container: HTMLElement, ticks: ReadonlyArray<string>) {
+export function expectXAxisTicks(container: Element, ticks: ReadonlyArray<string>) {
   const allTicks = container.querySelectorAll('.recharts-xAxis .recharts-cartesian-axis-tick');
   assertNotNull(allTicks);
   const ticksContexts = Array.from(allTicks).map(tick => tick.textContent);
