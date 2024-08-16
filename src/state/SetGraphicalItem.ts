9bbe1d36607f3723afb1fe11c51235cdcf7562da
Coltin Kifer <coltinkifer19@gmail.com>
2024-08-15 22:13:47 -0700

    refactor(Pie): move static functions out of class, add internal props add polargraphical item (#4899)

--- src/state/SetGraphicalItem.ts
+++ src/state/SetGraphicalItem.ts
@@ -2,8 +2,11 @@ import { useEffect } from 'react';
 import { useAppDispatch } from './hooks';
 import {
   addCartesianGraphicalItem,
+  addPolarGraphicalItem,
   CartesianGraphicalItemSettings,
+  PolarGraphicalItemSettings,
   removeCartesianGraphicalItem,
+  removePolarGraphicalItem,
 } from './graphicalItemsSlice';
 
 export function SetCartesianGraphicalItem(props: CartesianGraphicalItemSettings): null {
@@ -12,6 +15,17 @@ export function SetCartesianGraphicalItem(props: CartesianGraphicalItemSettings)
     dispatch(addCartesianGraphicalItem(props));
     return () => {
       dispatch(removeCartesianGraphicalItem(props));
+    };
+  }, [dispatch, props]);
+  return null;
+}
+
+export function SetPolarGraphicalItem(props: PolarGraphicalItemSettings): null {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(addPolarGraphicalItem(props));
+    return () => {
+      dispatch(removePolarGraphicalItem(props));
     };
   }, [dispatch, props]);
   return null;
