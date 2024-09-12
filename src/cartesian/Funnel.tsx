836dd443c956cf2c203b93878275c1195a7ea450
Coltin Kifer <coltinkifer19@gmail.com>
2024-09-12 14:31:04 -0700

    refactor(Funnel): move classes around to support hooks and internal state (#4998)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    No logic changes, move classes around as like the other components
    
    * move Funnel to have separate Impl/WithState components
    * remove `labelLine` prop - this was never used
    
    Next, change the composed function to compute
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    https://github.com/recharts/recharts/issues/4583
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    almost done with generator charts
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    - npm test, no storybook diff
    
    
    https://www.chromatic.com/build?appId=63da8268a0da9970db6992aa&number=1429
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] I have added a storybook story or extended an existing story to
    show my changes
    
    Co-authored-by: Coltin Kifer <ckifer@amazon.com>

--- src/cartesian/Funnel.tsx
+++ src/cartesian/Funnel.tsx
@@ -1,6 +1,4 @@
-/**
- * @fileOverview Render sectors of a funnel
- */
+/* eslint-disable max-classes-per-file */
 import React, { PureComponent, ReactElement } from 'react';
 import Animate from 'react-smooth';
 import isFunction from 'lodash/isFunction';
@@ -39,7 +37,7 @@ import {
 } from '../context/tooltipContext';
 import { TooltipPayloadConfiguration } from '../state/tooltipSlice';
 import { SetTooltipEntrySettings } from '../state/SetTooltipEntrySettings';
-import { UpdateId } from '../context/chartLayoutContext';
+import { UpdateId, useOffset, useUpdateId } from '../context/chartLayoutContext';
 
 export interface FunnelTrapezoidItem extends TrapezoidProps {
   value?: number | string;
@@ -186,158 +184,48 @@ function FunnelTrapezoids(props: FunnelTrapezoidsProps) {
   });
 }
 
-export class Funnel extends PureComponent<Props, State> {
-  static displayName = 'Funnel';
+const getRealWidthHeight = (item: Funnel, offset: ChartOffset) => {
+  const customWidth = item.props.width;
+  const { width, height, left, right, top, bottom } = offset;
+  const realHeight = height;
+  let realWidth = width;
 
-  static defaultProps = {
-    stroke: '#fff',
-    fill: '#808080',
-    legendType: 'rect',
-    labelLine: true,
-    hide: false,
-    isAnimationActive: !Global.isSsr,
-    animationBegin: 400,
-    animationDuration: 1500,
-    animationEasing: 'ease',
-    nameKey: 'name',
-    lastShapeType: 'triangle',
+  if (isNumber(customWidth)) {
+    realWidth = customWidth;
+  } else if (isString(customWidth)) {
+    realWidth = (realWidth * parseFloat(customWidth)) / 100;
+  }
+
+  return {
+    realWidth: realWidth - left - right - 50,
+    realHeight: realHeight - bottom - top,
+    offsetX: (width - realWidth) / 2,
+    offsetY: (height - realHeight) / 2,
   };
+};
 
-  static getRealFunnelData = (item: Funnel): RealFunnelData[] => {
-    const { data, children } = item.props;
-    const presentationProps = filterProps(item.props, false);
-    const cells = findAllByType(children, Cell);
+const getRealFunnelData = (item: Funnel): RealFunnelData[] => {
+  const { data, children } = item.props;
+  const presentationProps = filterProps(item.props, false);
+  const cells = findAllByType(children, Cell);
 
-    if (data && data.length) {
-      return data.map((entry: any, index: number) => ({
-        payload: entry,
-        ...presentationProps,
-        ...entry,
-        ...(cells && cells[index] && cells[index].props),
-      }));
-    }
+  if (data && data.length) {
+    return data.map((entry: any, index: number) => ({
+      payload: entry,
+      ...presentationProps,
+      ...entry,
+      ...(cells && cells[index] && cells[index].props),
+    }));
+  }
 
-    if (cells && cells.length) {
-      return cells.map((cell: ReactElement<CellProps>) => ({ ...presentationProps, ...cell.props }));
-    }
+  if (cells && cells.length) {
+    return cells.map((cell: ReactElement<CellProps>) => ({ ...presentationProps, ...cell.props }));
+  }
 
-    return [];
-  };
+  return [];
+};
 
-  static getRealWidthHeight = (item: Funnel, offset: ChartOffset) => {
-    const customWidth = item.props.width;
-    const { width, height, left, right, top, bottom } = offset;
-    const realHeight = height;
-    let realWidth = width;
-
-    if (isNumber(customWidth)) {
-      realWidth = customWidth;
-    } else if (isString(customWidth)) {
-      realWidth = (realWidth * parseFloat(customWidth)) / 100;
-    }
-
-    return {
-      realWidth: realWidth - left - right - 50,
-      realHeight: realHeight - bottom - top,
-      offsetX: (width - realWidth) / 2,
-      offsetY: (height - realHeight) / 2,
-    };
-  };
-
-  static getComposedData = ({ item, offset }: { item: Funnel; offset: ChartOffset }): FunnelComposedData => {
-    const funnelData = Funnel.getRealFunnelData(item);
-    const { dataKey, nameKey, tooltipType, lastShapeType, reversed } = item.props;
-    const { left, top } = offset;
-    const { realHeight, realWidth, offsetX, offsetY } = Funnel.getRealWidthHeight(item, offset);
-    const maxValue = Math.max.apply(
-      null,
-      funnelData.map((entry: any) => getValueByDataKey(entry, dataKey, 0)),
-    );
-    const len = funnelData.length;
-    const rowHeight = realHeight / len;
-    const parentViewBox = { x: offset.left, y: offset.top, width: offset.width, height: offset.height };
-
-    let trapezoids = funnelData.map((entry: any, i: number): FunnelTrapezoidItem => {
-      const rawVal = getValueByDataKey(entry, dataKey, 0);
-      const name = getValueByDataKey(entry, nameKey, i);
-      let val = rawVal;
-      let nextVal;
-
-      if (i !== len - 1) {
-        nextVal = getValueByDataKey(funnelData[i + 1], dataKey, 0);
-
-        if (nextVal instanceof Array) {
-          [nextVal] = nextVal;
-        }
-      } else if (rawVal instanceof Array && rawVal.length === 2) {
-        [val, nextVal] = rawVal;
-      } else if (lastShapeType === 'rectangle') {
-        nextVal = val;
-      } else {
-        nextVal = 0;
-      }
-
-      // @ts-expect-error getValueByDataKey does not validate the output type
-      const x = ((maxValue - val) * realWidth) / (2 * maxValue) + top + 25 + offsetX;
-      const y = rowHeight * i + left + offsetY;
-      // @ts-expect-error getValueByDataKey does not validate the output type
-      const upperWidth = (val / maxValue) * realWidth;
-      const lowerWidth = (nextVal / maxValue) * realWidth;
-
-      const tooltipPayload = [{ name, value: val, payload: entry, dataKey, type: tooltipType }];
-      const tooltipPosition = {
-        x: x + upperWidth / 2,
-        y: y + rowHeight / 2,
-      };
-
-      return {
-        x,
-        y,
-        width: Math.max(upperWidth, lowerWidth),
-        upperWidth,
-        lowerWidth,
-        height: rowHeight,
-        // @ts-expect-error getValueByDataKey does not validate the output type
-        name,
-        val,
-        tooltipPayload,
-        tooltipPosition,
-        ...omit(entry, 'width'),
-        payload: entry,
-        parentViewBox,
-        labelViewBox: {
-          x: x + (upperWidth - lowerWidth) / 4,
-          y,
-          width: Math.abs(upperWidth - lowerWidth) / 2 + Math.min(upperWidth, lowerWidth),
-          height: rowHeight,
-        },
-      };
-    });
-
-    if (reversed) {
-      trapezoids = trapezoids.map((entry: any, index: number) => {
-        const newY = entry.y - index * rowHeight + (len - 1 - index) * rowHeight;
-        return {
-          ...entry,
-          upperWidth: entry.lowerWidth,
-          lowerWidth: entry.upperWidth,
-          x: entry.x - (entry.lowerWidth - entry.upperWidth) / 2,
-          y: entry.y - index * rowHeight + (len - 1 - index) * rowHeight,
-          tooltipPosition: { ...entry.tooltipPosition, y: newY + rowHeight / 2 },
-          labelViewBox: {
-            ...entry.labelViewBox,
-            y: newY,
-          },
-        };
-      });
-    }
-
-    return {
-      trapezoids,
-      data: funnelData,
-    };
-  };
-
+export class FunnelWithState extends PureComponent<Props, State> {
   state: State = { isAnimationFinished: false };
 
   static getDerivedStateFromProps(nextProps: Props, prevState: State): State {
@@ -463,21 +351,186 @@ export class Funnel extends PureComponent<Props, State> {
   }
 
   render() {
-    const { hide, trapezoids, className, isAnimationActive } = this.props;
+    const { trapezoids, className, isAnimationActive } = this.props;
     const { isAnimationFinished } = this.state;
-
-    if (hide || !trapezoids || !trapezoids.length) {
-      return <SetTooltipEntrySettings fn={getTooltipEntrySettings} args={this.props} />;
-    }
 
     const layerClass = clsx('recharts-trapezoids', className);
 
     return (
       <Layer className={layerClass}>
-        <SetTooltipEntrySettings fn={getTooltipEntrySettings} args={this.props} />
         {this.renderTrapezoids()}
         {(!isAnimationActive || isAnimationFinished) && LabelList.renderCallByParent(this.props, trapezoids)}
       </Layer>
+    );
+  }
+}
+
+const defaultFunnelProps: Partial<Props> = {
+  stroke: '#fff',
+  fill: '#808080',
+  legendType: 'rect',
+  hide: false,
+  isAnimationActive: !Global.isSsr,
+  animationBegin: 400,
+  animationDuration: 1500,
+  animationEasing: 'ease',
+  nameKey: 'name',
+  lastShapeType: 'triangle',
+};
+
+function FunnelImpl(props: Props) {
+  const { height, width } = useOffset();
+
+  const updateId = useUpdateId();
+
+  const {
+    ref,
+    stroke = defaultFunnelProps.stroke,
+    fill = defaultFunnelProps.fill,
+    legendType = defaultFunnelProps.legendType,
+    hide = defaultFunnelProps.hide,
+
+    isAnimationActive = defaultFunnelProps.isAnimationActive,
+    animationBegin = defaultFunnelProps.animationBegin,
+    animationDuration = defaultFunnelProps.animationDuration,
+    animationEasing = defaultFunnelProps.animationEasing,
+    nameKey = defaultFunnelProps.nameKey,
+    lastShapeType = defaultFunnelProps.lastShapeType,
+    ...everythingElse
+  } = props;
+
+  return (
+    <FunnelWithState
+      {...everythingElse}
+      stroke={stroke}
+      fill={fill}
+      nameKey={nameKey}
+      lastShapeType={lastShapeType}
+      animationId={updateId}
+      animationBegin={animationBegin}
+      animationDuration={animationDuration}
+      animationEasing={animationEasing}
+      isAnimationActive={isAnimationActive}
+      hide={hide}
+      legendType={legendType}
+      height={height}
+      width={width}
+      trapezoids={props.trapezoids}
+    />
+  );
+}
+
+export class Funnel extends PureComponent<Props> {
+  static displayName = 'Funnel';
+
+  static defaultProps = defaultFunnelProps;
+
+  static getComposedData = ({ item, offset }: { item: Funnel; offset: ChartOffset }): FunnelComposedData => {
+    const funnelData = getRealFunnelData(item);
+    const { dataKey, nameKey, tooltipType, lastShapeType, reversed } = item.props;
+    const { left, top } = offset;
+    const { realHeight, realWidth, offsetX, offsetY } = getRealWidthHeight(item, offset);
+    const maxValue = Math.max.apply(
+      null,
+      funnelData.map((entry: any) => getValueByDataKey(entry, dataKey, 0)),
+    );
+    const len = funnelData.length;
+    const rowHeight = realHeight / len;
+    const parentViewBox = { x: offset.left, y: offset.top, width: offset.width, height: offset.height };
+
+    let trapezoids = funnelData.map((entry: any, i: number): FunnelTrapezoidItem => {
+      const rawVal = getValueByDataKey(entry, dataKey, 0);
+      const name = getValueByDataKey(entry, nameKey, i);
+      let val = rawVal;
+      let nextVal;
+
+      if (i !== len - 1) {
+        nextVal = getValueByDataKey(funnelData[i + 1], dataKey, 0);
+
+        if (nextVal instanceof Array) {
+          [nextVal] = nextVal;
+        }
+      } else if (rawVal instanceof Array && rawVal.length === 2) {
+        [val, nextVal] = rawVal;
+      } else if (lastShapeType === 'rectangle') {
+        nextVal = val;
+      } else {
+        nextVal = 0;
+      }
+
+      // @ts-expect-error getValueByDataKey does not validate the output type
+      const x = ((maxValue - val) * realWidth) / (2 * maxValue) + top + 25 + offsetX;
+      const y = rowHeight * i + left + offsetY;
+      // @ts-expect-error getValueByDataKey does not validate the output type
+      const upperWidth = (val / maxValue) * realWidth;
+      const lowerWidth = (nextVal / maxValue) * realWidth;
+
+      const tooltipPayload = [{ name, value: val, payload: entry, dataKey, type: tooltipType }];
+      const tooltipPosition = {
+        x: x + upperWidth / 2,
+        y: y + rowHeight / 2,
+      };
+
+      return {
+        x,
+        y,
+        width: Math.max(upperWidth, lowerWidth),
+        upperWidth,
+        lowerWidth,
+        height: rowHeight,
+        // @ts-expect-error getValueByDataKey does not validate the output type
+        name,
+        val,
+        tooltipPayload,
+        tooltipPosition,
+        ...omit(entry, 'width'),
+        payload: entry,
+        parentViewBox,
+        labelViewBox: {
+          x: x + (upperWidth - lowerWidth) / 4,
+          y,
+          width: Math.abs(upperWidth - lowerWidth) / 2 + Math.min(upperWidth, lowerWidth),
+          height: rowHeight,
+        },
+      };
+    });
+
+    if (reversed) {
+      trapezoids = trapezoids.map((entry: any, index: number) => {
+        const newY = entry.y - index * rowHeight + (len - 1 - index) * rowHeight;
+        return {
+          ...entry,
+          upperWidth: entry.lowerWidth,
+          lowerWidth: entry.upperWidth,
+          x: entry.x - (entry.lowerWidth - entry.upperWidth) / 2,
+          y: entry.y - index * rowHeight + (len - 1 - index) * rowHeight,
+          tooltipPosition: { ...entry.tooltipPosition, y: newY + rowHeight / 2 },
+          labelViewBox: {
+            ...entry.labelViewBox,
+            y: newY,
+          },
+        };
+      });
+    }
+
+    return {
+      trapezoids,
+      data: funnelData,
+    };
+  };
+
+  render() {
+    const { hide, trapezoids } = this.props;
+
+    if (hide || !trapezoids || !trapezoids.length) {
+      return <SetTooltipEntrySettings fn={getTooltipEntrySettings} args={this.props} />;
+    }
+
+    return (
+      <>
+        <SetTooltipEntrySettings fn={getTooltipEntrySettings} args={this.props} />
+        <FunnelImpl {...this.props} />
+      </>
     );
   }
 }
