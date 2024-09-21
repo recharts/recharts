4791a753505c8cb61894919278f9341cac735985
Pavel Vanecek <corkscreewe@gmail.com>
2024-09-21 15:25:07 +0900

    PolarRadiusAxis.spec.tsx renders axis inside chart, not inside Surface

--- test/polar/PolarRadiusAxis.spec.tsx
+++ test/polar/PolarRadiusAxis.spec.tsx
@@ -71,205 +71,204 @@ describe('<PolarRadiusAxis />', () => {
     { coordinate: 90 },
   ];
 
-  test('Renders 5 ticks when orientation is set to be middle', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <PolarRadiusAxis orientation="middle" cx={250} cy={250} ticks={ticks} label="test" />
-      </Surface>,
-    );
-
-    expectRadiusAxisTicks(container, [
-      {
-        textContent: '',
-        transform: 'rotate(90, 260, 250)',
-        x: '260',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 1250, 250)',
-        x: '1250',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 270, 250)',
-        x: '270',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 290, 250)',
-        x: '290',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 340, 250)',
-        x: '340',
-        y: '250',
-      },
-    ]);
-
-    expectRadiusAxisLabel(container, {
-      textContent: 'test',
-      x: '755',
-      y: '250',
-    });
-  });
-
-  test('Renders 5 ticks when orientation is set to be left', () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <PolarRadiusAxis orientation="left" cx={250} cy={250} ticks={ticks} label="test" />
-      </Surface>,
-    );
-
-    expectRadiusAxisTicks(container, [
-      {
-        textContent: '',
-        transform: 'rotate(90, 260, 250)',
-        x: '260',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 1250, 250)',
-        x: '1250',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 270, 250)',
-        x: '270',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 290, 250)',
-        x: '290',
-        y: '250',
-      },
-      {
-        textContent: '',
-        transform: 'rotate(90, 340, 250)',
-        x: '340',
-        y: '250',
-      },
-    ]);
-
-    expectRadiusAxisLabel(container, {
-      textContent: 'test',
-      x: '755',
-      y: '250',
-    });
-  });
-
-  test('Renders 5 ticks when tick is set to be a function', () => {
-    const renderTick = (props: any) => {
-      const { payload, x, y } = props;
-
-      return (
-        <text x={x} y={y} className="customized-tick">
-          {payload.value}
-        </text>
-      );
-    };
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <PolarRadiusAxis
-          orientation="left"
-          cx={250}
-          cy={250}
-          ticks={ticks as TickItem[]}
-          label="test"
-          tick={renderTick}
-        />
-      </Surface>,
-    );
-
-    expect(container.querySelectorAll('.customized-tick')).toHaveLength(5);
-    expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
-  });
-
-  test('Renders 5 ticks when tick is set to be a react element', () => {
-    const Tick = (props: any) => {
-      const { payload, x, y } = props;
-
-      return (
-        <text x={x} y={y} className="customized-tick">
-          {payload.value}
-        </text>
-      );
-    };
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <PolarRadiusAxis
-          orientation="left"
-          cx={250}
-          cy={250}
-          ticks={ticks as TickItem[]}
-          label="test"
-          tick={<Tick />}
-        />
-      </Surface>,
-    );
-
-    expect(container.querySelectorAll('.customized-tick')).toHaveLength(5);
-    expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
-  });
-
-  test('Renders label when label is set to be a function', () => {
-    const renderLabel = (props: any) => {
-      const { x, y } = props;
-
-      return (
-        <text x={x} y={y} className="customized-label">
-          any
-        </text>
-      );
-    };
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <PolarRadiusAxis orientation="left" cx={250} cy={250} ticks={ticks as TickItem[]} label={renderLabel} />
-      </Surface>,
-    );
-
-    expect(container.querySelectorAll('.customized-label')).toHaveLength(1);
-  });
-
-  test('Renders label when label is set to be a react element', () => {
-    const Label = (props: any) => {
-      const { x, y } = props;
-
-      return (
-        <text x={x} y={y} className="customized-label">
-          any
-        </text>
-      );
-    };
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <PolarRadiusAxis orientation="left" cx={250} cy={250} ticks={ticks} label={<Label />} />
-      </Surface>,
-    );
-
-    expect(container.querySelectorAll('.customized-label')).toHaveLength(1);
-  });
-
-  test("Don't Renders any ticks or label in when ticks is empty", () => {
-    const { container } = render(
-      <Surface width={500} height={500}>
-        <PolarRadiusAxis orientation="left" cx={250} cy={250} ticks={[]} label="test" />
-      </Surface>,
-    );
-
-    expectRadiusAxisTicks(container, []);
-
-    expectRadiusAxisLabel(container, undefined);
-  });
-
   describe('in RadarChart', () => {
+    test('Renders ticks when orientation=middle', () => {
+      const { container } = render(
+        <RadarChart width={500} height={500} data={exampleRadarData}>
+          <PolarRadiusAxis orientation="middle" cx={250} cy={250} label="test" dataKey="value" />
+        </RadarChart>,
+      );
+
+      expectRadiusAxisTicks(container, [
+        {
+          textContent: '0',
+          transform: 'rotate(90, 250, 250)',
+          x: '250',
+          y: '250',
+        },
+        {
+          textContent: '250',
+          transform: 'rotate(90, 299, 250)',
+          x: '299',
+          y: '250',
+        },
+        {
+          textContent: '500',
+          transform: 'rotate(90, 348, 250)',
+          x: '348',
+          y: '250',
+        },
+        {
+          textContent: '750',
+          transform: 'rotate(90, 397, 250)',
+          x: '397',
+          y: '250',
+        },
+        {
+          textContent: '1000',
+          transform: 'rotate(90, 446, 250)',
+          x: '446',
+          y: '250',
+        },
+      ]);
+
+      expectRadiusAxisLabel(container, {
+        textContent: 'test',
+        x: '348',
+        y: '250',
+      });
+    });
+
+    test('Renders ticks when orientation=left', () => {
+      const { container } = render(
+        <RadarChart width={500} height={500} data={exampleRadarData}>
+          <Radar dataKey="value" />
+          <PolarRadiusAxis orientation="left" cx={250} cy={250} label="test" />
+        </RadarChart>,
+      );
+
+      expectRadiusAxisTicks(container, [
+        {
+          textContent: '0',
+          transform: 'rotate(90, 250, 250)',
+          x: '250',
+          y: '250',
+        },
+        {
+          textContent: '250',
+          transform: 'rotate(90, 299, 250)',
+          x: '299',
+          y: '250',
+        },
+        {
+          textContent: '500',
+          transform: 'rotate(90, 348, 250)',
+          x: '348',
+          y: '250',
+        },
+        {
+          textContent: '750',
+          transform: 'rotate(90, 397, 250)',
+          x: '397',
+          y: '250',
+        },
+        {
+          textContent: '1000',
+          transform: 'rotate(90, 446, 250)',
+          x: '446',
+          y: '250',
+        },
+      ]);
+
+      expectRadiusAxisLabel(container, {
+        textContent: 'test',
+        x: '348',
+        y: '250',
+      });
+    });
+
+    test('Renders ticks when tick is set to be a function', () => {
+      const renderTick = (props: any) => {
+        const { payload, x, y } = props;
+
+        return (
+          <text x={x} y={y} className="customized-tick">
+            {payload.value}
+          </text>
+        );
+      };
+      const { container } = render(
+        <RadarChart width={500} height={500} data={exampleRadarData}>
+          <PolarRadiusAxis orientation="left" cx={250} cy={250} label="test" tick={renderTick} dataKey="value" />
+        </RadarChart>,
+      );
+
+      expect(container.querySelectorAll('.customized-tick')).toHaveLength(5);
+      expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
+    });
+
+    test('Renders ticks when tick is set to be a react element', () => {
+      const Tick = (props: any) => {
+        const { payload, x, y } = props;
+
+        return (
+          <text x={x} y={y} className="customized-tick">
+            {payload.value}
+          </text>
+        );
+      };
+      const { container } = render(
+        <RadarChart width={500} height={500} data={exampleRadarData}>
+          <PolarRadiusAxis orientation="left" cx={250} cy={250} label="test" tick={<Tick />} dataKey="value" />
+        </RadarChart>,
+      );
+
+      expect(container.querySelectorAll('.customized-tick')).toHaveLength(5);
+      expect(container.querySelectorAll('.recharts-label')).toHaveLength(1);
+    });
+
+    test('Renders label when label is set to be a function', () => {
+      const renderLabel = (props: any) => {
+        const { x, y } = props;
+
+        return (
+          <text x={x} y={y} className="customized-label">
+            any
+          </text>
+        );
+      };
+      const { container } = render(
+        <RadarChart width={500} height={500} data={exampleRadarData}>
+          <PolarRadiusAxis orientation="left" cx={250} cy={250} label={renderLabel} dataKey="value" />
+        </RadarChart>,
+      );
+
+      expect(container.querySelectorAll('.customized-label')).toHaveLength(1);
+    });
+
+    test('Renders label when label is set to be a react element', () => {
+      const Label = (props: any) => {
+        const { x, y } = props;
+
+        return (
+          <text x={x} y={y} className="customized-label">
+            any
+          </text>
+        );
+      };
+      const { container } = render(
+        <RadarChart width={500} height={500} data={exampleRadarData}>
+          <PolarRadiusAxis orientation="left" cx={250} cy={250} label={<Label />} dataKey="value" />
+        </RadarChart>,
+      );
+
+      expect(container.querySelectorAll('.customized-label')).toHaveLength(1);
+    });
+
+    test("Don't Renders any ticks or label in when data is empty", () => {
+      const { container } = render(
+        <RadarChart width={500} height={500} data={[]}>
+          <PolarRadiusAxis orientation="left" cx={250} cy={250} label="test" dataKey="value" />
+        </RadarChart>,
+      );
+
+      expectRadiusAxisTicks(container, []);
+
+      expectRadiusAxisLabel(container, undefined);
+    });
+
+    test("don't render any ticks or label when dataKey is empty", () => {
+      const { container } = render(
+        <RadarChart width={500} height={500} data={exampleRadarData}>
+          <PolarRadiusAxis orientation="left" cx={250} cy={250} label="test" dataKey="" />
+        </RadarChart>,
+      );
+
+      expectRadiusAxisTicks(container, []);
+
+      expectRadiusAxisLabel(container, undefined);
+    });
+
     test('Renders polar radius axis', () => {
       const { container } = render(
         <RadarChart width={500} height={500} data={exampleRadarData}>
