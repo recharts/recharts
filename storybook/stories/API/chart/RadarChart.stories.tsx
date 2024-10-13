ac21724cf13b69eba91104d0d07f5d69cb7eb03a
Pavel Vanecek <corkscreewe@gmail.com>
2024-10-13 20:39:30 +0900

    Update RadarChart story so that it shows axes and grid instead of black blob

--- storybook/stories/API/chart/RadarChart.stories.tsx
+++ storybook/stories/API/chart/RadarChart.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { pageData } from '../../data';
-import { Radar, RadarChart } from '../../../../src';
+import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, Tooltip } from '../../../../src';
 import { getStoryArgsFromArgsTypesObject } from '../props/utils';
 import { RadarChartProps } from '../props/RadarChartProps';
 
@@ -13,7 +13,11 @@ export const Simple = {
   render: (args: Record<string, any>) => {
     return (
       <RadarChart {...args}>
-        <Radar dataKey="uv" />
+        <PolarAngleAxis dataKey="name" />
+        <PolarRadiusAxis />
+        <PolarGrid />
+        <Tooltip defaultIndex={1} />
+        <Radar dataKey="uv" stroke="green" strokeOpacity={0.7} fill="green" fillOpacity={0.5} strokeWidth={3} />
       </RadarChart>
     );
   },
