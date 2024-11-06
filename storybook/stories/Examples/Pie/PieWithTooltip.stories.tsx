fd50fb8c67a60b5b9e8af3388afc4212396838ed
ajay-studio137 <ajay@studio137.co.za>
2024-11-06 21:47:58 +0530

    Add storybook for arrowed tooltip in Piechart. (#5215)

---
+++ storybook/stories/Examples/Pie/PieWithTooltip.stories.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Args } from '@storybook/react';
+import { PieChart, Pie, Cell, Tooltip, Legend } from '../../../../src';
+
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
+
+interface DataPoint {
+  name: string;
+  value: number;
+}
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const CustomContent = ({ active, payload }: { active: boolean; payload: Array<DataPoint> }) => {
+  if (active && payload && payload.length > 0) {
+    return (
+      <div className="my-custom-tooltip">
+        {payload.map(pld => (
+          <p
+            key={pld.name}
+            style={{
+              borderStyle: 'solid 1px',
+              fontSize: '13px',
+              fontWeight: '600',
+              fontFamily: 'sans-serif',
+              color: '#fff',
+            }}
+          >
+            {`${pld.name} : ${pld.value}`}
+          </p>
+        ))}
+        <div className="tooltip-arrow" />
+      </div>
+    );
+  }
+  return null;
+};
+export default {
+  component: Pie,
+};
+
+export const PieWithTooltip = {
+  render: (args: Args) => {
+    const [ttPos, setTtPos] = useState(undefined);
+    const [active, setActive] = useState(false);
+    return (
+      <PieChart
+        width={400}
+        height={400}
+        onMouseMove={(_: any, event: MouseEvent) => {
+          // follow the mouse and adjust for some offset
+          setTtPos({ x: event.clientX - 185, y: event.clientY - 50 });
+        }}
+      >
+        <Pie
+          {...args}
+          dataKey="value"
+          onMouseEnter={() => {
+            setActive(true);
+          }}
+          onMouseLeave={() => {
+            setActive(false);
+          }}
+        >
+          {data.map((value: DataPoint, index: number) => (
+            <Cell key={`cell-${value.name}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip content={CustomContent} position={ttPos} active={active} />
+        <Legend />
+      </PieChart>
+    );
+  },
+  args: {
+    cx: 200,
+    cy: 200,
+    data,
+    fill: '#8884d8',
+    outerRadius: 80,
+    labelLine: false,
+  },
+};
