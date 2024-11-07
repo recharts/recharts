11e2b8e13e53b7daee087ee5e0f9e48339dfb856
Coltin Kifer <ckifer@amazon.com>
2024-11-07 08:02:57 +0000

    feat: remove both names and values from the tooltip if formatter returns a nullish value

--- test/component/DefaultTooltipContent.spec.tsx
+++ test/component/DefaultTooltipContent.spec.tsx
@@ -49,8 +49,45 @@ describe('DefaultTooltipContent', () => {
     itemSorter: (d: { name: string }) => d.name,
     labelFormatter: () => `mock labelFormatter`,
   };
+
   it('renders without crashing, finds div with default class attr', () => {
     const { container } = render(<DefaultTooltipContent {...mockProps} />);
     expect(container.querySelectorAll('div.recharts-default-tooltip').length).toBe(1);
+  });
+
+  it('does not render any name or value when tooltip formatter returns null', () => {
+    const mockPropsWithFormatter = {
+      ...mockProps,
+      formatter: (): null => null,
+    };
+    const { container } = render(<DefaultTooltipContent {...mockPropsWithFormatter} />);
+    const tooltip = container.querySelectorAll('div.recharts-default-tooltip');
+    expect(tooltip.length).toBe(1);
+
+    expect(tooltip[0]).toHaveTextContent('mock labelFormatter');
+  });
+
+  it('renders the value returned by the formatter as a recharts tooltip item', () => {
+    const mockPropsWithFormatter = {
+      ...mockProps,
+      formatter: (): string => 'SOME VALUE',
+    };
+    const { container } = render(<DefaultTooltipContent {...mockPropsWithFormatter} />);
+    const tooltip = container.querySelectorAll('div.recharts-default-tooltip');
+    expect(tooltip.length).toBe(1);
+
+    expect(tooltip[0]).toHaveTextContent('mock labelFormatteruv : SOME VALUE');
+  });
+
+  it('renders the name and value returned by the formatter as a recharts tooltip item', () => {
+    const mockPropsWithFormatter = {
+      ...mockProps,
+      formatter: (): [string, string] => ['SOME VALUE', 'SOME NAME'],
+    };
+    const { container } = render(<DefaultTooltipContent {...mockPropsWithFormatter} />);
+    const tooltip = container.querySelectorAll('div.recharts-default-tooltip');
+    expect(tooltip.length).toBe(1);
+
+    expect(tooltip[0]).toHaveTextContent('mock labelFormatterSOME NAME : SOME VALUE');
   });
 });
