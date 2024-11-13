e54b52c60b2ffb04cf03b9f9fbcc57f0df744544
Pavel Vanecek <corkscreewe@gmail.com>
2024-11-13 18:46:29 +0900

    Add story for spurious correlation

---
+++ storybook/stories/data/spurriousCorrelations.ts
@@ -0,0 +1,70 @@
+/**
+ * Popularity of the first name Mara
+ * correlates with
+ * Average length of Stand-up Maths YouTube videos
+ *
+ * Found in: https://tylervigen.com/spurious/correlation/6628_popularity-of-the-first-name-mara_correlates-with_average-length-of-stand-up-maths-youtube-videos
+ * By Tyler Vigen
+ */
+export const babiesAndVideosCorrelation = [
+  {
+    year: 2011,
+    babies: 312,
+    length: 194.5,
+  },
+  {
+    year: 2012,
+    babies: 335,
+    length: 345.333,
+  },
+  {
+    year: 2013,
+    babies: 366,
+    length: 243.667,
+  },
+  {
+    year: 2014,
+    babies: 409,
+    length: 843.5,
+  },
+  {
+    year: 2015,
+    babies: 382,
+    length: 484.4,
+  },
+  {
+    year: 2016,
+    babies: 427,
+    length: 735.898,
+  },
+  {
+    year: 2017,
+    babies: 439,
+    length: 733.833,
+  },
+  {
+    year: 2018,
+    babies: 430,
+    length: 638.625,
+  },
+  {
+    year: 2019,
+    babies: 438,
+    length: 700.172,
+  },
+  {
+    year: 2020,
+    babies: 463,
+    length: 1009.8,
+  },
+  {
+    year: 2021,
+    babies: 568,
+    length: 1556.52,
+  },
+  {
+    year: 2022,
+    babies: 476,
+    length: 1288.42,
+  },
+];
