61e511f32df9e0165b67125599678feeb8623a40
Pavel Vanecek <corkscreewe@gmail.com>
2024-12-19 10:59:39 +0900

    Remove formattedGraphicalItems - it's not used anywhere anymore

--- src/chart/generateCategoricalChart.tsx
+++ src/chart/generateCategoricalChart.tsx
@@ -864,7 +864,6 @@ export const generateCategoricalChart = ({
     const ticksObj = tooltipTicksGenerator(cateAxisMap);
 
     return {
-      formattedGraphicalItems: [],
       graphicalItems,
       offset,
       stackGroups,
@@ -945,13 +944,13 @@ export const generateCategoricalChart = ({
       }
 
       const activeLabel = this.state.tooltipTicks[defaultIndex] && this.state.tooltipTicks[defaultIndex].value;
-      let activePayload = getTooltipContent(this.state, data, defaultIndex, activeLabel);
+      const activePayload = getTooltipContent(this.state, data, defaultIndex, activeLabel);
 
       const independentAxisCoord = this.state.tooltipTicks[defaultIndex].coordinate;
       const dependentAxisCoord = (this.state.offset.top + height) / 2;
 
       const isHorizontal = layout === 'horizontal';
-      let activeCoordinate = isHorizontal
+      const activeCoordinate = isHorizontal
         ? {
             x: independentAxisCoord,
             y: dependentAxisCoord,
@@ -960,20 +959,6 @@ export const generateCategoricalChart = ({
             y: independentAxisCoord,
             x: dependentAxisCoord,
           };
-
-      // Unlike other chart types, scatter plot's tooltip positions rely on both X and Y coordinates. Only the scatter plot
-      // element knows its own Y coordinates.
-      // If there's a scatter plot, we'll want to grab that element for an interrogation.
-      const scatterPlotElement = this.state.formattedGraphicalItems.find(
-        ({ item }: { item: any }) => item.type.name === 'Scatter',
-      );
-      if (scatterPlotElement) {
-        activeCoordinate = {
-          ...activeCoordinate,
-          ...scatterPlotElement.props.points[defaultIndex].tooltipPosition,
-        };
-        activePayload = scatterPlotElement.props.points[defaultIndex].tooltipPayload;
-      }
 
       const nextState = {
         activeTooltipIndex: defaultIndex,
