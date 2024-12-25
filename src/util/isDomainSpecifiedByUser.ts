1999c68d1b306216f9037e0ec762a316ec0ac8bf
Pavel Vanecek <corkscreewe@gmail.com>
2024-12-25 11:35:29 +0900

    Remove isDomainSpecifiedByUser

--- src/util/isDomainSpecifiedByUser.ts
+++ src/util/isDomainSpecifiedByUser.ts
@@ -1,37 +1,7 @@
 import { MAX_VALUE_REG, MIN_VALUE_REG } from './ChartUtils';
 import { isNumber } from './DataUtils';
-import { AxisDomain, AxisDomainType, NumberDomain } from './types';
+import { AxisDomain, NumberDomain } from './types';
 import { isWellBehavedNumber } from './isWellBehavedNumber';
-
-/**
- * @deprecated instead use `numericalDomainSpecifiedWithoutRequiringData`
- *
- * Takes a domain and user props to determine whether he provided the domain via props or if we need to calculate it.
- * @param   {AxisDomain}  domain              The potential domain from props
- * @param   {Boolean}     allowDataOverflow   from props
- * @param   {String}      axisType            from props
- * @returns {Boolean}                         `true` if domain is specified by user
- */
-export function isDomainSpecifiedByUser(
-  domain: AxisDomain,
-  allowDataOverflow: boolean,
-  axisType: AxisDomainType,
-): boolean {
-  if (axisType === 'number' && allowDataOverflow === true && Array.isArray(domain)) {
-    const domainStart: unknown | null | undefined = domain?.[0];
-    const domainEnd: unknown | null | undefined = domain?.[1];
-
-    /*
-     * The `isNumber` check is needed because the user could also provide strings like "dataMin" via the domain props.
-     * In such case, we have to compute the domain from the data.
-     */
-    if (!!domainStart && !!domainEnd && isNumber(domainStart) && isNumber(domainEnd)) {
-      return true;
-    }
-  }
-
-  return false;
-}
 
 export function isWellFormedNumberDomain(v: unknown): v is NumberDomain {
   if (Array.isArray(v) && v.length === 2) {
