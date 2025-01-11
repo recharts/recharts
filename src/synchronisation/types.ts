aff8b2ee1efe8abce5b270d8d56299366adfd54a
Pavel Vanecek <corkscreewe@gmail.com>
2025-01-11 12:02:52 +0900

    Fix syncMethod=function

--- src/synchronisation/types.ts
+++ src/synchronisation/types.ts
@@ -1,5 +1,13 @@
-import { TickItem } from '../util/types';
-import { CategoricalChartState } from '../chart/types';
+import { Coordinate, DataKey, TickItem } from '../util/types';
+
+export type CustomSyncMethodDataParam = {
+  activeTooltipIndex: number | undefined;
+  isTooltipActive: boolean;
+  activeIndex: number | undefined;
+  activeLabel: string | undefined;
+  activeDataKey: DataKey<any> | undefined;
+  activeCoordinate: Coordinate | undefined;
+};
 
 /**
  * Allows customisation of how the charts will synchronize tooltips and brushes.
@@ -7,6 +15,11 @@ import { CategoricalChartState } from '../chart/types';
  *
  * 'index': other charts will reuse current datum's index within the data array. In cases where data does not have the same length, this might yield unexpected results.
  * 'value': will try to match other charts values
- * custom function: will receive ticks, data as argument and should return an index.
+ * custom function: will receive two arguments and should return an index of the active tick in the current chart:
+ * argument 1: ticks from the current chart
+ * argument 2: active tooltip state from the other chart
  */
-export type SyncMethod = 'index' | 'value' | ((ticks: TickItem[], data: CategoricalChartState) => number);
+export type SyncMethod =
+  | 'index'
+  | 'value'
+  | ((ticks: ReadonlyArray<TickItem>, data: CustomSyncMethodDataParam) => number);
