4cb5010135f7b4e48e5f2a0af7b3ccc4d7b2f8ad
Coltin Kifer <ckifer@amazon.com>
2025-02-25 06:01:49 +0000

    fix: add onTouchMove touchEvent middleware

---
+++ src/state/touchEventsMiddleware.ts
@@ -0,0 +1,38 @@
+import { createAction, createListenerMiddleware, ListenerEffectAPI, PayloadAction } from '@reduxjs/toolkit';
+import * as React from 'react';
+import { AppDispatch, RechartsRootState } from './store';
+import { setMouseOverAxisIndex } from './tooltipSlice';
+import { selectActivePropsFromChartPointer } from './selectors/selectActivePropsFromChartPointer';
+
+import { getChartPointer } from '../util/getChartPointer';
+
+export const touchEventAction = createAction<React.TouchEvent<HTMLDivElement>>('touchMove');
+
+export const touchEventMiddleware = createListenerMiddleware();
+
+touchEventMiddleware.startListening({
+  actionCreator: touchEventAction,
+  effect: (
+    action: PayloadAction<React.TouchEvent<HTMLDivElement>>,
+    listenerApi: ListenerEffectAPI<RechartsRootState, AppDispatch>,
+  ) => {
+    const touchEvent = action.payload;
+    const activeProps = selectActivePropsFromChartPointer(
+      listenerApi.getState(),
+      getChartPointer({
+        clientX: touchEvent.touches[0].clientX,
+        clientY: touchEvent.touches[0].clientY,
+        currentTarget: touchEvent.currentTarget,
+      }),
+    );
+    if (activeProps?.activeIndex != null) {
+      listenerApi.dispatch(
+        setMouseOverAxisIndex({
+          activeIndex: activeProps.activeIndex,
+          activeDataKey: undefined,
+          activeCoordinate: activeProps.activeCoordinate,
+        }),
+      );
+    }
+  },
+});
