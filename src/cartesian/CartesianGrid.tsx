993ef54653e20c12bd9c685e2aeac5a01b5a5a90
Joaquin Palacios <joa_palacios@hotmail.com>
2025-03-20 09:54:48 +1100

    Fix: Update CartesianGrid component

--- src/cartesian/CartesianGrid.tsx
+++ src/cartesian/CartesianGrid.tsx
@@ -92,10 +92,11 @@ interface CartesianGridProps extends InternalCartesianGridProps {
    * The values from this array will be passed in as the `fill` property in a `rect` SVG element.
    * For possible values see: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/fill#rect
    *
-   * In case there are more stripes than colors, the colors will start from beginning
+   * In case there are more stripes than colors, the colors will start from beginning.
    * So for example: verticalFill['yellow', 'black'] produces a pattern of yellow|black|yellow|black
    *
    * If this is undefined, or an empty array, then there is no background fill.
+   * Note: Grid lines will be rendered above these background stripes.
    */
   verticalFill?: string[];
   /**
@@ -104,10 +105,11 @@ interface CartesianGridProps extends InternalCartesianGridProps {
    * The values from this array will be passed in as the `fill` property in a `rect` SVG element.
    * For possible values see: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/fill#rect
    *
-   * In case there are more stripes than colors, the colors will start from beginning
+   * In case there are more stripes than colors, the colors will start from beginning.
    * So for example: horizontalFill['yellow', 'black'] produces a pattern of yellow|black|yellow|black
    *
    * If this is undefined, or an empty array, then there is no background fill.
+   * Note: Grid lines will be rendered above these background stripes.
    */
   horizontalFill?: string[];
   /**
