cf39baa1fc9aa9f44c1e904989a20df561c9be46
Pavel Vanecek <corkscreewe@gmail.com>
2025-04-23 19:06:57 +0900

    Remove get, set from Global
    we never call the set anyway and there is a type error in it.
    
    1108 -> 1107 errors

--- src/util/Global.ts
+++ src/util/Global.ts
@@ -1,30 +1,6 @@
 const parseIsSsrByDefault = (): boolean =>
   !(typeof window !== 'undefined' && window.document && window.document.createElement && window.setTimeout);
 
-export interface GlobalConfig {
-  isSsr?: boolean;
-}
-
-export type GlobalConfigKeys = 'isSsr';
-
 export const Global = {
   isSsr: parseIsSsrByDefault(),
-
-  get: (key: 'isSsr') => {
-    return Global[key];
-  },
-
-  set: (key: GlobalConfigKeys | GlobalConfig, value?: any) => {
-    if (typeof key === 'string') {
-      Global[key] = value;
-    } else {
-      const keys = Object.keys(key);
-
-      if (keys && keys.length) {
-        keys.forEach((k: GlobalConfigKeys) => {
-          Global[k] = key[k];
-        });
-      }
-    }
-  },
 };
