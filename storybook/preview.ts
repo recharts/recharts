160ba667e39e4fb8e1c6ac37c6133a65043c72d0
Coltin Kifer <ckifer@amazon.com>
2025-05-20 05:52:45 +0000

    rm autodocs tag

--- storybook/preview.ts
+++ storybook/preview.ts
@@ -21,7 +21,6 @@ const preview: Preview = {
     // Sets a delay of 1550ms before each story to give them time to animate (recharts animation default is 1500ms)
     chromatic: { delay: 1550 },
   },
-  tags: ['autodocs'],
 };
 
 export default preview;
