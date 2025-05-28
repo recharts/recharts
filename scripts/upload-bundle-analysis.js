017e5b4ee42e42628f4bd222a53dc24811073a08
Pavel Vanecek <corkscreewe@gmail.com>
2025-05-28 22:31:49 +0900

    Add codecov bundle analysis plugin

---
+++ scripts/upload-bundle-analysis.js
@@ -0,0 +1,23 @@
+const { createAndUploadReport } = require('@codecov/bundle-analyzer');
+
+const buildDirs = ['lib'];
+
+const coreOpts = {
+  dryRun: true,
+  uploadToken: process.env.CODECOV_TOKEN,
+  retryCount: 3,
+  apiUrl: 'https://api.codecov.io',
+  bundleName: 'recharts/bundle-cjs',
+  enableBundleAnalysis: true,
+  debug: true,
+};
+
+const bundleAnalyzerOpts = {
+  beforeReportUpload: async original => original,
+  ignorePatterns: ['*.map'],
+  normalizeAssetsPattern: '[name]-[hash].js',
+};
+
+createAndUploadReport(buildDirs, coreOpts, bundleAnalyzerOpts)
+  .then(reportAsJson => console.log(`Report successfully generated and uploaded: ${reportAsJson}`))
+  .catch(error => console.error('Failed to generate or upload report:', error));
