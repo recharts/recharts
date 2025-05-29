51a6ed632f14ca1d1d276f04b791fe209c92a187
saurabhraj123 <me4saurabh4u@gmail.com>
2025-05-30 00:04:11 +0530

    chore: add js docs for getCalculatedYAxisWidth

--- src/util/YAxisUtils.tsx
+++ src/util/YAxisUtils.tsx
@@ -35,6 +35,15 @@ export const getModifiedYAxisProps = ({
   return { label: modifiedLabel };
 };
 
+/**
+ * Calculates the width of the Y-axis based on the tick labels and the axis label.
+ *
+ * @param {Object} params - The parameters object.
+ * @param {React.RefObject<any>} params.cartesianAxisRef - The ref to the CartesianAxis component.
+ * @param {React.RefObject<SVGElement>} params.labelRef - The ref to the label element.
+ * @param {number} [params.labelGapWithTick=5] - The gap between the label and the tick.
+ * @returns {number} The calculated width of the Y-axis.
+ */
 export const getCalculatedYAxisWidth = ({
   cartesianAxisRef,
   labelRef,
