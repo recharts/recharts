2910df71522570325c75e8a4e34b6a8442015229
Pavel Vanecek <corkscreewe@gmail.com>
2025-06-02 16:28:20 +0900

    Reuse array instance between renders

--- src/chart/LineChart.tsx
+++ src/chart/LineChart.tsx
@@ -3,13 +3,16 @@ import { forwardRef } from 'react';
 import { arrayTooltipSearcher } from '../state/optionsSlice';
 import { CartesianChart } from './CartesianChart';
 import { CategoricalChartProps } from './generateCategoricalChart';
+import { TooltipEventType } from '../util/types';
+
+const allowedTooltipTypes: ReadonlyArray<TooltipEventType> = ['axis'];
 
 export const LineChart = forwardRef<SVGSVGElement, CategoricalChartProps>((props: CategoricalChartProps, ref) => {
   return (
     <CartesianChart
       chartName="LineChart"
       defaultTooltipEventType="axis"
-      validateTooltipEventTypes={['axis']}
+      validateTooltipEventTypes={allowedTooltipTypes}
       tooltipPayloadSearcher={arrayTooltipSearcher}
       categoricalChartProps={props}
       ref={ref}
