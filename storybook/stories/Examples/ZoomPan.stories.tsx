8db2762865daf52a02f6b4da0b0bea2f772ce385
Fefe_du_973 <80718477+Fefedu973@users.noreply.github.com>
2025-06-05 15:52:40 +0200

    Add diverse zoom/pan storybook examples

--- storybook/stories/Examples/ZoomPan.stories.tsx
+++ storybook/stories/Examples/ZoomPan.stories.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import { StoryContext } from '@storybook/react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from '../../../src';
-import { pageData } from '../data';
+import {
+  LineChart,
+  Line,
+  BarChart,
+  Bar,
+  ScatterChart,
+  Scatter,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from '../../../src';
+import { pageData, babiesAndVideosCorrelation } from '../data';
 import { RechartsHookInspector } from '../../storybook-addon-recharts/RechartsHookInspector';
 
 export default {
@@ -21,6 +33,51 @@ export const BasicZoomPan = {
         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
         <RechartsHookInspector rechartsInspectorEnabled={context.rechartsInspectorEnabled} />
       </LineChart>
+    </ResponsiveContainer>
+  ),
+};
+
+export const XAxisZoom = {
+  render: (_: Record<string, any>, context: StoryContext) => (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={500} height={300} data={pageData} zoom={{ mode: 'x', maxScale: 8 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+        <RechartsHookInspector rechartsInspectorEnabled={context.rechartsInspectorEnabled} />
+      </LineChart>
+    </ResponsiveContainer>
+  ),
+};
+
+export const YAxisZoomBar = {
+  render: (_: Record<string, any>, context: StoryContext) => (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart width={500} height={300} data={pageData} zoom={{ mode: 'y' }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="amt" fill="#8884d8" />
+        <RechartsHookInspector rechartsInspectorEnabled={context.rechartsInspectorEnabled} />
+      </BarChart>
+    </ResponsiveContainer>
+  ),
+};
+
+export const ScatterXYZoom = {
+  render: (_: Record<string, any>, context: StoryContext) => (
+    <ResponsiveContainer width="100%" height={400}>
+      <ScatterChart data={babiesAndVideosCorrelation} zoom={{ mode: 'xy', minScale: 1, maxScale: 10 }}>
+        <CartesianGrid />
+        <XAxis type="number" dataKey="year" />
+        <YAxis type="number" dataKey="babies" />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter dataKey="length" fill="#8884d8" />
+        <RechartsHookInspector rechartsInspectorEnabled={context.rechartsInspectorEnabled} />
+      </ScatterChart>
     </ResponsiveContainer>
   ),
 };
