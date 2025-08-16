458a37e8947f06f66bc7eb60604e86819ba0d223
Pavel Vanecek <corkscreewe@gmail.com>
2025-08-16 21:11:27 +0900

    Add VR tests for LabelList in other components

--- test-vr/tests/LabelList.spec-vr.tsx
+++ test-vr/tests/LabelList.spec-vr.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
 import { test, expect } from '@playwright/experimental-ct-react';
-import { AreaExplicitLabelListTest, AreaImplicitLabelListTest } from './LabelListComponents';
+import {
+  AreaExplicitLabelListTest,
+  AreaImplicitLabelListTest,
+  BarExplicitLabelListTest,
+  BarImplicitLabelListTest,
+  LineExplicitLabelListTest,
+  LineImplicitLabelListTest,
+  ScatterExplicitLabelListTest,
+  ScatterImplicitLabelListTest,
+  PieExplicitLabelListTest,
+  PieImplicitLabelListTest,
+  RadarExplicitLabelListTest,
+  RadarImplicitLabelListTest,
+  RadialBarExplicitLabelListTest,
+  RadialBarImplicitLabelListTest,
+} from './LabelListComponents';
 
 test('Area > implicit LabelList', async ({ mount }) => {
   const component = await mount(<AreaImplicitLabelListTest />);
@@ -9,5 +24,65 @@ test('Area > implicit LabelList', async ({ mount }) => {
 
 test('Area > explicit LabelList', async ({ mount }) => {
   const component = await mount(<AreaExplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Bar > implicit LabelList', async ({ mount }) => {
+  const component = await mount(<BarImplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Bar > explicit LabelList', async ({ mount }) => {
+  const component = await mount(<BarExplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Line > implicit LabelList', async ({ mount }) => {
+  const component = await mount(<LineImplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Line > explicit LabelList', async ({ mount }) => {
+  const component = await mount(<LineExplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Scatter > implicit LabelList', async ({ mount }) => {
+  const component = await mount(<ScatterImplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Scatter > explicit LabelList', async ({ mount }) => {
+  const component = await mount(<ScatterExplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Pie > implicit LabelList', async ({ mount }) => {
+  const component = await mount(<PieImplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Pie > explicit LabelList', async ({ mount }) => {
+  const component = await mount(<PieExplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Radar > implicit LabelList', async ({ mount }) => {
+  const component = await mount(<RadarImplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('Radar > explicit LabelList', async ({ mount }) => {
+  const component = await mount(<RadarExplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('RadialBar > implicit LabelList', async ({ mount }) => {
+  const component = await mount(<RadialBarImplicitLabelListTest />);
+  await expect(component).toHaveScreenshot();
+});
+
+test('RadialBar > explicit LabelList', async ({ mount }) => {
+  const component = await mount(<RadialBarExplicitLabelListTest />);
   await expect(component).toHaveScreenshot();
 });
--- test-vr/tests/LabelListComponents.tsx
+++ test-vr/tests/LabelListComponents.tsx
@@ -3,10 +3,30 @@
  */
 
 import * as React from 'react';
-import { Area, AreaChart, LabelList } from '../../src';
-import { pageData } from '../../storybook/stories/data';
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  Line,
+  LineChart,
+  Scatter,
+  ScatterChart,
+  Pie,
+  PieChart,
+  Radar,
+  RadarChart,
+  RadialBar,
+  RadialBarChart,
+  LabelList,
+  XAxis,
+  YAxis,
+} from '../../src';
+import { pageData, pageDataWithFillColor } from '../../storybook/stories/data';
 
 const shortData = pageData.slice(2, 4);
+
+const shortFillColorData = pageDataWithFillColor.slice(2, 4);
 
 const margin = { top: 155, right: 180, left: 180, bottom: 0 };
 
@@ -33,10 +53,6 @@ export const availablePositions = [
   'centerBottom',
   'middle',
 ] as const;
-
-// Parent Components
-
-// <Area /><Bar /><Line /><Scatter /><Pie /><Radar /><RadialBar />
 
 export const AreaImplicitLabelListTest = () => (
   <AreaChart width={900} height={500} data={shortData} margin={margin}>
@@ -68,4 +84,212 @@ export const AreaExplicitLabelListTest = () => (
       ))}
     </Area>
   </AreaChart>
+);
+
+export const BarImplicitLabelListTest = () => (
+  <BarChart width={900} height={500} data={shortData} margin={margin}>
+    {availablePositions.map(position => (
+      <Bar
+        dataKey="uv"
+        fill="none"
+        stroke="green"
+        // @ts-expect-error typescript type says that valueAccessor is not a valid prop, but it does work
+        label={{ position, offset: 50, angle: -30, valueAccessor: () => position }}
+      />
+    ))}
+    <LabelList />
+  </BarChart>
+);
+
+export const BarExplicitLabelListTest = () => (
+  <BarChart width={900} height={500} data={shortData} margin={margin}>
+    <Bar dataKey="uv" fill="none" stroke="green">
+      {availablePositions.map(position => (
+        <LabelList
+          key={position}
+          position={position}
+          offset={50}
+          angle={-30}
+          className={position}
+          valueAccessor={() => position}
+        />
+      ))}
+    </Bar>
+  </BarChart>
+);
+
+export const LineImplicitLabelListTest = () => (
+  <LineChart width={900} height={500} data={shortData} margin={margin}>
+    {availablePositions.map(position => (
+      <Line
+        type="monotone"
+        dataKey="uv"
+        stroke="#8884d8"
+        // @ts-expect-error typescript type says that valueAccessor is not a valid prop, but it does work
+        label={{ position, offset: 50, angle: -30, valueAccessor: () => position }}
+      />
+    ))}
+    <LabelList />
+  </LineChart>
+);
+
+export const LineExplicitLabelListTest = () => (
+  <LineChart width={900} height={500} data={shortData} margin={margin}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8">
+      {availablePositions.map(position => (
+        <LabelList
+          key={position}
+          position={position}
+          offset={50}
+          angle={-30}
+          className={position}
+          valueAccessor={() => position}
+        />
+      ))}
+    </Line>
+  </LineChart>
+);
+
+export const ScatterImplicitLabelListTest = () => (
+  <ScatterChart width={900} height={500} margin={margin}>
+    <XAxis dataKey="name" allowDuplicatedCategory={false} />
+    <YAxis dataKey="uv" />
+    {availablePositions.map(position => (
+      <Scatter
+        data={shortData}
+        fill="#8884d8"
+        label={{ position, offset: 50, angle: -30, valueAccessor: () => position }}
+      />
+    ))}
+    <LabelList />
+  </ScatterChart>
+);
+
+export const ScatterExplicitLabelListTest = () => (
+  <ScatterChart width={900} height={500} margin={margin}>
+    <XAxis dataKey="name" />
+    <YAxis dataKey="uv" />
+    <Scatter data={shortData} fill="#8884d8">
+      {availablePositions.map(position => (
+        <LabelList
+          key={position}
+          position={position}
+          offset={50}
+          angle={-30}
+          className={position}
+          valueAccessor={() => position}
+        />
+      ))}
+    </Scatter>
+  </ScatterChart>
+);
+
+export const PieImplicitLabelListTest = () => (
+  <PieChart width={900} height={500}>
+    {availablePositions.map(position => (
+      <Pie
+        data={shortFillColorData}
+        dataKey="uv"
+        cx={450}
+        cy={250}
+        outerRadius={120}
+        isAnimationActive={false}
+        // @ts-expect-error typescript type says that position is not a valid prop, but it does work
+        label={{ position, offset: 50, angle: -30, valueAccessor: () => position }}
+      />
+    ))}
+    <LabelList />
+  </PieChart>
+);
+
+export const PieExplicitLabelListTest = () => (
+  <PieChart width={900} height={500}>
+    <Pie data={shortFillColorData} dataKey="uv" isAnimationActive={false}>
+      {availablePositions.map(position => (
+        <LabelList
+          key={position}
+          position={position}
+          offset={50}
+          angle={-30}
+          className={position}
+          valueAccessor={() => position}
+        />
+      ))}
+    </Pie>
+  </PieChart>
+);
+
+export const RadarImplicitLabelListTest = () => (
+  <RadarChart width={900} height={500} data={shortData} margin={margin}>
+    {availablePositions.map(position => (
+      <Radar
+        dataKey="uv"
+        stroke="#8884d8"
+        fill="none"
+        label={{ position, offset: 50, angle: -30, valueAccessor: () => position }}
+      />
+    ))}
+    <LabelList />
+  </RadarChart>
+);
+
+export const RadarExplicitLabelListTest = () => (
+  <RadarChart width={900} height={500} data={shortData} margin={margin}>
+    <Radar dataKey="uv" stroke="#8884d8" fill="none">
+      {availablePositions.map(position => (
+        <LabelList
+          key={position}
+          position={position}
+          offset={50}
+          angle={-30}
+          className={position}
+          valueAccessor={() => position}
+        />
+      ))}
+    </Radar>
+  </RadarChart>
+);
+
+export const RadialBarImplicitLabelListTest = () => (
+  <RadialBarChart
+    width={900}
+    height={500}
+    cx={450}
+    cy={250}
+    innerRadius={60}
+    outerRadius={120}
+    barSize={20}
+    data={shortData}
+  >
+    {availablePositions.map(position => (
+      <RadialBar dataKey="uv" fill="none" label={{ position, offset: 50, angle: -30, valueAccessor: () => position }} />
+    ))}
+    <LabelList />
+  </RadialBarChart>
+);
+
+export const RadialBarExplicitLabelListTest = () => (
+  <RadialBarChart
+    width={900}
+    height={500}
+    cx={450}
+    cy={250}
+    innerRadius={60}
+    outerRadius={120}
+    barSize={20}
+    data={shortData}
+  >
+    <RadialBar dataKey="uv" fill="none">
+      {availablePositions.map(position => (
+        <LabelList
+          key={position}
+          position={position}
+          offset={50}
+          angle={-30}
+          className={position}
+          valueAccessor={() => position}
+        />
+      ))}
+    </RadialBar>
+  </RadialBarChart>
 );
