93967e75004deb5c2d2719abb462b46e658ae6d8
Pavel Vanecek <corkscreewe@gmail.com>
2025-08-24 23:49:45 +0900

    Fix exception when Scatter data has non-string type property

--- test/chart/ScatterChart.spec.tsx
+++ test/chart/ScatterChart.spec.tsx
@@ -2652,3 +2652,31 @@ describe('ScatterChart with allowDuplicateCategory=false', () => {
     ]);
   });
 });
+
+describe('ScatterChart with data with type property', () => {
+  // https://github.com/recharts/recharts/issues/6197
+  const data = [
+    { type: 1, x: 100, y: 200, z: 200 },
+    { x: 120, y: 100, z: 260 },
+    { x: 170, y: 300, z: 400 },
+    { x: 140, y: 250, z: 280 },
+    { x: 150, y: 400, z: 500 },
+    { x: 110, y: 280, z: 200 },
+  ];
+
+  const renderTestCase = createSelectorTestCase(({ children }) => (
+    <ScatterChart width={500} height={500}>
+      <CartesianGrid />
+      <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+      <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+      <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+      <Scatter name="A school" data={data} fill="#8884d8" />
+      {children}
+    </ScatterChart>
+  ));
+
+  it('should render without throwing error', () => {
+    const { container } = renderTestCase();
+    expect(container).toBeInTheDocument();
+  });
+});
