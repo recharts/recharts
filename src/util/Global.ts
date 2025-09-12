79547e613402ee77ffddbb121288ccb058c299bb
uncaught <uncaught42@gmail.com>
2025-09-12 07:55:59 +0200

    Enable dev tools via Global module instead of window to fix server-side rendering.

--- src/util/Global.ts
+++ src/util/Global.ts
@@ -2,5 +2,6 @@ const parseIsSsrByDefault = (): boolean =>
   !(typeof window !== 'undefined' && window.document && Boolean(window.document.createElement) && window.setTimeout);
 
 export const Global = {
+  devToolsEnabled: false,
   isSsr: parseIsSsrByDefault(),
 };
