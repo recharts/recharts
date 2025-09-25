17a474a9e7ff9fdb047d4aa4b7ec477aba16d4ca
Pavel Vanecek <corkscreewe@gmail.com>
2025-09-25 19:05:16 +0900

    Convert SimpleRadialBarChart to tsx

--- www/src/docs/exampleComponents/RadialBarChart/SimpleRadialBarChart.tsx
+++ www/src/docs/exampleComponents/RadialBarChart/SimpleRadialBarChart.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
@@ -57,7 +56,7 @@ const Example = () => {
   return (
     <ResponsiveContainer width="100%" height="100%">
       <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={data}>
-        <RadialBar minAngle={15} label={{ position: 'insideStart', fill: '#fff' }} background clockWise dataKey="uv" />
+        <RadialBar label={{ position: 'insideStart', fill: '#fff' }} background dataKey="uv" />
         <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
       </RadialBarChart>
     </ResponsiveContainer>
