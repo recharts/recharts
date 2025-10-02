8f149c539e2e35c83ecca4cc9118dfee0dc0a4f4
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-03 08:03:28 +0900

    Newline

--- www/src/docs/apiExamples/Legend.tsx
+++ www/src/docs/apiExamples/Legend.tsx
@@ -71,7 +71,8 @@ const example: ApiExampleDemo = ({ isAnimationActive }) => (
 const exampleCode = `
 <LineChart
   style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
-  responsive data={data}
+  responsive
+  data={data}
   margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
 >
   <XAxis dataKey="name" />
