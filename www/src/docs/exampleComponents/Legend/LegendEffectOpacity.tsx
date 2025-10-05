a2613ae44cc3b9d486ed2820fee90b52c965faa8
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-05 16:18:01 +0900

    Legend examples are responsive

--- www/src/docs/exampleComponents/Legend/LegendEffectOpacity.tsx
+++ www/src/docs/exampleComponents/Legend/LegendEffectOpacity.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  LegendPayload,
-} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LegendPayload } from 'recharts';
 import { DataKey } from 'recharts/types/util/types';
 
 const data = [
@@ -57,7 +47,7 @@ const data = [
   },
 ];
 
-const Example = () => {
+const LegendEffectOpacity = () => {
   const [hoveringDataKey, setHoveringDataKey] = React.useState<DataKey<any> | undefined>(undefined);
 
   let pvOpacity = 1;
@@ -81,30 +71,28 @@ const Example = () => {
 
   return (
     <div style={{ width: '100%' }}>
-      <ResponsiveContainer width="100%" height={300}>
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
-          <Line type="monotone" dataKey="pv" strokeOpacity={pvOpacity} stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" strokeOpacity={uvOpacity} stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
+      <LineChart
+        style={{ width: '100%', maxWidth: '300px', maxHeight: '70vh', aspectRatio: 1.618 }}
+        responsive
+        data={data}
+        margin={{
+          top: 20,
+          right: 0,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis width="auto" />
+        <Tooltip />
+        <Legend onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
+        <Line type="monotone" dataKey="pv" strokeOpacity={pvOpacity} stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="uv" strokeOpacity={uvOpacity} stroke="#82ca9d" />
+      </LineChart>
       <p className="notes">Tips: Hover the legend !</p>
     </div>
   );
 };
 
-export default Example;
+export default LegendEffectOpacity;
