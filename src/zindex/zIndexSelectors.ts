318d567e3e140512585ae9221caaa252d65a899f
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-20 22:16:01 +0900

    Add null check

--- src/zindex/zIndexSelectors.ts
+++ src/zindex/zIndexSelectors.ts
@@ -1,11 +1,14 @@
 import { createSelector } from 'reselect';
 import { RechartsRootState } from '../state/store';
 
-export const selectZIndexQuerySelector: (state: RechartsRootState, zIndex: number) => string | undefined =
+export const selectZIndexQuerySelector: (state: RechartsRootState, zIndex: number | undefined) => string | undefined =
   createSelector(
     (state: RechartsRootState) => state.zIndex.zIndexMap,
     (_: RechartsRootState, zIndex: number) => zIndex,
     (zIndexMap, zIndex) => {
+      if (zIndex == null) {
+        return undefined;
+      }
       return zIndexMap[zIndex]?.elementId;
     },
   );
