8ef006ec8a06c56dd5d3c63e4287f7e60c4ec82e
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-21 23:07:09 +0900

    11 errors -> 10

--- src/context/useTooltipAxis.ts
+++ src/context/useTooltipAxis.ts
@@ -10,5 +10,8 @@ export const useTooltipAxisBandSize = (): number | undefined => {
   const tooltipAxis = useTooltipAxis();
   const tooltipTicks = useAppSelector(selectTooltipAxisTicks);
   const tooltipAxisScale = useAppSelector(selectTooltipAxisScale);
+  if (!tooltipAxis || !tooltipAxisScale) {
+    return getBandSizeOfAxis(undefined, tooltipTicks);
+  }
   return getBandSizeOfAxis({ ...tooltipAxis, scale: tooltipAxisScale }, tooltipTicks);
 };
