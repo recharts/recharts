09061b728416505452794fade4253aed0818f661
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-21 18:19:54 +0900

    75 errors -> 73

--- src/cartesian/ZAxis.tsx
+++ src/cartesian/ZAxis.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Component, useEffect } from 'react';
-import { ScaleType, DataKey, AxisDomain } from '../util/types';
+import { useEffect } from 'react';
+import { AxisDomain, DataKey, ScaleType } from '../util/types';
 import { addZAxis, removeZAxis, ZAxisSettings } from '../state/cartesianAxisSlice';
 import { useAppDispatch } from '../state/hooks';
 import { RechartsScale } from '../util/ChartUtils';
 import { AxisRange, implicitZAxis } from '../state/selectors/axisSelectors';
+import { resolveDefaultProps } from '../util/resolveDefaultProps';
 
 function SetZAxisSettings(settings: ZAxisSettings): null {
   const dispatch = useAppDispatch();
@@ -34,33 +35,31 @@ export interface Props {
   domain?: AxisDomain;
 }
 
-// eslint-disable-next-line react/prefer-stateless-function
-export class ZAxis extends Component<Props> {
-  static displayName = 'ZAxis';
+const zAxisDefaultProps = {
+  zAxisId: 0,
+  range: implicitZAxis.range,
+  scale: implicitZAxis.scale,
+  type: implicitZAxis.type,
+} as const satisfies Partial<Props>;
 
-  static defaultProps = {
-    zAxisId: 0,
-    range: implicitZAxis.range,
-    scale: implicitZAxis.scale,
-    type: implicitZAxis.type,
-  };
-
-  render() {
-    return (
-      <SetZAxisSettings
-        domain={this.props.domain}
-        id={this.props.zAxisId}
-        dataKey={this.props.dataKey}
-        name={this.props.name}
-        unit={this.props.unit}
-        range={this.props.range}
-        scale={this.props.scale}
-        type={this.props.type}
-        allowDuplicatedCategory={implicitZAxis.allowDuplicatedCategory}
-        allowDataOverflow={implicitZAxis.allowDataOverflow}
-        reversed={implicitZAxis.reversed}
-        includeHidden={implicitZAxis.includeHidden}
-      />
-    );
-  }
+export function ZAxis(outsideProps: Props) {
+  const props = resolveDefaultProps(outsideProps, zAxisDefaultProps);
+  return (
+    <SetZAxisSettings
+      domain={props.domain}
+      id={props.zAxisId}
+      dataKey={props.dataKey}
+      name={props.name}
+      unit={props.unit}
+      range={props.range}
+      scale={props.scale}
+      type={props.type}
+      allowDuplicatedCategory={implicitZAxis.allowDuplicatedCategory}
+      allowDataOverflow={implicitZAxis.allowDataOverflow}
+      reversed={implicitZAxis.reversed}
+      includeHidden={implicitZAxis.includeHidden}
+    />
+  );
 }
+
+ZAxis.displayName = 'ZAxis';
