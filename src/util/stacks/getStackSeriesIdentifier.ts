8b7983cddffdd26a8ee0c0cb6fc10e0c5845f8bc
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-21 23:06:11 +0900

    13 errors -> 11

--- src/util/stacks/getStackSeriesIdentifier.ts
+++ src/util/stacks/getStackSeriesIdentifier.ts
@@ -3,6 +3,9 @@ import { MaybeStackedGraphicalItem } from '../../state/types/StackedGraphicalIte
 
 export function getStackSeriesIdentifier(graphicalItem: MaybeStackedGraphicalItem): StackSeriesIdentifier;
 export function getStackSeriesIdentifier(graphicalItem: undefined): undefined;
+export function getStackSeriesIdentifier(
+  graphicalItem: MaybeStackedGraphicalItem | undefined,
+): StackSeriesIdentifier | undefined;
 /**
  * Returns identifier for stack series which is one individual graphical item in the stack.
  * @param graphicalItem - The graphical item representing the series in the stack.
