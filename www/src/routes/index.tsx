2aac87ca35f10415c8d0307f14fd9aa9abc3a45a
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-28 00:22:04 +0900

    Add locale-less route variants

--- www/src/routes/index.tsx
+++ www/src/routes/index.tsx
@@ -5,6 +5,7 @@ import { Frame } from '../layouts/Frame';
 export function routes() {
   return (
     <Routes>
+      {/* Routes with explicit locale */}
       <Route
         path="/:locale/guide/:name?/"
         element={
@@ -42,6 +43,66 @@ export function routes() {
         element={
           <Frame>
             <Storybook />
+          </Frame>
+        }
+      />
+
+      {/* Default locale routes (no locale prefix - defaults to en-US) */}
+      <Route
+        path="/guide/:name?/"
+        element={
+          <Frame>
+            <GuideView />
+          </Frame>
+        }
+      />
+      <Route
+        path="/api/:name?/"
+        element={
+          <Frame>
+            <APIView />
+          </Frame>
+        }
+      />
+      <Route
+        path="/examples/"
+        element={
+          <Frame>
+            <ExamplesIndexView />
+          </Frame>
+        }
+      />
+      <Route
+        path="/examples/:name/"
+        element={
+          <Frame>
+            <ExamplesView />
+          </Frame>
+        }
+      />
+      <Route
+        path="/storybook/"
+        element={
+          <Frame>
+            <Storybook />
+          </Frame>
+        }
+      />
+
+      {/* Index and catch-all */}
+      <Route
+        path="/:locale/"
+        element={
+          <Frame>
+            <IndexView />
+          </Frame>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <Frame>
+            <IndexView />
           </Frame>
         }
       />
