ecd05f256db65ae9f904169406247cea691707e8
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-28 22:38:45 +0900

    Remove old ExamplesView

--- www/src/views/index.ts
+++ www/src/views/index.ts
@@ -1,7 +1,7 @@
 import { IndexView } from './IndexView';
 import { GuideView } from './GuideView';
 import { APIView } from './APIView';
-import { ExamplesViewNew as ExamplesView } from './ExamplesViewNew';
+import { ExamplesView } from './ExamplesView.tsx';
 import { ExamplesIndexView } from './ExamplesIndexView';
 import { Storybook } from './Storybook';
 import { NotFoundView } from './NotFoundView';
