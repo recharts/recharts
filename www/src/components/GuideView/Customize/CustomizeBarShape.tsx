45b45ed6d2f5d11d7eeadf4e1b8c547cba55c6fe
Pavel Vanecek <corkscreewe@gmail.com>
2025-10-30 15:55:28 +0900

    Convert Customize to new editor

---
+++ www/src/components/GuideView/Customize/CustomizeBarShape.tsx
@@ -0,0 +1,77 @@
+import { Bar, BarChart, BarProps, XAxis, YAxis } from 'recharts';
+
+// #region Sample data
+const data = [
+  {
+    name: 'Page A',
+    uv: 400,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 300,
+    pv: 4567,
+    amt: 2400,
+  },
+  {
+    name: 'Page C',
+    uv: 300,
+    pv: 1398,
+    amt: 2400,
+  },
+  {
+    name: 'Page D',
+    uv: 200,
+    pv: 9800,
+    amt: 2400,
+  },
+  {
+    name: 'Page E',
+    uv: 278,
+    pv: 3908,
+    amt: 2400,
+  },
+  {
+    name: 'Page F',
+    uv: 189,
+    pv: 4800,
+    amt: 2400,
+  },
+];
+
+const margin = {
+  top: 20,
+  right: 30,
+  left: 20,
+  bottom: 5,
+};
+// #endregion
+
+const getPath = (x: number, y: number, width: number, height: number) =>
+  `M${x},${y + height}
+   C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
+   C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+   Z`;
+
+export function TriangleBar(props: BarProps) {
+  const { fill, x, y, width, height } = props;
+
+  if (x == null || y == null || width == null || height == null) {
+    // recharts types demand that JSX.Element is returned, even if nothing is rendered
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <></>;
+  }
+
+  return <path d={getPath(Number(x), Number(y), Number(width), Number(height))} stroke="none" fill={fill} />;
+}
+
+export default function CustomizeBarShape() {
+  return (
+    <BarChart width={600} height={300} data={data} margin={margin}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Bar dataKey="uv" fill="#8884d8" shape={TriangleBar} />
+    </BarChart>
+  );
+}
