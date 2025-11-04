b98be032e57ebcf414081f3360b8edbf8689ea8b
Pavel Vanecek <corkscreewe@gmail.com>
2025-11-04 20:34:39 +0900

    Convert all SASS variables to CSS variables

--- www/src/components/Navigation.tsx
+++ www/src/components/Navigation.tsx
@@ -35,7 +35,7 @@ function HamburgerIcon() {
       data={[{ v: 1 }, { v: 1 }, { v: 1 }]}
       layout="vertical"
     >
-      <Bar dataKey="v" fill="var(--var-brand-info)" barSize={3} radius={[2, 2, 2, 2]} />
+      <Bar dataKey="v" fill="var(--brand-info)" barSize={3} radius={[2, 2, 2, 2]} />
       <XAxis dataKey="v" type="number" hide />
       <YAxis type="category" hide />
     </BarChart>
