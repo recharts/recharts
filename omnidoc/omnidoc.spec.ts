bce97219cfd533eeda68e3d1af62a54c71f314e2
Pavel Vanecek <corkscreewe@gmail.com>
2025-11-12 22:00:13 +0900

    Add import

--- omnidoc/omnidoc.spec.ts
+++ omnidoc/omnidoc.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, test } from 'vitest';
 import { ProjectDocReader } from './readProject';
 import { ApiDocReader } from './readApiDoc';
 import { StorybookDocReader } from './readStorybookDoc';
