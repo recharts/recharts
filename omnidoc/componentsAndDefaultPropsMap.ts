7becf32900f971f96a76ccdcfaa36cae6100946b
Pavel Vanecek <corkscreewe@gmail.com>
2025-11-22 22:51:46 +0900

    Fix YAxis default props

--- omnidoc/componentsAndDefaultPropsMap.ts
+++ omnidoc/componentsAndDefaultPropsMap.ts
@@ -12,6 +12,7 @@ import { errorBarDefaultProps } from '../src/cartesian/ErrorBar';
 import { referenceAreaDefaultProps } from '../src/cartesian/ReferenceArea';
 import { referenceDotDefaultProps } from '../src/cartesian/ReferenceDot';
 import { xAxisDefaultProps } from '../src/cartesian/XAxis';
+import { yAxisDefaultProps } from '../src/cartesian/YAxis';
 
 type ComponentMeta = {
   defaultProps: Record<string, unknown> | undefined;
@@ -32,4 +33,5 @@ export const componentMetaMap: Record<string, ComponentMeta> = {
   Text: { defaultProps: textDefaultProps },
   Treemap: { defaultProps: defaultTreeMapProps },
   XAxis: { defaultProps: xAxisDefaultProps },
+  YAxis: { defaultProps: yAxisDefaultProps },
 };
