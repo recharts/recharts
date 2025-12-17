0447f699678078d78d191fc689fb53a13424ad9f
Pavel Vanecek <corkscreewe@gmail.com>
2025-12-17 11:30:56 +0900

    Remove extra fragment

--- src/chart/SunburstChart.tsx
+++ src/chart/SunburstChart.tsx
@@ -364,18 +364,16 @@ const SunburstChartImpl = ({
   return (
     <Surface width={width} height={height}>
       <Layer className={layerClass}>{sectors}</Layer>
-      <>
-        <SetSunburstTooltipEntrySettings
-          dataKey={dataKey}
-          nameKey={nameKey}
-          data={data}
-          stroke={stroke}
-          fill={fill}
-          positions={positions}
-          id={id}
-        />
-        {children}
-      </>
+      <SetSunburstTooltipEntrySettings
+        dataKey={dataKey}
+        nameKey={nameKey}
+        data={data}
+        stroke={stroke}
+        fill={fill}
+        positions={positions}
+        id={id}
+      />
+      {children}
     </Surface>
   );
 };
