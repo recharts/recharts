6261b5709cdecd297b9a68d9de41f66d6688e15d
Pavel Vanecek <corkscreewe@gmail.com>
2025-12-19 13:36:42 +0900

    Fix BarStack clipPath with stackOffset=sign

--- src/state/selectors/barStackSelectors.ts
+++ src/state/selectors/barStackSelectors.ts
@@ -58,10 +58,10 @@ export const expandRectangle = (
   if (!rect2) {
     return rect1;
   }
-  const x = Math.min(rect1.x, rect2.x);
-  const y = Math.min(rect1.y, rect2.y);
-  const maxX = Math.max(rect1.x + rect1.width, rect2.x + rect2.width);
-  const maxY = Math.max(rect1.y + rect1.height, rect2.y + rect2.height);
+  const x = Math.min(rect1.x, rect1.x + rect1.width, rect2.x, rect2.x + rect2.width);
+  const y = Math.min(rect1.y, rect1.y + rect1.height, rect2.y, rect2.y + rect2.height);
+  const maxX = Math.max(rect1.x, rect1.x + rect1.width, rect2.x, rect2.x + rect2.width);
+  const maxY = Math.max(rect1.y, rect1.y + rect1.height, rect2.y, rect2.y + rect2.height);
   const width = maxX - x;
   const height = maxY - y;
   return { x, y, width, height };
