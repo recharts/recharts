15dddfafa33c65d3e8fa8adfc5507f639415095c
Luca Barone <baro.luc@gmail.com>
2026-01-11 17:57:51 +0100

    refactor: simplify dark mode snapshot test by removing unnecessary light mode checks

--- test-vr/tests/www/dark-mode.spec-vr.tsx
+++ test-vr/tests/www/dark-mode.spec-vr.tsx
@@ -10,19 +10,6 @@ test.use({
 test('dark mode snapshot', async ({ mount }) => {
   const component = await mount(<Root />);
 
-  /**
-   * Until dark mode is fully rolled out, light mode is the default.
-   * So we need to switch to dark mode for this test.
-   */
-  // const lightModePicker = component.getByRole('button', { name: 'light' });
-  // await expect(lightModePicker).toBeEnabled();
-  // await lightModePicker.click();
-
-  // const darkModePicker = component.getByRole('button', { name: 'dark' });
-  // await expect(darkModePicker).toBeInViewport();
-  // await darkModePicker.click();
-
-  const systemModePicker = component.getByRole('button', { name: 'system' });
-  await expect(systemModePicker).toBeInViewport();
+  await expect(component.getByRole('button', { name: 'system' })).toBeInViewport();
   await expect(component).toHaveScreenshot();
 });
