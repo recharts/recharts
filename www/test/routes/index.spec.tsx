f2424db5876047ce0bc41bd68491b9bd7fae0356
Luca Barone <baro.luc@gmail.com>
2026-01-11 13:45:21 +0100

    feat: integrate ColorModeProvider into route tests and clean up color mode tests

--- www/test/routes/index.spec.tsx
+++ www/test/routes/index.spec.tsx
@@ -4,9 +4,14 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router';
 import { routes } from '../../src/routes';
+import { ColorModeProvider, defineColorModeStore } from '../../src/components/color-mode';
 
 function baseRender(url: string) {
-  const renderResult = render(<MemoryRouter initialEntries={[url]}>{routes()}</MemoryRouter>);
+  const renderResult = render(
+    <ColorModeProvider store={defineColorModeStore()}>
+      <MemoryRouter initialEntries={[url]}>{routes()}</MemoryRouter>
+    </ColorModeProvider>,
+  );
   expect(renderResult.getByRole('main')).toBeInTheDocument();
   return renderResult;
 }
