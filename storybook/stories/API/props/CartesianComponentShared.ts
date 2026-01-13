53da0ca43317f508d66d50945e882e07406afb43
Pavel Vanecek <corkscreewe@gmail.com>
2026-01-13 22:26:52 +0900

    Replace General

--- storybook/stories/API/props/CartesianComponentShared.ts
+++ storybook/stories/API/props/CartesianComponentShared.ts
@@ -1,4 +1,4 @@
-import { StorybookArg, StorybookArgs } from '../../../StorybookArgs';
+import { StorybookArg } from '../../../StorybookArgs';
 
 export const dataKey: StorybookArg = {
   description: `The key or getter of a group of data.
@@ -7,41 +7,4 @@ export const dataKey: StorybookArg = {
     type: { summary: 'string | number | function' },
     category: 'General',
   },
-};
-
-export const xAxisId: StorybookArg = {
-  description: 'The id of x-axis which is corresponding to the data.',
-  defaultValue: 0,
-  table: { type: { summary: 'string | number' }, category: 'General' },
-};
-export const yAxisId: StorybookArg = {
-  description: 'The id of y-axis which is corresponding to the data.',
-  defaultValue: 0,
-  table: { type: { summary: 'string | number' }, category: 'General' },
-};
-
-export const General: StorybookArgs = {
-  dataKey,
-  id: {
-    description: `The unique id of this component, which will be used to generate unique clip path id internally.
-      This props is suggested to be set in SSR.`,
-    type: { name: 'string' },
-    table: { category: 'General' },
-  },
-  name: {
-    description: `The name of data. This option will be used in tooltip and legend to represent a line.
-    If no value was set to this option, the value of dataKey will be used alternatively.`,
-    table: {
-      type: { summary: 'string | number' },
-      category: 'General',
-    },
-  },
-  unit: {
-    table: {
-      type: { summary: 'string | number' },
-      category: 'General',
-    },
-  },
-  xAxisId,
-  yAxisId,
 };
