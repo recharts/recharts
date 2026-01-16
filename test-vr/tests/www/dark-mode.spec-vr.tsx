821094b3584699b5603d9ef197174021532e00e1
Luca Barone <baro.luc@gmail.com>
2026-01-16 07:55:29 +0100

    feat: add dark mode snapshots and improve SimpleLineChart styling for better theming

--- test-vr/tests/www/dark-mode.spec-vr.tsx
+++ test-vr/tests/www/dark-mode.spec-vr.tsx
@@ -1,17 +1,49 @@
 import * as React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { expect, test } from '@playwright/experimental-ct-react';
 
-import { Root } from '../../../www/src/containers/Root.tsx';
+import { ExamplesIndexView } from '../../../www/src/views/ExamplesIndexView.tsx';
+import { Frame } from '../../../www/src/layouts/Frame.tsx';
+import { TestColorModeProvider } from './TestColorModeProvider.tsx';
+import SimpleLineChart from '../../../www/src/docs/exampleComponents/LineChart/SimpleLineChart.tsx';
 
 test.use({
   colorScheme: 'dark',
 });
 
-test('dark mode snapshot', async ({ mount }) => {
-  const component = await mount(<Root />);
+test('dark mode: examples list', async ({ mount }) => {
+  const component = await mount(
+    <TestColorModeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="examples" />} />
+          <Route
+            path="/examples"
+            element={
+              <Frame>
+                <ExamplesIndexView />
+              </Frame>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </TestColorModeProvider>,
+  );
 
-  await expect(component.getByRole('button', { name: 'system' })).toBeInViewport();
-  await expect(component).toHaveScreenshot({
-    mask: [component.getByTestId('github-star-btn'), component.getByTestId('user-item')],
-  });
+  await expect(component).toHaveScreenshot();
+});
+
+test('dark mode: Simple Line Chart', async ({ mount }) => {
+  const component = await mount(
+    <TestColorModeProvider>
+      <div
+        style={{
+          backgroundColor: 'var(--color-surface-base)',
+        }}
+      >
+        <SimpleLineChart />
+      </div>
+    </TestColorModeProvider>,
+  );
+  await expect(component).toHaveScreenshot();
 });
