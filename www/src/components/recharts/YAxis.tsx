aa816a9b723b88bd3cce9688459957ca6ce5ea61
Luca Barone <baro.luc@gmail.com>
2026-01-17 20:11:26 +0100

    refactor: remove unused chart components to streamline codebase

--- www/src/components/recharts/YAxis.tsx
+++
@@ -1,5 +0,0 @@
-import * as C from 'recharts';
-
-export function YAxis(props: C.YAxisProps) {
-  return <C.YAxis stroke="var(--color-text-3)" {...props} />;
-}
--- www/src/components/recharts/index.ts
+++
@@ -1,6 +0,0 @@
-export { Bar, BarChart, Pie, PieChart, LineChart, Legend } from 'recharts';
-export { Tooltip } from './Tooltip';
-export { Line } from './Line';
-export { CartesianGrid } from './CartesianGrid';
-export { YAxis } from './YAxis';
-export { XAxis } from './XAxis';
