1d8fee68eba556d7da72569c13f63d69d457bb46
Luca Barone <baro.luc@gmail.com>
2026-01-17 20:11:26 +0100

    refactor: remove unused chart components to streamline codebase

--- www/src/components/recharts/CartesianGrid.tsx
+++
@@ -1,5 +0,0 @@
-import * as C from 'recharts';
-
-export function CartesianGrid(props: C.CartesianGridProps) {
-  return <C.CartesianGrid stroke="var(--color-border-3)" {...props} />;
-}
--- www/src/components/recharts/XAxis.tsx
+++
@@ -1,5 +0,0 @@
-import * as C from 'recharts';
-
-export function XAxis(props: C.XAxisProps) {
-  return <C.XAxis stroke="var(--color-text-3)" {...props} />;
-}
