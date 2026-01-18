ac3fbdb92939eef1551c26ccf7cfee8deb14c576
Pavel Vanecek <corkscreewe@gmail.com>
2026-01-18 19:40:47 +0900

    Add index to Scatter.shape, export ScatterShapeProps

--- src/util/ScatterUtils.tsx
+++ src/util/ScatterUtils.tsx
@@ -3,15 +3,22 @@ import { ActiveShape, SymbolType } from './types';
 import { ScatterPointItem } from '../cartesian/Scatter';
 import { Symbols } from '../shape/Symbols';
 import { Shape } from './ActiveShapeUtils';
+import { DATA_ITEM_GRAPHICAL_ITEM_ID_ATTRIBUTE_NAME } from './Constants';
+import { GraphicalItemId } from '../state/graphicalItemsSlice';
+
+export type ScatterShapeProps = ScatterPointItem & {
+  index: number;
+  [DATA_ITEM_GRAPHICAL_ITEM_ID_ATTRIBUTE_NAME]: GraphicalItemId;
+};
 
 export function ScatterSymbol({
   option,
   isActive,
   ...props
 }: {
-  option: ActiveShape<ScatterPointItem> | SymbolType;
+  option: ActiveShape<ScatterShapeProps> | SymbolType;
   isActive: boolean;
-} & ScatterPointItem) {
+} & ScatterShapeProps) {
   if (typeof option === 'string') {
     return <Shape option={<Symbols type={option} {...props} />} isActive={isActive} shapeType="symbols" {...props} />;
   }
