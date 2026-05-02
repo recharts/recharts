3b9205fd2dcf3055cc429d175a8753bcd70da050
Pavel Vanecek <corkscreewe@gmail.com>
2026-05-02 22:14:33 +0900

    Type union instead of unknown

--- www/src/docs/apiCates.ts
+++ www/src/docs/apiCates.ts
@@ -105,6 +105,13 @@ export const apiCates = [
   },
   {
     name: 'animation',
-    items: ['matchByIndex', 'matchByDataKey', 'matchAppend', 'AnimationProgressProvider', 'useAnimationProgress', 'interpolate'],
+    items: [
+      'matchByIndex',
+      'matchByDataKey',
+      'matchAppend',
+      'AnimationProgressProvider',
+      'useAnimationProgress',
+      'interpolate',
+    ],
   },
 ];
