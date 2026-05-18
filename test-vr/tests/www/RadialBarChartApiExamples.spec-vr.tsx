7339d3645532d4fb94d876caf10cd2add5b57773
Pavel Vanecek <corkscreewe@gmail.com>
2026-05-18 18:05:04 +0900

    Update VR tests

--- test-vr/tests/www/RadialBarChartApiExamples.spec-vr.tsx
+++ test-vr/tests/www/RadialBarChartApiExamples.spec-vr.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { test, expect } from '@playwright/experimental-ct-react';
 
-import RadialBarChartExample from '../../../www/src/docs/exampleComponents/RadialBarChart/RadialBarChartExample';
 import SimpleRadialBarChart from '../../../www/src/docs/exampleComponents/RadialBarChart/SimpleRadialBarChart';
 import RadialBarChartNavExample from '../../../www/src/docs/exampleComponents/RadialBarChart/RadialBarChartNavExample';
 import RadialBarChartClickToFocusLegendExample from '../../../www/src/docs/exampleComponents/RadialBarChart/RadialBarChartClickToFocusLegendExample';
@@ -13,11 +12,6 @@ test('RadialBarChartNavExample', async ({ mount }) => {
 
 test('SimpleRadialBarChart', async ({ mount }) => {
   const component = await mount(<SimpleRadialBarChart />);
-  await expect(component).toHaveScreenshot();
-});
-
-test('RadialBarChartExample', async ({ mount }) => {
-  const component = await mount(<RadialBarChartExample isAnimationActive={false} />);
   await expect(component).toHaveScreenshot();
 });
 
