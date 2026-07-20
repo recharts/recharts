4d577e557a92ea735027835956339e8122b360b9
kimlj <kljulongbayan@gmail.com>
2026-07-20 17:34:23 +0800

    docs(AreaChart): add connectNulls stacked all-null example and VR snapshots

--- www/src/docs/exampleComponents/AreaChart/AreaChartConnectNulls.tsx
+++ www/src/docs/exampleComponents/AreaChart/AreaChartConnectNulls.tsx
@@ -46,6 +46,43 @@ const data = [
   },
 ];
 
+const dataWithGaps = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    // no series has a value here
+  },
+  {
+    name: 'Page D',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    // only uv is missing here
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 // #endregion
 const AreaChartConnectNulls = () => {
   return (
@@ -113,6 +150,48 @@ const AreaChartConnectNulls = () => {
         style={{ width: '100%', maxWidth: '700px', maxHeight: '30vh', aspectRatio: 1.618 }}
         responsive
         data={data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area connectNulls type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+        <Area connectNulls type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+        <Area connectNulls type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        <RechartsDevtools />
+      </AreaChart>
+
+      <AreaChart
+        style={{ width: '100%', maxWidth: '700px', maxHeight: '30vh', aspectRatio: 1.618 }}
+        responsive
+        data={dataWithGaps}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+        <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+        <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        <RechartsDevtools />
+      </AreaChart>
+
+      <AreaChart
+        style={{ width: '100%', maxWidth: '700px', maxHeight: '30vh', aspectRatio: 1.618 }}
+        responsive
+        data={dataWithGaps}
         margin={{
           top: 10,
           right: 30,
