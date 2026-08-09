d6ffd63eb545828f435dfb42803297b0f3855213
Pavel Vanecek <corkscreewe@gmail.com>
2026-08-09 12:34:32 +0200

    Update Radar examples and VR tests

--- www/src/docs/exampleComponents/RadarChart/RadarChartNavExample.tsx
+++ www/src/docs/exampleComponents/RadarChart/RadarChartNavExample.tsx
@@ -2,12 +2,12 @@ import { Radar, RadarChart, PolarGrid } from 'recharts';
 
 // #region Sample data
 const data = [
-  { subject: 'Math', A: 120, B: 110, fullMark: 150 },
-  { subject: 'Chinese', A: 98, B: 130, fullMark: 150 },
-  { subject: 'English', A: 86, B: 130, fullMark: 150 },
-  { subject: 'Geography', A: 99, B: 100, fullMark: 150 },
-  { subject: 'Physics', A: 85, B: 90, fullMark: 150 },
-  { subject: 'History', A: 65, B: 85, fullMark: 150 },
+  { subject: 'Math', A: 120 },
+  { subject: 'Chinese', A: 98 },
+  { subject: 'English', A: 86 },
+  { subject: 'Geography', A: 99 },
+  { subject: 'Physics', A: 85 },
+  { subject: 'History', A: 65 },
 ];
 
 // #endregion
@@ -15,7 +15,7 @@ const RadarChartNavExample = () => {
   return (
     <RadarChart responsive outerRadius="120%" data={data} style={{ aspectRatio: 1 }}>
       <PolarGrid />
-      <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+      <Radar name="Mike" dataKey="A" />
     </RadarChart>
   );
 };
--- www/src/docs/exampleComponents/RadarChart/SimpleRadarChart.tsx
+++ www/src/docs/exampleComponents/RadarChart/SimpleRadarChart.tsx
@@ -3,42 +3,12 @@ import { RechartsDevtools } from '@recharts/devtools';
 
 // #region Sample data
 const data = [
-  {
-    subject: 'Math',
-    A: 120,
-    B: 110,
-    fullMark: 150,
-  },
-  {
-    subject: 'Chinese',
-    A: 98,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: 'English',
-    A: 86,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: 'Geography',
-    A: 99,
-    B: 100,
-    fullMark: 150,
-  },
-  {
-    subject: 'Physics',
-    A: 85,
-    B: 90,
-    fullMark: 150,
-  },
-  {
-    subject: 'History',
-    A: 65,
-    B: 85,
-    fullMark: 150,
-  },
+  { subject: 'Math', A: 120 },
+  { subject: 'Chinese', A: 98 },
+  { subject: 'English', A: 86 },
+  { subject: 'Geography', A: 99 },
+  { subject: 'Physics', A: 85 },
+  { subject: 'History', A: 65 },
 ];
 
 // #endregion
@@ -47,19 +17,12 @@ const SimpleRadarChart = () => {
     <RadarChart
       style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
       responsive
-      outerRadius="80%"
       data={data}
-      margin={{
-        top: 20,
-        left: 20,
-        right: 20,
-        bottom: 20,
-      }}
     >
       <PolarGrid />
       <PolarAngleAxis dataKey="subject" />
       <PolarRadiusAxis />
-      <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+      <Radar name="Mike" dataKey="A" />
       <RechartsDevtools />
     </RadarChart>
   );
