f4739533f3bd4ce9b8ce9cfbb9a88c38112b60e1
Medyooo <arbielbaidi6@gmail.com>
2026-08-24 22:38:27 +0200

    docs: add locale formatting examples to tickFormatter and Tooltip formatters

--- src/component/Tooltip.tsx
+++ src/component/Tooltip.tsx
@@ -137,6 +137,8 @@ export type TooltipProps<TValue extends ValueType = ValueType, TName extends Nam
   /**
    * Function to customize the value in the tooltip.
    * If you return an array, the first entry will be the formatted "value", and the second entry will be the formatted "name"
+   *
+   * @example <Tooltip formatter={(value) => new Intl.NumberFormat('ar', { numberingSystem: 'arab' }).format(value)} />
    */
   formatter?: (
     value: TValue,
@@ -177,6 +179,8 @@ export type TooltipProps<TValue extends ValueType = ValueType, TName extends Nam
   itemStyle?: CSSProperties;
   /**
    * The formatter function of label in tooltip.
+   *
+   * @example <Tooltip labelFormatter={(label) => new Intl.NumberFormat('ar', { numberingSystem: 'arab' }).format(label)} />
    */
   labelFormatter?: (label: ReactNode, payload: TooltipPayload) => ReactNode;
   /**
--- src/util/types.ts
+++ src/util/types.ts
@@ -951,6 +951,8 @@ export interface RenderableAxisProps<DataPointType, DataValueType> extends BaseA
   tickSize?: number;
   /**
    * The formatter function of tick.
+   *
+   * @example <XAxis tickFormatter={(value) => new Intl.NumberFormat('ar', { numberingSystem: 'arab' }).format(value)} />
    */
   tickFormatter?: (value: any, index: number) => string;
   /**
