ccdf5c8416efbb14c7ec15c46e60e6aecb82b50f
Pavel Vanecek <corkscreewe@gmail.com>
2026-09-09 17:35:54 +0200

    Migrate BarChart.reverseStackOrder spec to testWithThemes fixture
    Relates to https://github.com/recharts/recharts/issues/7737

--- test-vr/tests/BarChart.reverseStackOrder.spec-vr.tsx
+++ test-vr/tests/BarChart.reverseStackOrder.spec-vr.tsx
@@ -1,11 +1,11 @@
-import { expect, test } from './fixtures';
+import { expect, testWithThemes } from './fixtures';
 
-test('StackedBarChart with reverseStackOrder=false', async ({ mountStory }) => {
+testWithThemes('StackedBarChart with reverseStackOrder=false', async ({ mountStory }) => {
   const component = await mountStory('BarChart.reverseStackOrder/StackedBarChartWithReverseStackOrderFalse');
   await expect(component).toHaveScreenshot();
 });
 
-test('StackedBarChart with reverseStackOrder=true', async ({ mountStory }) => {
+testWithThemes('StackedBarChart with reverseStackOrder=true', async ({ mountStory }) => {
   const component = await mountStory('BarChart.reverseStackOrder/StackedBarChartWithReverseStackOrderTrue');
   await expect(component).toHaveScreenshot();
 });
