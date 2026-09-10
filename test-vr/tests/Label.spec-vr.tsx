c4a6c7e38184c2445c6e58aad7672d7af1835304
Pavel Vanecek <corkscreewe@gmail.com>
2026-09-10 12:01:19 +0200

    migrate Label visual-regression spec to testWithThemes fixture (#7737)

--- test-vr/tests/Label.spec-vr.tsx
+++ test-vr/tests/Label.spec-vr.tsx
@@ -1,51 +1,51 @@
-import { expect, test } from './fixtures';
+import { expect, testWithThemes } from './fixtures';
 
-test('XAxis > Label', async ({ mountStory }) => {
+testWithThemes('XAxis > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/XAxisLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('YAxis > Label', async ({ mountStory }) => {
+testWithThemes('YAxis > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/YAxisLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('ReferenceArea > Label', async ({ mountStory }) => {
+testWithThemes('ReferenceArea > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/ReferenceAreaLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('ReferenceDot > Label', async ({ mountStory }) => {
+testWithThemes('ReferenceDot > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/ReferenceDotLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('ReferenceLine > Label', async ({ mountStory }) => {
+testWithThemes('ReferenceLine > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/ReferenceLineLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('RadarChart > Label', async ({ mountStory }) => {
+testWithThemes('RadarChart > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/RadarChartLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('PolarRadiusAxis > Label', async ({ mountStory }) => {
+testWithThemes('PolarRadiusAxis > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/PolarRadiusAxisLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('PolarAngleAxis > Label', async ({ mountStory }) => {
+testWithThemes('PolarAngleAxis > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/PolarAngleAxisLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('PieChart > Label', async ({ mountStory }) => {
+testWithThemes('PieChart > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/PieChartLabel');
   await expect(component).toHaveScreenshot();
 });
 
-test('Pie > Label', async ({ mountStory }) => {
+testWithThemes('Pie > Label', async ({ mountStory }) => {
   const component = await mountStory('Label/PieLabel');
   await expect(component).toHaveScreenshot();
 });
