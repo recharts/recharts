dfb12e8c6d239e85f4275bd4cdeb080036646a6f
Pavel Vanecek <corkscreewe@gmail.com>
2026-09-14 12:33:57 +0200

    Migrate SankeyChart VR spec to testWithThemes
    Migrate test-vr/tests/SankeyChart.spec-vr.tsx from legacy test fixture to testWithThemes for automatic legacy/light/dark variants. Add light/dark snapshots and refresh legacy baselines.
    
    Relates to https://github.com/recharts/recharts/issues/7737

--- test-vr/tests/SankeyChart.spec-vr.tsx
+++ test-vr/tests/SankeyChart.spec-vr.tsx
@@ -1,26 +1,26 @@
-import { expect, test } from './fixtures';
+import { expect, testWithThemes } from './fixtures';
 
-test('Sankey align justify', async ({ mountStory }) => {
+testWithThemes('Sankey align justify', async ({ mountStory }) => {
   const component = await mountStory('SankeyChart/SankeyAlignJustify');
   await expect(component).toHaveScreenshot();
 });
 
-test('Sankey align left', async ({ mountStory }) => {
+testWithThemes('Sankey align left', async ({ mountStory }) => {
   const component = await mountStory('SankeyChart/SankeyAlignLeft');
   await expect(component).toHaveScreenshot();
 });
 
-test('Sankey verticalAlign justify', async ({ mountStory }) => {
+testWithThemes('Sankey verticalAlign justify', async ({ mountStory }) => {
   const component = await mountStory('SankeyChart/SankeyVerticalAlignJustify');
   await expect(component).toHaveScreenshot();
 });
 
-test('Sankey verticalAlign top', async ({ mountStory }) => {
+testWithThemes('Sankey verticalAlign top', async ({ mountStory }) => {
   const component = await mountStory('SankeyChart/SankeyVerticalAlignTop');
   await expect(component).toHaveScreenshot();
 });
 
-test('Sankey should keep intermediate nodes out of skipped-depth links', async ({ mountStory }) => {
+testWithThemes('Sankey should keep intermediate nodes out of skipped-depth links', async ({ mountStory }) => {
   const component = await mountStory('SankeyChart/SankeyKeepIntermediateNodesOutOfSkippedDepthLinks');
   await expect(component).toHaveScreenshot();
 });
