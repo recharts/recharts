60f2e4b43338e41e0234b9e7e7970b8e141c0d24
Pavel Vanecek <corkscreewe@gmail.com>
2026-09-16 14:14:08 +0200

    Remove OneChartFourThemes.spec-vr.tsx and dark-mode.spec-vr.tsx

--- test-vr/tests/theme/OneChartFourThemes.spec-vr.tsx
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '../fixtures';
-
-test('one chart four themes', async ({ mountStory }) => {
-  const component = await mountStory('theme/OneChartFourThemes/OneChartFourThemes');
-  await expect(component).toHaveScreenshot();
-});
